8a2938eb204db87ee8398e5139b6d01de9406414
한대건 <han15050424@gmail.com>
2020-06-27 14:18:16 +0900

    Floor(층) 클래스와 Room(객실) 클래스를 추가한다.
    층이 방을 제어하는 방식으로 작동한다. 이 둘이 커플링을 맺고 있다는 것이 조금 그렇기는 하다만..아무튼!
    
    Room클래스 :
    
    - Init함수의 인자를 통해서 자신을 제어하는 층을 주입받는다.
    - CR_update코루틴을 통해서 자신의 하위 객체들을 업데이트 한다. 여기에 해당되는 '하위 객체'는 주로 몬스터나,
    아이템 상자가 해당된다.
    - CR_update코루틴은 자신을 제어하는 층에 의해 제어된다. 층에 플레이어가 존재하지 않는다는 정보를 받게되면
    코루틴을 중단하고, 업데이트의 여부를 제어하는 CanUpdate의 값이 거짓인 동안에는 코루틴을 중단한다.
    
    Floor클래스 :
    
    - Init함수를 통해서 자신이 제어할 방 객체를 생성한다(아직 만들어 지지 않음 그럴 계획이라는 말)
    - mCanUpdate변수의 값을 통해 자신이 제어하는 방 객체들을 업데이트할지를 제어할 수 있다.
    - mIsOnPlayer변수의 값을 통해 해당 층에 더이상의 업데이트가 필요가 있는지를 결정하고, 이를 자신이 제아하는
    방 객체들에게 전달한다.

---
+++ Assets/Floor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    private Room[] mMemberRooms = new Room[3];
+    
+    public  bool  CanUpdate
+    {
+        get { return mCanUpdate; }
+    }
+    private bool mCanUpdate = true;
+
+    public  bool  IsOnPlayer
+    {
+        get { return mIsOnPlayer; }
+    }
+    private bool mIsOnPlayer = false;
+
+    public void Init()
+    {
+        // mMemberRooms배열을 여기서 초기화한다
+    }
+}
---
+++ Assets/Room.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    private Floor mMasterFloor;
+
+    public void Init(Floor masterFloor)
+    {
+        mMasterFloor = masterFloor;
+    }
+
+    private IEnumerator CR_update()
+    {
+        while(mMasterFloor.IsOnPlayer)
+        {
+
+
+            yield return new WaitUntil(() => mMasterFloor.CanUpdate);
+            yield return null;
+        }
+        yield break;
+    }
+}
