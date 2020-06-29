69a47ef4c3ed6c2ef58a29e5a4e3cb3b83190ed9
한대건 <han15050424@gmail.com>
2020-06-29 18:36:45 +0900

    싱글톤 클래스를 추가한다
    말 그대로 싱글톤 클래스를 추가합니다 앓~~

---
+++ Assets/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public  static T  Instnace
+    {
+        get
+        {
+            if(mInstnace == null)
+            {
+                mInstnace = FindObjectOfType<T>() as T;
+
+                if(mInstnace == null)
+                {
+                    mInstnace = new GameObject(typeof(T).ToString(), typeof(T)) as T;
+                }
+                DontDestroyOnLoad(mInstnace);
+            }
+            return mInstnace;
+        }
+    }
+    private static T mInstnace;
+}
