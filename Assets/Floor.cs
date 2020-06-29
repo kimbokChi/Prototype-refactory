3ce78c0fdbba75c094d529e2eeedb51b077d1a36
한대건 <han15050424@gmail.com>
2020-06-29 21:49:02 +0900

    Floor클래스와 Room클래스는 public지정자의 함수IInit와 IUpdate함수를 사용한다. 이렇게하여 Floor가 Room의 초기화/업데이트를 제어할 수 있도록 한다. 그러니까...큰 흐름에서 작은 흐름을 제어할 수 있도록 하는 것이다! 다음 커밋에서는 Floor을 제어하는 Castle클래스가 추가될 것이다.
    이러한 Floor과 Room클래스의 변화에 따라, 기존의 방식에 필요했던 일부 변수를 삭제했고, 새로운 변수를 추가하기도 했다.
    
    Room의 변화에 대해 짚어보자면, 아무레도 시각적으로 보이는 것이 제일 베스트라고 생각하기 때문에, 플레이어가 움직이는
    지점을 스크립트 내부에서 지정하는 것이 아닌, 하이라키창에서 MovePoint라는 이름의 자식 오브젝트의 위치를 조절하는 것으로
    플레이어가 움직이는 지점을 지정할 수 있도록 수정했다.

--- Assets/Floor.cs
+++ Assets/Floor.cs
@@ -6,47 +6,18 @@ public class Floor : MonoBehaviour
 {
     [SerializeField]
     private Room[] mMemberRooms = new Room[3];
-    
-    public  bool  CanUpdate
+
+    public void IInit()
     {
-        get { return mCanUpdate; }
-    }
-    private bool mCanUpdate = true;
-
-    public  bool  IsOnPlayer
-    {
-        get { return mIsOnPlayer; }
-    }
-    private bool mIsOnPlayer = true;
-
-    private void Start() => Init();
-
-    public void Init()
-    {
-        // mMemberRooms배열을 여기서 초기화한다
-
-        for(int i = 0; i < 3; ++i)
-        {
-            mMemberRooms[i].Init(this);
-        }
-
-        StartCoroutine(CR_update());
+        mMemberRooms[0].IInit(this);
+        mMemberRooms[1].IInit(this);
+        mMemberRooms[2].IInit(this);
     }
 
-    private IEnumerator CR_update()
+    public void IUpdate()
     {
-        while(gameObject.activeSelf)
-        {
-            if(Input.GetMouseButtonDown(0))
-            {
-                mIsOnPlayer = !mIsOnPlayer;
-            }
-            if (Input.GetMouseButtonDown(1))
-            {
-                mCanUpdate = !mCanUpdate;
-            }
-            yield return null;
-        }
-        yield break;
+        mMemberRooms[0].IUpdate();
+        mMemberRooms[1].IUpdate();
+        mMemberRooms[2].IUpdate();
     }
 }
--- Assets/Room.cs
+++ Assets/Room.cs
@@ -4,36 +4,23 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    private const float MOVEPOINT_OFFSET = 2.5f;
-
+    public  Transform[]  MovePoints = new Transform[3];
+    private   Vector2[] mMovePoints = new   Vector2[3];
 
     private Floor mMasterFloor;
 
-    private Vector2[] mMovePoints = new Vector2[3];
 
-    public void Init(Floor masterFloor)
+    public void IInit(Floor masterFloor)
     {
         mMasterFloor = masterFloor;
 
-        // 0번째 인덱스 : 왼쪽 지점 || 1번째 인덱스 : 가운데 지점 || 2번째 인덱스 : 오른쪽 지점
-        for (int i = 1; i > -1; --i)
-        {
-            mMovePoints[i] = (Vector2)transform.position + (Vector2.left * MOVEPOINT_OFFSET * i);
-        }
-        StartCoroutine(CR_update());
+        mMovePoints[0] = MovePoints[0].position;
+        mMovePoints[1] = MovePoints[1].position;
+        mMovePoints[2] = MovePoints[2].position;
     }
 
-    private IEnumerator CR_update()
+    public void IUpdate()
     {
-        while (mMasterFloor.IsOnPlayer)
-        {
-            Debug.Log("흐-른-다-!");
-
-            yield return new WaitUntil(() => mMasterFloor.CanUpdate);
-            yield return null;
-        }
-
-        Debug.Log("시스템 종료오오옯ㅇ..");
-        yield break;
+        // todo. . .
     }
 }
