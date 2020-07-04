eae900fa089f66dcd3ff6d759c3d3afe3f417b16
한대건 <han15050424@gmail.com>
2020-07-04 17:29:31 +0900

    일정한 시간이 지났는지의 여부를 판단하는 Timer클래스를 추가한다
    Timer클래스는 자신이 SetTimer함수를 통해 지정한 시간이 지났는지의 여부를 알려주는..도구? 비슷한 것이다.
    
    SetTimer함수로 시간을 지정하고, IsOver함수를 통해 지정한 시간이 지났는지의 여부를 반환받는다. 이때 주의해야할 점! IsOver함수는
    코루틴이 아니기 때문에, 계속해서 실행해주어야 한다!

---
+++ Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float mWaitTime    = 0.0f;
+    private float mWaitTimeSum = 0.0f;
+
+    public void SetTimer(float waitTime) => mWaitTime = waitTime;
+
+    public bool IsOver()
+    {
+        mWaitTimeSum += Time.deltaTime * Time.timeScale;
+
+        if (mWaitTimeSum >= mWaitTime)
+        {
+            mWaitTimeSum = 0.0f; return true;
+        }
+        return false;
+    }
+}
