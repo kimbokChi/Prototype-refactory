70a9cb9da9243bce986b98710fcadb6441a7ccfb
한대건 <han15050424@gmail.com>
2020-07-25 12:02:07 +0900

    아이템은 자신의 스프라이트를 반환하는 Getter형 변수 Sprite를 추가한다
    Sprite변수는 mSprite를 반환하는 Getter형 변수로, mSprite가 비어있을(값이 null) 경우에는 아이템 자신의 스프라이트 렌더러의 값을 통해서mSprite에 값을 대입한다.
    이 변수를 추가하는 이유는..이후에 추가할 인벤토리창에서는 인벤토리에 있는 아이템을 시각적으로 보여주어야 하기 때문에, 아이템 각각이 자신의 스프라이트를 갖게 하기 위해서 그렇다.

--- Assets/Item/Item.cs
+++ Assets/Item/Item.cs
@@ -10,6 +10,22 @@ public abstract class Item : MonoBehaviour
     }
     protected float mWeaponRange = 0.0f;
 
+    public    Sprite  Sprite
+    {
+        get
+        {
+            if (mSprite == null)
+            {
+                if (TryGetComponent(out SpriteRenderer renderer))
+                {
+                    mSprite = renderer.sprite;
+                }
+            }
+            return mSprite;
+        }
+    }
+    protected Sprite mSprite;
+
     public abstract void Init();
 
     public abstract void AccessoryUse(ITEM_KEYWORD keyword);
