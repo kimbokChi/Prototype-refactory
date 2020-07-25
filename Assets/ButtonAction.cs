098cb83080a847caf131847dbb677dabcdf9533b
한대건 <han15050424@gmail.com>
2020-07-25 12:39:17 +0900

    인벤토리창을 제어할 수 있는 기능을 추가한다
    인벤토리창을 활성화/비활성화할 수 있는 버튼을 추가한다. 버튼을 누를때마다 인벤토리창이 활성화/비활성화된다.
    
    이 기능은 ButtonAction스크립트를 통해서 구현한다. ButtonAction이 포함하는 함수 ActiveSwitching은 인스펙터 창에서 지정한 SwitchingObject의 활성화 여부를
    SwitchingObject의 활성화 여부의 반대값으로 설정하는 기능을 한다.

---
+++ Assets/ButtonAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour
+{
+    public GameObject SwitchingObject;
+
+    public void ActiveSwitching()
+    {
+        SwitchingObject.SetActive(!SwitchingObject.activeSelf);
+    }
+}
