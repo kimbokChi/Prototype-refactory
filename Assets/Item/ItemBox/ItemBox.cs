d45c18d45fd67b9982baf983c5252c61a60e76dc
한대건 <han15050424@gmail.com>
2020-07-25 21:03:38 +0900

    아이템 상자를 추가한다
    아이템 상자와 관련된 리소스들은 ItemBox폴더안에 있다. 아이템 상자는 플레이어와 접촉하게 되면 열리는 구조로 작동한다. 만! 상자가 어마어마하게 크다
    그러니 빨리 리소스 내놔

---
+++ Assets/Item/ItemBox/ItemBox.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour, IObject
+{
+    [SerializeField] private Sprite mOpenBox;
+    
+    private Animator mAnimator;
+
+    private bool mIsOpen;
+
+    public void IInit()
+    {
+
+    }
+
+    public void IUpdate()
+    {
+        
+    }
+
+    private void Start()
+    {
+        mIsOpen = false;
+
+        if (TryGetComponent(out mAnimator))
+        {
+            mAnimator.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !mIsOpen)
+        {
+            mAnimator.enabled = true;
+
+            StartCoroutine(CR_openBox());
+        }
+    }
+
+    private IEnumerator CR_openBox()
+    {
+        for (float i = 0; i < 0.333f; i += Time.deltaTime * Time.timeScale)
+        {
+            yield return null;
+        }
+
+        if (TryGetComponent(out SpriteRenderer renderer))
+        {
+            mIsOpen = true;
+
+            renderer.sprite = mOpenBox;
+        }
+        yield break;
+    }
+}
