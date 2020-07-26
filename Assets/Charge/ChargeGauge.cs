959a3e012c0a5ab5d2a6bfc0f67547b57d03bfc5
한대건 <han15050424@gmail.com>
2020-07-26 17:36:48 +0900

    충전 게이지를 시각적으로 표시하는 ChargeGauge오브젝트와 스크립트를 추가한다
    Charge Gauge오브젝트는 반투명한 오브젝트로, 연결되는 스크립트의 함수 GaugeUp을 통해서 크기를 키운다. 이렇게 크기를 키우는 것으로,
    충전이 얼마나 진행되었는지를 시각적으로 표시한다.
    
    GaugeUp함수가 반환하는 값은 충전이 얼마나 되었는지를 0~1의 값으로 반환한다.

---
+++ Assets/Charge/ChargeGauge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChargeGauge : MonoBehaviour
+{
+    public readonly Vector3 MAX_SCALE = new Vector3(1.8f, 1.8f, 1.8f);
+
+    private float mLerpAmount = 0.0f;
+
+    public float GaugeUp(float accel)
+    {
+        mLerpAmount = Mathf.Min(mLerpAmount + (Time.deltaTime * accel), 1);
+
+        transform.localScale = Vector3.Lerp(Vector3.zero, MAX_SCALE, mLerpAmount);
+
+        return mLerpAmount;
+    }
+
+    private void OnEnable() => mLerpAmount = 0.0f;
+}
