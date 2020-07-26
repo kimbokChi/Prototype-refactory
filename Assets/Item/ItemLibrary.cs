762d91c75382e801cf7e62cdc6114e536e95a9bd
한대건 <han15050424@gmail.com>
2020-07-26 09:42:01 +0900

    아이템객체를 저장하는 싱글톤 객체 ItemLibrary를 추가한다
    ItemLibrary는 모든 아이템 개체에대한 정보를 가지고 있는 객체로, 이 객체를 통해서 특정 아이템을 검색하거나, 무작위 아이템을 불러올 수 있다.
    
    ItemLibrary에 포함되는 아이템은 인스펙터창에서 추가한 아이템들이 포함된다. 포함된 아이템들은 mItems에 저장되고, mItems에 저장된 아이템은
    게임이 시작될때 mLibrary에 저장된다.
    
    혀재 ItemLibrary를 통해 사용할 수 있는 기능은 GetRandomItem만 존재한다.  GetRandomItem은 중복되지 않은 무작위 아이템을 반환하는 함수로,
    함수를 사용할 때마다 mItems에 저장된 값이 하나씩 사라지게된다.

---
+++ Assets/Item/ItemLibrary.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ItemLibrary : Singleton<ItemLibrary>
+{
+    [SerializeField]
+    private List<Item> mItems = new List<Item>();
+
+    private Dictionary<ITEM_DATA, Item> mLibrary = new Dictionary<ITEM_DATA, Item>();
+
+
+
+    private System.Random mRandom = new System.Random();
+
+
+    private void Awake()
+    {
+        for(int i = 0; i < mItems.Count; ++i)
+        {
+            if (!mLibrary.ContainsKey(mItems[i].DATA))
+            {
+                mLibrary.Add(mItems[i].DATA, mItems[i]);
+            }
+            else
+            {
+                Debug.LogWarning($"중복된 아이템이 존재합니다. 중복된 인덱스 : {i}");
+            }
+        }
+    }
+
+    public Item GetRandomItem()
+    {
+        int index = mRandom.Next(0, mItems.Count);
+
+
+        Item randomItem = mItems[index];
+
+        mItems.Remove(mItems[index]);
+
+
+        return randomItem;
+    }
+}
