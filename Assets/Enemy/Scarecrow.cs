87f809d3cbad69a42e6f40ca75442992f599ed5c
한대건 <han15050424@gmail.com>
2020-07-27 15:05:55 +0900

    허수아비 적을 추가한다
    허수아비 적은 일정 시간마다(WAIT_FOR_MOVE)마다 상수를 통해 지정된 범위내의 무작위 지점을 향해 이동하게된다.
    이동속도가 일정하지 않은 이유는 Lerp함수를 사용해서 이동하기 때문이다. 이동속도가 들쑥날쑥하다보니 이동이 어색하게 느껴지기 때문에,
    이후의 커밋에서 이동방식을 수정할 것 같다.

---
+++ Assets/Enemy/Scarecrow.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scarecrow : MonoBehaviour, IObject
+{
+    private const float WAIT_FOR_MOVE = 1.2f;
+
+    private const float HALF_MOVE_RANGE_X = 2.5f;
+    private const float HALF_MOVE_RANGE_Y = 0.0f;
+
+    [SerializeField]
+    private float mMoveSpeed;
+
+    private Timer mWaitForMove;
+
+    private IEnumerator mEMove;
+
+    private void Start()
+    {
+        mWaitForMove = new Timer();
+
+        mWaitForMove.Start(WAIT_FOR_MOVE);
+    }
+
+    private void Update()
+    {
+        if (mWaitForMove.IsOver())
+        {
+            if (mEMove == null)
+            {
+                Vector2 movePoint;
+
+                movePoint.x = Random.Range(-HALF_MOVE_RANGE_X, HALF_MOVE_RANGE_X);
+                movePoint.y = Random.Range(-HALF_MOVE_RANGE_Y, HALF_MOVE_RANGE_Y) + transform.position.y;
+
+                if (TryGetComponent(out SpriteRenderer renderer))
+                {
+                    renderer.flipX = (movePoint.x < transform.position.x);
+                }
+                StartCoroutine(mEMove = EMove(movePoint));
+            }
+        }
+        else
+        {
+            mWaitForMove.Update();
+        }
+    }
+
+    private IEnumerator EMove(Vector2 movePoint)
+    {
+        float lerp = 0.0f;
+
+        Vector2 beginPos = transform.position;
+
+        while (lerp < 1)
+        {
+            lerp = Mathf.Min(lerp + Time.deltaTime * Time.timeScale * mMoveSpeed, 1);
+
+            transform.position = Vector2.Lerp(beginPos, movePoint, lerp);
+
+            yield return null;
+        }
+        mEMove = null;
+
+        mWaitForMove.Start(WAIT_FOR_MOVE); 
+
+        yield break;
+    }
+
+    public void IInit()
+    {
+
+    }
+    public void IUpdate()
+    {
+        
+    }
+}
