f1c220fb884a19ecd88adf7cafef6792f36e5527
한대건 <han15050424@gmail.com>
2020-08-09 13:04:38 +0900

    현재 층을 클리어할때, StageClearEvent함수를 실행하도록 한다
    StageClearEvent함수의 실행은 Castle객체가 한다. Castle은 자신이 해당 함수를 실행했는지의 여부를 기억한다.
    만약 현재 층(플레이어가 있는 층)이 클리어 되었고, StageClearEvent함수를 실행하지 않았다면 StageClearEvent함수를 실행한다.
    
    StageClearEvent함수는 3개의 인자를 사용하는데, 이 인자들은 현재 층의 가장 위에있는 방부터 가장 아래에 있는 방의 MovePoint들이다.
    이 인자들을 전달받아서 무슨 오브젝트를 생성하는 등의 이벤트를 처리한다.

--- Assets/StageEventLibrary.cs
+++ Assets/StageEventLibrary.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class StageEventLibrary : Singleton<StageEventLibrary>
 {
-    
+    public void StageClearEvent(Vector2[] TmovePoints, Vector2[] MmovePoints, Vector2[] BmovePoints )
+    {
+        // To Do . . 
+    }
 }
