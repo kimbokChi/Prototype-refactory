e0413a0889e22f660a03f96210a0aad6dd286bb5
한대건 <han15050424@gmail.com>
2020-08-09 13:42:02 +0900

    현재 층이 클리어되면 발동하는 이벤트 StageClear를 추가한다
    StageClearEvent함수는 StageClear이벤트에 담긴 함수들을 모두 실행한다. StageClear이벤트는 3개의 인자를 사용하는 함수들이 포함되며,
    이 3개의 인자는 StageClearEvent함수의 인자와 동일한 역할, 순서를 가진다.

--- Assets/StageEventLibrary.cs
+++ Assets/StageEventLibrary.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StageEventLibrary : Singleton<StageEventLibrary>
 {
+    public event Action<Vector2[], Vector2[], Vector2[]> StageClear;
+
+    private void Awake()
+    {
+        if (StageClear == null)
+        {
+            StageClear = delegate (Vector2[] T, Vector2[] M, Vector2[] B) { };
+        }
+    }
+
     public void StageClearEvent(Vector2[] TmovePoints, Vector2[] MmovePoints, Vector2[] BmovePoints )
     {
-        // To Do . . 
+        StageClear.Invoke(TmovePoints, MmovePoints, BmovePoints);
     }
 }
