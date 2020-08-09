e71d029c476837fde8701a3ce5a4eaf784bfb594
한대건 <han15050424@gmail.com>
2020-08-09 14:08:02 +0900

    아이템 상자를 생성하는 이벤트를 추가한다
    아이템 상자를 추가하는 이벤트는 현재 클리어한 층의 어느 한 지점에 아이템 상자를 생성하는 방식이다. 이렇게 생성할 아이템 상자는 인스펙터 창에서 지정한다.
    
    아이템 상자가 자신의 정보를 초기화하는 시기를 '게임을 시작할때'에서 '자신이 활성화될때'로 변경한다.
    변경하는 이유는 프리팹 원본 상자가 아이템을 할당받지 않도록 하기 위함이다. 때문에 씬에 존재하는 프리팹 원본은 비활성화된 상태로 존재한다.

--- Assets/StageEventLibrary.cs
+++ Assets/StageEventLibrary.cs
@@ -7,12 +7,39 @@ public class StageEventLibrary : Singleton<StageEventLibrary>
 {
     public event Action<Vector2[], Vector2[], Vector2[]> StageClear;
 
+    [SerializeField] private GameObject ItemAndItemBox;
+
     private void Awake()
     {
         if (StageClear == null)
         {
             StageClear = delegate (Vector2[] T, Vector2[] M, Vector2[] B) { };
         }
+        StageClear += CreateItemBox;
+    }
+
+    private void CreateItemBox(Vector2[] TopMovePoint, Vector2[] MidMovePoint, Vector2[] BotMovePoint)
+    {
+        Vector2[] selectMovePoint = new Vector2[3];
+
+        switch (UnityEngine.Random.Range(0,3))
+        {
+            case 0:
+                selectMovePoint = TopMovePoint;
+                break;
+            case 1:
+                selectMovePoint = MidMovePoint;
+                break;
+            case 2:
+                selectMovePoint = BotMovePoint;
+                break;
+        }
+
+        float xPoint = UnityEngine.Random.Range(selectMovePoint[0].x, selectMovePoint[2].x);
+
+        float yPoint = selectMovePoint[UnityEngine.Random.Range(0, 3)].y + 0.63f;
+
+        Instantiate(ItemAndItemBox, new Vector2(xPoint, yPoint), Quaternion.identity).SetActive(true);
     }
 
     public void StageClearEvent(Vector2[] TmovePoints, Vector2[] MmovePoints, Vector2[] BmovePoints )
