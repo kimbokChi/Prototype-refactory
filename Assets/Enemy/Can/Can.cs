b4c7caf62143fd7bb117eb9a2302020b4cd7c004
한대건 <han15050424@gmail.com>
2020-08-13 13:45:51 +0900

    새로운 더미 적 개체 '깡통'을 추가한다
    깡통의 그래픽 리소스와 스크립트를 추가한다. 깡통의 스크립트는 EnemyBase를 상속받는다.

---
+++ Assets/Enemy/Can/Can.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Can : EnemyBase
+{
+    public override void Damaged(float damage, GameObject attacker, out GameObject victim)
+    {
+        victim = gameObject;
+    }
+
+    public override void IInit()
+    {
+    }
+
+    public override void IUpdate()
+    {
+    }
+
+    public override void PlayerEnter(Player enterPlayer)
+    {
+        mPlayer = enterPlayer;
+    }
+
+    public override void PlayerExit()
+    {
+        mPlayer = null;
+    }
+
+    public override GameObject ThisObject()
+    {
+        return gameObject;
+    }
+
+    public override bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+}
