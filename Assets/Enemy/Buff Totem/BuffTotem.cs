d83b35b29209c1c29ae0bfa1de71b62aaf76834e
한대건 <han15050424@gmail.com>
2020-08-17 14:46:13 +0900

    범위안에 들어온 특정 오브젝트들을 감지하는 Area클래스를 추가한다
    Area클래스는 인스펙터 창에서 mSenseTags에 지정한 태그의 오브젝트들을 감지하고, 감지한
    오브젝트들을 EnterObjects함수로 반환한다.
    
    다만, Area를 사용하기 위해서는 감지할 오브젝트는 트리거가 아니어야한다!

--- Assets/Enemy/Buff Totem/BuffTotem.cs
+++ Assets/Enemy/Buff Totem/BuffTotem.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class BuffTotem : MonoBehaviour
 {
+    [SerializeField] private Area mSenseArae;
+
 
 }
---
+++ Assets/Modules/Area.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Area : MonoBehaviour
+{
+    [SerializeField]
+    private string[] mSenseTags;
+    
+    private List<GameObject> mEnterObjects;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < mSenseTags.Length; ++i)
+        {
+            if (collision.CompareTag(mSenseTags[i]))
+            {
+                mEnterObjects.Add(collision.gameObject);
+            }
+        }
+    }
+
+    private GameObject[] EnterObjects()
+    {
+        return mEnterObjects.ToArray();
+    }
+}
