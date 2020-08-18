cde4aa5af15d50f4e8503b90fb39246d64e3eca1
한대건 <han15050424@gmail.com>
2020-08-18 12:37:11 +0900

    버프 토템의 능력을 구현한다
    버프 토템은 일정 시간마다 주변에 있는 적 개체의 체력을 회복시킨다.
    여기서 말하는 '일정 시간'은 인스펙터 창에서 지정한 값이고, '주변'은 버프 토템의 자식 객체인
    Area게임오브젝트의 Collider만큼 이다.

--- Assets/Enemy/Buff Totem/BuffTotem.cs
+++ Assets/Enemy/Buff Totem/BuffTotem.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class BuffTotem : MonoBehaviour, IObject
 {
+    [SerializeField] private float mWaitCastTime;
+
     [SerializeField] private Area mSenseArae;
+
+    private Timer mWaitForCastBuff;
 
     public void IInit()
     {
-        throw new System.NotImplementedException();
+        mWaitForCastBuff = new Timer();
+
+        mWaitForCastBuff.Start(mWaitCastTime);
     }
 
     public bool IsActive()
@@ -18,6 +24,26 @@ public class BuffTotem : MonoBehaviour, IObject
 
     public void IUpdate()
     {
+        if (mWaitForCastBuff.IsOver())
+        {
+            CastBuff();
+
+            mWaitForCastBuff.Start(mWaitCastTime);
+        }
+        else
+        {
+            mWaitForCastBuff.Update();
+        }
+    }
+
+    private void CastBuff()
+    {
+        ICombat[] combats = mSenseArae.GetEnterTypeT<ICombat>();
+
+        for (int i = 0; i < combats.Length; ++i)
+        {
+            combats[i].Damaged(-20f, gameObject, out GameObject v);
+        }
     }
 
     public void PlayerEnter(Player enterPlayer) { }
