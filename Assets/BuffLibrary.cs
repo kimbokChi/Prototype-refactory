81fb046202d226ece13811cb8fbc027996363e2f
한대건 <han15050424@gmail.com>
2020-08-19 08:16:32 +0900

    CastBuff함수의 인자로 사용되는 각 버프들을 저장하는 클래스를 추가한다
    추가한 클래스의 이름은 BuffLibrary로, BUFF열거자와 대응되는 버프들의 IEnumator를 저장한다.
    CastBuff함수를 사용할때, 이곳에서 적절한 IEnumator를 사용하는 것으로 버프를 시전한다!
    
    현재 BuffLibrary에 추가한 버프는 체력 회복 버프로, 즉발로 발동하는 효과이다.

---
+++ Assets/BuffLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffLibrary : Singleton<BuffLibrary>
+{
+    public const float DEFAULT_HEAL = 15f;
+
+    public float DeltaTime => Time.deltaTime * Time.timeScale;
+
+    private IEnumerable<float> HealBuff(uint level)
+    {
+        yield return DEFAULT_HEAL * level;
+    }
+    #region READ
+    /// <summary>
+    /// 즉발 : 체력을 회복하는 버프입니다. 
+    /// <para>레벨 1당 상수DEFAULT_HEAL 만큼의 체력을 회복합니다.</para>
+    /// </summary>
+    #endregion
+    public IEnumerator<float> Heal(uint level)
+    {
+        return HealBuff(level).GetEnumerator();
+    }
+}
