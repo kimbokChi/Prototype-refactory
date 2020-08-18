af84712b66464020e633d2a7431bd078ce42d400
한대건 <han15050424@gmail.com>
2020-08-18 11:15:30 +0900

    버프 토템 스크립트는 IObject인터페이스를 구현하도록 한다
    결국 버프 토템또한 Castle-Floor-Room의 흐름에 포함되기 때문에,
    IObject인터페이스를 구현하도록 한다.

--- Assets/Enemy/Buff Totem/BuffTotem.cs
+++ Assets/Enemy/Buff Totem/BuffTotem.cs
@@ -2,9 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuffTotem : MonoBehaviour
+public class BuffTotem : MonoBehaviour, IObject
 {
     [SerializeField] private Area mSenseArae;
 
+    public void IInit()
+    {
+        throw new System.NotImplementedException();
+    }
 
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+
+    public void IUpdate()
+    {
+    }
+
+    public void PlayerEnter(Player enterPlayer) { }
+    public void PlayerExit() { }
+
+    public GameObject ThisObject() => gameObject;
 }
