7de2c51656a0233b33ec5fae790a18b39a0af77f
한대건 <han15050424@gmail.com>
2020-08-19 20:10:18 +0900

    StatTable의 주인이 되는 객체는 자신의 스탯을 제어할 수 있도록 한다
    스탯 테이블에 담긴 정보들을 제어하는 딕셔너리를 추가한다. 이 딕셔너리의 값을 기반으로
    스탯을 결정한다. 딕셔너리의 키 값은 각 능력치 변수와 대치되는 열거자로 한다.
    
    또한 스탯 테이블은 스탯을 제어할 수 있는 딕셔너리를 반환하는 GetTable함수를 포함한다.
    딕셔너리를 반환받는 객체를 제한하기 위해서, 딕셔너리를 반환 받기 위해서는 '주인 객체'의
    해시코드를 입력해야한다.
    
    이렇게 딕셔너리를 반환받음으로 하여, 스탯 테이블의 주인이되는 객체는 자신의 스탯을 제어할
    수 있게 된다. 그렇지만 아직 의문인 점은 '주인 객체에서 딕셔너리의 값을 수정했을때, 스탯
    테이블에있는 값 또한 바뀌는지'이다.

--- Assets/StatTable.cs
+++ Assets/StatTable.cs
@@ -2,8 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum STAT_ON_TABLE
+{
+    MOVESPEED
+}
+
 public class StatTable
 {
+    private int KeyCode;
+    private Dictionary<STAT_ON_TABLE, float> mStatTable;
+
+    [SerializeField]
+    private GameObject BelongObject;
+
+    private void Awake()
+    {
+        KeyCode = BelongObject.GetHashCode();
+
+        mStatTable = new Dictionary<STAT_ON_TABLE, float>();
+        mStatTable.Add(STAT_ON_TABLE.MOVESPEED, mMoveSpeed);
+    }
+
+    public bool GetTable(int keyCode, out Dictionary<STAT_ON_TABLE, float> table)
+    {
+        if (keyCode == KeyCode)
+        {
+            table = mStatTable;
+
+            return true;
+        }
+        table = null;
+
+        return false;
+    }
+
     public float  MoveSpeed
     {
         get =>  mMoveSpeed;
