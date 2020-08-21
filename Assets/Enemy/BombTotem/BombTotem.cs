9c8157fc0a7cbc82f867e5142554822780dab7db
한대건 <han15050424@gmail.com>
2020-08-21 19:28:23 +0900

    폭발 토템 클래스의 틀을 추가한다
    폭발 토템은 가까이에 플레이어가 있으면, 폭발 데미지를 입히는 토템이다. 이때 중요한 점은
    이 '폭발'은 '자폭'이 아니라는 것이다!

---
+++ Assets/Enemy/BombTotem/BombTotem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombTotem : MonoBehaviour, IObject
+{
+    [SerializeField] private float mTriggerRadius;
+
+    [SerializeField] private float mWaitForBoom;
+
+    private IEnumerator mEOnFuse;
+
+    private Player mPlayer;
+
+    public void IInit()
+    {
+    }
+
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+
+    public void IUpdate()
+    {
+        if (mPlayer != null && mEOnFuse == null)
+        {
+            Vector2 playerPos = mPlayer.transform.position;
+
+            if (Vector2.Distance(playerPos, transform.position) <= mTriggerRadius)
+            {
+                StartCoroutine(mEOnFuse = EOnFuse());
+            }
+        }
+    }
+
+    public void PlayerEnter(Player enterPlayer)
+    {
+        mPlayer = enterPlayer;
+    }
+
+    public void PlayerExit()
+    {
+        mPlayer = null;
+    }
+
+    private IEnumerator EOnFuse()
+    {
+        for (float i = 0; i < mWaitForBoom; i += Time.deltaTime * Time.timeScale) { yield return null; }
+
+        mEOnFuse = null;
+    }
+
+    public GameObject ThisObject() => gameObject;
+}
