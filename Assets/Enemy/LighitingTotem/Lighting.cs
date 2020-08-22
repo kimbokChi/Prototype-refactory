82aab710a6dcaf72aacf7f3d52c87624afd23841
한대건 <han15050424@gmail.com>
2020-08-22 12:28:17 +0900

    번게의 스크립트를 추가한다
    번게 토템을 만들기 위한 빌드업으로, 번게 토템이 소환하는 개체이다.
    번게는 자신에게 닿은 대상에게 피해를 입히는 개체이다.
    
    여기서 '닿은 대상'은 인스펙터 창에서 태그를 지정할 수 있다. 물론, 입히는 피해도 그렇고!

---
+++ Assets/Enemy/LighitingTotem/Lighting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+    [SerializeField] private float mDamage;
+
+    [SerializeField] private string[] mTargetTags;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < mTargetTags.Length; ++i)
+        {
+            if (collision.CompareTag(mTargetTags[i]))
+            {
+                if (collision.TryGetComponent(out ICombat combat))
+                {
+                    combat.Damaged(mDamage, gameObject, out GameObject v);
+                }
+            }
+        }
+    }
+}
