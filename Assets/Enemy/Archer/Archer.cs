7cd0db2a18ff02a1ce384e8c70cda7558a48f3bd
한대건 <han15050424@gmail.com>
2020-08-22 21:08:38 +0900

    원거리 공격을 하는 적 개체의 바탕이 되는 Archer스크립트를 추가한다
    근데 원거리 공격을 하는 개체가 있으면 그 공격에 사용하는 투사체도 있어야 겄지??
    바로 Projection브랜치 만들어야 겄네용 호홓~

---
+++ Assets/Enemy/Archer/Archer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : EnemyBase, IObject, ICombat
+{
+    [SerializeField]
+    private StatTable mStat;
+
+    private Dictionary<STAT_ON_TABLE, float> mStatTable;
+
+    public override StatTable Stat => mStat;
+
+    public override void Damaged(float damage, GameObject attacker, out GameObject victim)
+    {
+        victim = gameObject;
+
+        if ((mStatTable[STAT_ON_TABLE.CURHEALTH] -= damage) <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public override void IInit()
+    {
+        Debug.Assert(mStat.GetTable(gameObject.GetHashCode(), out mStatTable));
+    }
+
+    public override void IUpdate()
+    {
+    }
+
+    public override void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+    }
+
+    public override void PlayerExit(MESSAGE message)
+    {
+    }
+
+    public override void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+    public override bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+    public override GameObject ThisObject() => gameObject;
+}
