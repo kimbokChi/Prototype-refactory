ee1e057bde078e9abd66a5f20c185de9eeb42130
한대건 <han15050424@gmail.com>
2020-08-22 12:59:36 +0900

    번개를 소환하는 LightingTotem스크립트를 추가한다
    번개 토템(LightingTotem)은 인스펙터 창에서 지정한 시간마다, 인스펙터 창에서 지정한 번게를
    플레이어의 위치에 소환한다.
    
    이렇게 하여 번개 토템의 윤곽을 구현한다. 아직 보강해야할 점은 번개가 소환되는 위치가
    알맞지 않다는 것이다.

---
+++ Assets/Enemy/LighitingTotem/LightingTotem.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingTotem : MonoBehaviour, IObject
+{
+    [SerializeField] private Lighting mLighting;
+
+    [SerializeField] private float mWaitLighting;
+
+    private Player mPlayer;
+
+    private Timer mWaitForLighting;
+
+    public void IInit()
+    {
+        mWaitForLighting = new Timer();
+
+        mWaitForLighting.Start(mWaitLighting);
+    }
+
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+
+    public void IUpdate()
+    {
+        if (mPlayer != null)
+        {
+            if (mWaitForLighting.IsOver())
+            {
+                if (mPlayer.Position(out Vector2 playerPos))
+                {
+                    // LIGHTING-!
+                    Instantiate(mLighting, playerPos, Quaternion.identity);
+
+                    mWaitForLighting.Start(mWaitLighting);
+                }
+            }
+            else
+            {
+                mWaitForLighting.Update();
+            }
+        }
+    }
+
+    public void PlayerEnter(Player enterPlayer)
+    {
+        mPlayer = enterPlayer;
+    }
+
+    public void PlayerExit()
+    {
+        mPlayer = null;
+    }
+
+    public GameObject ThisObject() => gameObject;
+}
