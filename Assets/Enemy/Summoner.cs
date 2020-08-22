ba329d2c5f767885a8dcfd0942689b7810b57065
한대건 <han15050424@gmail.com>
2020-08-22 17:30:53 +0900

    무언가를 소환하는 적 개체의 기반이 되는 Summonerd스크립트를 추가한다
    이제 이 스크립트를 기반으로 허수아비 개체의 프리팹 버리언트로 개체들을 분화하는 것으로,
    유사한 패턴의 적 개체를 유연하게 추가할 수 있다.

---
+++ Assets/Enemy/Summoner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summoner : EnemyBase, IObject, ICombat
+{
+    [SerializeField]
+    private StatTable mStat;
+
+    private Dictionary<STAT_ON_TABLE, float> mStatTable;
+
+    public override StatTable Stat => mStat;
+
+    public override void Damaged(float damage, GameObject attacker, out GameObject victim)
+    {
+        victim = gameObject;
+
+        if ((mStatTable[STAT_ON_TABLE.CURHEALTH] -= damage) <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public override void IInit()
+    {
+        Debug.Assert(mStat.GetTable(gameObject.GetHashCode(), out mStatTable));
+    }
+    public override void IUpdate()
+    {
+    }
+
+    public override void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        if (message.Equals(MESSAGE.THIS_ROOM))
+        {
+            mPlayer = enterPlayer;
+        }
+    }
+    public override void PlayerExit(MESSAGE message)
+    {
+        mPlayer = null;
+    }
+
+    public override void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+    public override bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+    public override GameObject ThisObject() => gameObject;
+}
