c1c90c52f3344e652e7281cc5722353814af76e8
한대건 <han15050424@gmail.com>
2020-08-23 10:19:27 +0900

    투사체의 기반이 되는 Arrow스크립트를 추가한다
    Arrow가 작용할 개체의 태그를 인스펙터에서 지정하고, 그 개체와 충돌했을 때의 행도을
    SetTriggerAction함수를 통해서 지정할 수 있다~

---
+++ Assets/Enemy/Archer/Arrow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField]
+    private string[] mTargetTags;
+
+    private Action<ICombat> mTriggerAction;
+
+    public void SetTriggerAction(Action<ICombat> action)
+    {
+        mTriggerAction = action;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < mTargetTags.Length; ++i)
+        {
+            if (collision.CompareTag(mTargetTags[i]) && collision.TryGetComponent(out ICombat combat))
+            {
+                mTriggerAction.Invoke(combat);
+
+            }
+        }
+    }
+}
