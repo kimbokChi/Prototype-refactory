b8d3bf9c13ca48b0b80e684de497d9b8c2619e71
한대건 <han15050424@gmail.com>
2020-08-23 16:47:04 +0900

    고블린 암살자가 플레이어를 공격하도록 한다
    고블린 암살자가 공격할때 사용하는 것은, Area스크립트이다! 고블린 암살자에게 콜라이더를
    달아주는 방법도 생각해 봤는데..음..그렇게 되면 플레이어와 충돌하는 문제가 발생하거나,
    버프 토템이 주는 버프를 못 받기 때문에 Area를 컴포넌트처럼 사용하도록 한다!
    
    아무튼 아무튼, 고블린 암살자가 대쉬하던 중에 플레이어와 충돌하게 되면 플레이어게게
    피해를 '한번' 입히도록 한다!

--- Assets/Enemy/Assassin Goblin/Goblin_assassin.cs
+++ Assets/Enemy/Assassin Goblin/Goblin_assassin.cs
@@ -7,11 +7,16 @@ public class Goblin_assassin : EnemyBase, IObject, ICombat
     [SerializeField]
     private StatTable mStat;
 
+    [SerializeField]
+    private Area mContactArea;
+
     [SerializeField][Range(0f, 5f)]
     private float mMaxDashLength;
 
     private Timer mWaitForMoving;
     private Timer mWaitForATK;
+
+    private Dictionary<int, bool> mAttackedHashs;
 
     private Dictionary<STAT_ON_TABLE, float> mStatTable;
 
@@ -36,6 +41,8 @@ public class Goblin_assassin : EnemyBase, IObject, ICombat
 
     public override void IInit()
     {
+        mAttackedHashs = new Dictionary<int, bool>();
+
         Debug.Assert(mStat.GetTable(gameObject.GetHashCode(), out mStatTable));
 
         mWaitForMoving = new Timer();
@@ -111,12 +118,33 @@ public class Goblin_assassin : EnemyBase, IObject, ICombat
 
         while (lerpAmount < 1 && Vector2.Distance(initPoint, transform.localPosition) < mMaxDashLength)
         {
+            Attack();
+
             lerpAmount = Mathf.Min(1f, lerpAmount + Time.deltaTime * Time.timeScale * accel * mStat.RMoveSpeed);
 
             transform.localPosition = Vector2.Lerp(transform.localPosition, dashPoint, lerpAmount);
 
             yield return null;
         }
+        mAttackedHashs.Clear();
+
         mWaitForATK.Start(mWaitATKTime); mEDash = null;
+    }
+
+    private void Attack()
+    {
+        ICombat[] combat = mContactArea.GetEnterTypeT<ICombat>();
+
+        for (int i = 0; i < combat.Length; ++i)
+        {
+            int hash = combat[i].GetHashCode();
+
+            if (!mAttackedHashs.ContainsKey(hash))
+            {
+                mAttackedHashs.Add(hash, true);
+
+                combat[0].Damaged(Stat.RAttackPower, gameObject, out GameObject v);
+            }
+        }
     }
 }
