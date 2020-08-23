c170e537670bf8719b88c1de04f75c8403c8b67e
한대건 <han15050424@gmail.com>
2020-08-23 13:44:46 +0900

    암살자 고블린 스크립트를 추가한다
    고블린 암살자 스크립트는 Enemy/Assassin Goblin폴더에 있다.
    
    현재까지 작성된 스크립트의 내용은 "공격 못하는 허수아비"정도이다.
    이후에 추가할 내용은..플레이어를 발견하는 즉시, 플레이어를 향해 돌진하도록
    할 것이다!

---
+++ Assets/Enemy/Assassin Goblin/Goblin_assassin.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin_assassin : EnemyBase, IObject, ICombat
+{
+    [SerializeField]
+    private StatTable mStat;
+
+    private Timer mWaitForMoving;
+    private Timer mWaitForATK;
+
+    private Dictionary<STAT_ON_TABLE, float> mStatTable;
+
+    public override StatTable Stat => mStat;
+
+    public override void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+
+    public override void Damaged(float damage, GameObject attacker, out GameObject victim)
+    {
+        victim = gameObject;
+
+        if ((mStatTable[STAT_ON_TABLE.CURHEALTH] -= damage) <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public override void IInit()
+    {
+        Debug.Assert(mStat.GetTable(gameObject.GetHashCode(), out mStatTable));
+
+        mWaitForMoving = new Timer();
+        mWaitForATK    = new Timer();
+
+        mWaitForATK.Start(mWaitATKTime);
+    }
+
+    public override bool IsActive() => gameObject.activeSelf;
+
+    public override void IUpdate()
+    {
+        if (mWaitForMoving.IsOver())
+        {
+            if (IsMoveFinish && !IsInReachPlayer())
+            {
+                Vector2 movePoint;
+
+                movePoint.x = Random.Range(-mHalfMoveRangeX, mHalfMoveRangeX) + mOriginPosition.x;
+                movePoint.y = Random.Range(-mHalfMoveRangeY, mHalfMoveRangeY) + mOriginPosition.y;
+
+                if (mPlayer != null)
+                {
+                    Vector2 lookingDir = movePoint.x > transform.localPosition.x ? Vector2.right : Vector2.left;
+
+                    Vector2 playerPos;
+
+                    if ((IsLookAtPlayer(lookingDir) || IsLookAtPlayer()) && mPlayer.Position(out playerPos))
+                    {
+                        movePoint = PositionLocalized(playerPos);
+
+                        if (!IsPointOnRange(movePoint))
+                        {
+                            movePoint -= (movePoint.x > transform.localPosition.x ? Vector2.right : Vector2.left) * mRange;
+
+                        }
+                    }
+                }
+                MoveToPoint(movePoint);
+            }
+        }
+        else
+        {
+            mWaitForMoving.Update();
+        }
+
+        if (IsInReachPlayer())
+        {
+            if (mWaitForATK.IsOver())
+            {
+
+                // To do . . .
+            }
+            else
+            {
+                mWaitForATK.Update();
+            }
+        }
+    }
+
+    public override void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        if (message.Equals(MESSAGE.THIS_ROOM)) mPlayer = enterPlayer;
+    }
+
+    public override void PlayerExit(MESSAGE message)
+    {
+        mPlayer = null;
+    }
+
+    public override GameObject ThisObject() => gameObject;
+}
