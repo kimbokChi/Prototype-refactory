7c8254874c97d6f89a0811ac5cc04215b72d6122
한대건 <han15050424@gmail.com>
2020-08-24 19:02:58 +0900

    보스 개체인 고블린 족장의 스크립트를 추가한다
    현재 만들고 있는 그 뭐냐 스테이지의 보스인, 고블린 족장 스크립트를 추가한다!
    고블린 족장 스크립트의 이름(Chief)앞에 고블린이라는 말이 붙지 않는 이유는,
    나중에 고블린 개체들은 Goblin이라는 네임 스페이스에 묶어둘 계획이라서 그렇다
    앓~

---
+++ Assets/Enemy/Boss/Chief of goblin clan/Chief.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chief : EnemyBase, IObject, ICombat
+{
+    private Timer mWaitForATK;
+    private Timer mWaitForMove;
+
+    [SerializeField] private StatTable mStat;
+
+    private Dictionary<STAT_ON_TABLE, float> mStatTable;
+
+    public override StatTable Stat => mStat;
+
+    public override void Damaged(float damage, GameObject attacker, out GameObject victim)
+    {
+        victim = gameObject;
+
+        if ((mStatTable[STAT_ON_TABLE.CURHEALTH] -= damage) <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public override void IInit()
+    {
+        Debug.Assert(Stat.GetTable(gameObject.GetHashCode(), out mStatTable));
+
+        mWaitForATK  = new Timer();
+        mWaitForMove = new Timer();
+
+        mWaitForATK.Start(mWaitATKTime);
+    }
+
+    public override bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+
+    public override void IUpdate()
+    {
+    }
+
+    public override void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        mPlayer = enterPlayer;
+    }
+
+    public override void PlayerExit(MESSAGE message)
+    {
+        if (message.Equals(MESSAGE.BELONG_FLOOR))
+        {
+            mPlayer = null;
+        }
+    }
+
+    public override void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+
+    public override GameObject ThisObject() => gameObject;
+}
