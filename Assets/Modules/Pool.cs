113c1ab3fb6352812831c504a850e5261509fa39
한대건 <han15050424@gmail.com>
2020-08-26 13:14:07 +0900

    오브젝트 풀링을 할 수 있는 Pool클래스를 추가한다
    풀 클래스를 통해 오브젝트를 유연하게 재사용 할 수 있도록 한다.
    지금 구현된 기능은 풀에 오브젝트를 넣고 빼는 기본적인 풀의 기능과,
    그 오브젝트가 풀에 들어올 때 실행될 함수또한 지정할 수 있다.

---
+++ Assets/Modules/Pool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> : MonoBehaviour
+{
+    private Stack<T> mIsntances;
+
+    private Action<T> InstanceResetMethod;
+
+    public void Init(Action<T> instanceResetMethod)
+    {
+        mIsntances = new Stack<T>();
+
+        InstanceResetMethod = instanceResetMethod;
+    }
+
+    public void Add(T instance)
+    {
+        mIsntances.Push(instance);
+
+        InstanceResetMethod.Invoke(instance);
+    }
+
+    public T Pop() => mIsntances.Pop();
+}
