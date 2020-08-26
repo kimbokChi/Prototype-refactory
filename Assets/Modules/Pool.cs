77389301f350340eefb318a4d3101fd7722ad290
한대건 <han15050424@gmail.com>
2020-08-26 13:53:22 +0900

    풀에 특정 조건에 부합하는 오브젝트가 있는지를 검사하는 기능을 추가한다
    여기서 말하는 '특정 조건'은 Init함수를 통해서 선택적으로 지정이 가능하다!

--- Assets/Modules/Pool.cs
+++ Assets/Modules/Pool.cs
@@ -8,12 +8,36 @@ public class Pool<T> : MonoBehaviour
     private Stack<T> mIsntances;
 
     private Action<T> InstanceResetMethod;
+    private Func<T, bool> InstanceFilter;
 
-    public void Init(Action<T> instanceResetMethod = null)
+    public void Init(Action<T> instanceResetMethod = null, Func<T, bool> instanceFilter = null)
     {
         mIsntances = new Stack<T>();
 
         InstanceResetMethod = instanceResetMethod;
+
+        InstanceFilter = instanceFilter;
+    }
+
+    public bool OnInstanceFilter(out T[] instances)
+    {
+        if (InstanceFilter == null)
+        {
+            instances = null; return false;
+
+        }
+        List<T> filteringThings = new List<T>();
+
+        foreach (var item in mIsntances)
+        {
+            if (InstanceFilter.Invoke(item))
+            {
+                filteringThings.Add(item);
+            }
+        }
+        instances = filteringThings.ToArray();
+
+        return instances.Length > 0;
     }
 
     public void Add(T instance)
