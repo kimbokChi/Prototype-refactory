6bccd241f152bbf3c09fae3f5fc3f246cf04c6d0
한대건 <han15050424@gmail.com>
2020-08-26 09:26:37 +0900

    십자방향 토템이 총탄을 발사하는 타입을 인스펙터에서 지정할 수 있도록 한다.
    mShootingType값을 조정하여, 해당 토템이 발사하는 방향을 지정할 수 있다.

--- Assets/Enemy/CrossTotem/CrossTotem.cs
+++ Assets/Enemy/CrossTotem/CrossTotem.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CrossTotem : MonoBehaviour, IObject, ICombat
 {
+    public enum SHOOTING_TYPE
+    {
+        CROSS, XSHAPE
+    }
+
+    [SerializeField] private SHOOTING_TYPE mShootingType;
+
     [SerializeField] private StatTable mStat;
 
     [SerializeField] private Arrow mDartOrigin;
@@ -56,8 +63,37 @@ public class CrossTotem : MonoBehaviour, IObject, ICombat
             {
                 Arrow arrow = Instantiate(mDartOrigin, transform.position, Quaternion.identity);
 
-                arrow.Setting(mDartSpeed, new Vector2(Mathf.Cos((45f + 90f * i) * Mathf.Deg2Rad), Mathf.Sin((45f + 90f * i) * Mathf.Deg2Rad)));
+                switch (mShootingType)
+                {
+                    case SHOOTING_TYPE.CROSS:
+                        switch (i)
+                        {
+                            case 0:
+                                arrow.Setting(mDartSpeed, Vector2.left);
+                                break;
 
+                            case 1:
+                                arrow.Setting(mDartSpeed, Vector2.right);
+                                break;
+
+                            case 2:
+                                arrow.Setting(mDartSpeed, Vector2.down);
+                                break;
+
+                            case 3:
+                                arrow.Setting(mDartSpeed, Vector2.up);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+
+                    case SHOOTING_TYPE.XSHAPE:
+                        float rotation = (45f + 90f * i) * Mathf.Deg2Rad;
+
+                        arrow.Setting(mDartSpeed, new Vector2(Mathf.Cos(rotation), Mathf.Sin(rotation)));
+                        break;
+                }
                 arrow.Setting(Arrow_targetHit, Arrow_canDistroy);
             }
             mWaitForShoot.Start(mWaitNextShoot);
