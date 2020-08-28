e799d6f0705b147cc535aeaa4698cfcba27a61cb
한대건 <han15050424@gmail.com>
2020-08-28 19:58:52 +0900

    자폭 토템 스크립트를 추가한다
    스크립트의 이름은 SuicideTotem으로, 위치는 Enemy/Totems/TotemSuicide에 있다.
    자폭 토템은 플레이어를 감지하게되면 도화선이 활성화되고, 도화선이 끝나게되면
    자폭한다!

---
+++ Assets/Enemy/Totems/TotemSuicide/SuicideTotem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicideTotem : MonoBehaviour, IObject, ICombatable
+{
+    [SerializeField] private StatTable mStat;
+
+    private Dictionary<STAT_ON_TABLE, float> mStatTable;
+
+    public StatTable Stat
+    { get => mStat; }
+
+    public void CastBuff(BUFF buffType, IEnumerator castedBuff) => StartCoroutine(castedBuff);
+
+    public void Damaged(float damage, GameObject attacker)
+    {
+        if ((mStatTable[STAT_ON_TABLE.CURHEALTH] -= damage) <= 0) {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void IInit()
+    {
+        Debug.Assert(mStat.GetTable(gameObject.GetHashCode(), out mStatTable));
+    }
+
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+
+    public void IUpdate()
+    {
+        
+    }
+
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        
+    }
+
+    public void PlayerExit(MESSAGE message)
+    {
+        
+    }
+
+    public GameObject ThisObject() => gameObject;
+}
