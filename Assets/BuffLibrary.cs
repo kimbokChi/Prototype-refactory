1cdbc875432fa7cb428c334f1395d49fdae9455f
한대건 <han15050424@gmail.com>
2020-09-01 18:34:42 +0900

    버프를 반환하는 함수의 수를 줄인다
    기존의 경우 각 버프마다 함수를 할당하는 방식이었다. 이 방식은 버프를 하나 추가할 대마다
    새로운 버프 함수를 추가해줘야 한다는 수고가 따르는 데다가 너무 반복적인 작업이었다.
    
    이러한 방식의 문제점을 두개의 함수를 추가하는 것으로 해결한다!
    추가한 두개의 함수는 각각 지속 버프롸 즉발 버프를 반환하는 함수로, 인자로 지정한 BUFF열거자와
    대치되는 버프를 반환하도록 한다!

--- Assets/BuffLibrary.cs
+++ Assets/BuffLibrary.cs
@@ -16,17 +16,7 @@ public class BuffLibrary : Singleton<BuffLibrary>
 
         yield break;
     }
-    #region READ
-    /// <summary>
-    /// 즉발 : 체력을 회복하는 버프입니다. 
-    /// <para>레벨 1당 HEAL만큼의 체력을 회복합니다.</para>
-    /// </summary>
-    #endregion
-    public  IEnumerator Heal    (uint level, AbilityTable ability)
-    {
-        return HealBuff(level, ability).GetEnumerator();
-    }
-
+   
     private IEnumerable SpeedUpBuff(float durate, uint level, AbilityTable ability)
     {
         ability.IMoveSpeed += ability.MoveSpeed * level * SPEEDUP;
@@ -35,17 +25,7 @@ public class BuffLibrary : Singleton<BuffLibrary>
 
         ability.IMoveSpeed -= ability.MoveSpeed * level * SPEEDUP;
     }
-    #region READ
-    /// <summary>
-    /// 지속 : 이동속도를 증가시키는 버프입니다.
-    /// <para>레벨 1당 SPEEDUP만큼의 이동속도 증가치를 반환합니다.</para>
-    /// </summary>
-    #endregion
-    public  IEnumerator SpeedUp    (float durate, uint level, AbilityTable ability)
-    {
-        return SpeedUpBuff(durate, level, ability).GetEnumerator();
-    }
-
+  
     private IEnumerable PowerBoostBuff(float durate, uint level, AbilityTable ability)
     {
         ability.IAttackPower += ability.AttackPower * level * POWER_BOOST;
@@ -54,13 +34,42 @@ public class BuffLibrary : Singleton<BuffLibrary>
 
         ability.IAttackPower -= ability.AttackPower * level * POWER_BOOST;
     }
+    
     #region READ
     /// <summary>
-    /// 지속 : 공격력을 증가시키는 버프입니다.
+    /// 즉발 : 지속시간 없이, 시전된 즉시 효과를 발휘하는 버프를 반환합니다.
     /// </summary>
     #endregion
-    public IEnumerator PowerBoost(float durate, uint level, AbilityTable ability)
+    public IEnumerator GetBurstBUFF(BUFF buff,uint level, AbilityTable ability)
     {
-        yield return PowerBoostBuff(durate, level, ability);
+        switch (buff)
+        {
+            case BUFF.HEAL:
+                return HealBuff(level, ability).GetEnumerator();
+
+            default:
+                Debug.Log($"The {buff} is not burst BUFF");
+                return null;
+        }
+    }
+    #region READ
+    /// <summary>
+    /// 지속 : 지속시간동안 효과를 발휘하는 버프를 반환합니다.
+    /// </summary>
+    #endregion
+    public IEnumerator GetSlowBUFF(BUFF buff, uint level, float durateTime, AbilityTable ability)
+    {
+        switch (buff)
+        {
+            case BUFF.POWER_BOOST:
+                return PowerBoostBuff(durateTime, level, ability).GetEnumerator();
+
+            case BUFF.SPEEDUP:
+                return SpeedUpBuff(durateTime, level, ability).GetEnumerator();
+
+            default:
+                Debug.Log($"The {buff} is not slow BUFF");
+                return null;
+        }
     }
 }
