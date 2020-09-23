5555519dad8cb3337cd6fa7c4b579758a9a7f51d
한대건 <han15050424@gmail.com>
2020-09-23 17:45:19 +0900

    AbilityTable을 리팩토링한다
    열거자 STAT_ON_TABLE을 Ability으로 이름을 변경하고, 열거자의 요소들의 이름에 Pascal Casing을 사용한다.
    
    변수의 이름을 변경하며, 몇가지 능력치를 불러오는데 사용되는 변수들을 제거했다. 이제 능력치를 불러오는 것은
    public 컬렉션 Table을 통해서 능력치를 불러오고, 설정할 수 있다.
    
    그리고 Awake함수를 통해 초기화를 진행하는 대신, Init함수를 사용하여 초기화를 진행하도록 한다.
    Init함수의 호출 시점은 Table의 값을 불러올때, 반환하는 값인 mTable의 값이 null일때이다.
    Awake함수대신 Init함수를 사용하는 이유는 게으른 초기화를 유도하여 부하를 나름대로 줄이기 위함이다.

--- Assets/AbilityTable.cs
+++ Assets/AbilityTable.cs
@@ -2,100 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum STAT_ON_TABLE
+public enum Ability
 {
-    MOVESPEED, IMOVESPEED,
-    CURHEALTH, MAXHEALTH,
-    ATTACK_POWER, IATTACK_POWER
+    MoveSpeed, IMoveSpeed, CurHealth, MaxHealth,
+    AttackPower, IAttackPower, End
 }
-
 public class AbilityTable : MonoBehaviour
 {
-    private int KeyCode;
-    private Dictionary<STAT_ON_TABLE, float> mPersonalTable;
-
-    [SerializeField] private GameObject BelongObject;
-
-    private void Reset() {
-        BelongObject = transform.parent.gameObject;
-    }
-
-    private void Awake()
+    public  Dictionary<Ability, float>  Table
     {
-        KeyCode = BelongObject.GetHashCode();
-
-        mPersonalTable = new Dictionary<STAT_ON_TABLE, float>();
-
-        float increase = 0f;
-
-        mPersonalTable.Add(STAT_ON_TABLE. MOVESPEED, mMoveSpeed);
-        mPersonalTable.Add(STAT_ON_TABLE.IMOVESPEED, increase);
-
-        mPersonalTable.Add(STAT_ON_TABLE.CURHEALTH, mMaxHealth);
-        mPersonalTable.Add(STAT_ON_TABLE.MAXHEALTH, mMaxHealth);
-
-        mPersonalTable.Add(STAT_ON_TABLE. ATTACK_POWER, mAttackPower);
-        mPersonalTable.Add(STAT_ON_TABLE.IATTACK_POWER, increase);
-    }
-
-    public bool GetTable(int keyCode, out Dictionary<STAT_ON_TABLE, float> table)
-    {
-        if (keyCode == KeyCode) {
-            table = mPersonalTable; return true;
-        }
-        table = null;
-
-        return false;
-    }
-
-    // MoveSpeed
-    public float  MoveSpeed
-    {
-        get => mPersonalTable[STAT_ON_TABLE.MOVESPEED];
-    }
-    public float IMoveSpeed
-    {
-        get => mPersonalTable[STAT_ON_TABLE.IMOVESPEED];
-        set => mPersonalTable[STAT_ON_TABLE.IMOVESPEED] = value;
-    }
-    public float RMoveSpeed
-    {
-        get => mPersonalTable[STAT_ON_TABLE.MOVESPEED] + mPersonalTable[STAT_ON_TABLE.IMOVESPEED];
-    }
-
-    // Health
-    public float CurHealth
-    {
-        get => mPersonalTable[STAT_ON_TABLE.CURHEALTH];
-        set
+        get
         {
-            float curHealth = mPersonalTable[STAT_ON_TABLE.CURHEALTH];
+            if (mTable == null) { Init(); }
 
-            mPersonalTable[STAT_ON_TABLE.CURHEALTH] = Mathf.Min(curHealth, curHealth + value);
+            return mTable;
         }
     }
-    public float MaxHealth
-    {
-        get => mPersonalTable[STAT_ON_TABLE.MAXHEALTH];
-    }
+    private Dictionary<Ability, float> mTable;
 
-    // AttackPower
-    public float  AttackPower
+    private void Init()
     {
-        get => mPersonalTable[STAT_ON_TABLE.ATTACK_POWER];
-    }
-    public float IAttackPower
-    {
-        get => mPersonalTable[STAT_ON_TABLE.IATTACK_POWER];
-        set => mPersonalTable[STAT_ON_TABLE.IATTACK_POWER] = value;
-    }
-    public float RAttackPower
-    {
-        get => mPersonalTable[STAT_ON_TABLE.IATTACK_POWER] +
-               mPersonalTable[STAT_ON_TABLE.ATTACK_POWER];
-    }
+        mTable = new Dictionary<Ability, float>();
 
-    [SerializeField] private float mMoveSpeed;
-    [SerializeField] private float mMaxHealth;
-    [SerializeField] private float mAttackPower;
+        for (Ability i = 0; i < Ability.End; ++i)
+        {
+            switch (i)
+            {
+                case Ability.CurHealth:
+                case Ability.MaxHealth:
+                    mTable.Add(i, MaxHealth);
+                    break;
+                case Ability.AttackPower:
+                    mTable.Add(i, AttackPower);
+                    break;
+                default:
+                    mTable.Add(i, default);
+                    break;
+            }
+        }
+    }
+    [SerializeField] private float MoveSpeed;
+    [SerializeField] private float MaxHealth;
+    [SerializeField] private float AttackPower;
 }
