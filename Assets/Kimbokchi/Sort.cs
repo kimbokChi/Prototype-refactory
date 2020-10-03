5a01213668ec3a44830049abf3be0413a6df21a9
한대건 <han15050424@gmail.com>
2020-10-03 13:24:14 +0900

    Kimbokchi패키지를 임포트한다
    짱짱좋은 기능들을 다량 포함한 최강의 패키지 Kimbokchi를 임포트하여 프로젝트의 번영을 기원한다 앓~~~

---
+++ Assets/Kimbokchi/Sort.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kimbokchi
+{
+    public static class Sort
+    {
+        public static void MergeSort<T>(this T[] array, Func<T, T, bool> IsRValueBigger)
+        {
+            MergeSort(array, 0, array.Length - 1, new T[array.Length], IsRValueBigger);
+        }
+        public static void MergeSort<T>(this T[] array, int min, int max, T[] tempArray, Func<T, T, bool> IsRValueBigger)
+        {
+            if (min >= max) return;
+
+            int mid = (min + max) / 2;
+
+            MergeSort(array, min,     mid, tempArray, IsRValueBigger);
+            MergeSort(array, mid + 1, max, tempArray, IsRValueBigger);
+
+            int i = min;
+            int j = mid + 1;
+
+            for (int k = min; k <= max; k++)
+            {
+                if (i > mid)
+                {
+                    tempArray[k] = array[j++];
+                }
+                else if (j > max)
+                {
+                    tempArray[k] = array[i++];
+                }
+                else if(IsRValueBigger(array[i], array[j]))
+                {
+                    tempArray[k] = array[i++];
+                }
+                else
+                {
+                    tempArray[k] = array[j++];
+                }
+            }
+            for (i = min; i <= max; i++) array[i] = tempArray[i];
+        }
+        public static void MergeSort<T>(this List<T> list, Func<T, T, bool> IsRValueBigger)
+        {
+            MergeSort(list, 0, list.Count - 1, new List<T>(list), IsRValueBigger);
+        }
+        public static void MergeSort<T>(this List<T> list, int min, int max, List<T> tempList, Func<T, T, bool> IsRValueBigger)
+        {
+            if (min >= max) return;
+
+            int mid = (min + max) / 2;
+
+            MergeSort(list, min,     mid, tempList, IsRValueBigger);
+            MergeSort(list, mid + 1, max, tempList, IsRValueBigger);
+
+            int i = min;
+            int j = mid + 1;
+
+            for (int k = min; k <= max; k++)
+            {
+                if (i > mid)
+                {
+                    tempList[k] = list[j++];
+                }
+                else if (j > max)
+                {
+                    tempList[k] = list[i++];
+                }
+                else if (IsRValueBigger(list[i], list[j]))
+                {
+                    tempList[k] = list[i++];
+                }
+                else
+                {
+                    tempList[k] = list[j++];
+                }
+            }
+            for (i = min; i <= max; i++) list[i] = tempList[i];
+        }
+
+        public static void SelectionSort<T>(this T[] array, Func<T, T, bool> IsRValueBigger)
+        {
+            int i, j, minIndex, length = array.Length;
+
+            for (i = 0; i < length - 1; ++i)
+            {
+                minIndex = i;
+
+                for (j = i + 1; j < length; ++j)
+                {
+                    if (IsRValueBigger(array[j], array[minIndex]))
+                    {
+                        minIndex = j;
+                    }
+                }
+                array.Swap(minIndex, i);
+            }
+        }
+        public static void SelectionSort<T>(this List<T> list, Func<T, T, bool> IsRValueBigger)
+        {
+            int i, j, minIndex, length = list.Count;
+
+            for (i = 0; i < length - 1; ++i)
+            {
+                minIndex = i;
+
+                for (j = i + 1; j < length; ++j)
+                {
+                    if (IsRValueBigger(list[j], list[minIndex]))
+                    {
+                        minIndex = j;
+                    }
+                }
+                list.Swap(minIndex, i);
+            }
+        }
+
+        public static void InsertionSort<T>(this T[] array, Func<T, T, bool> IsRValueBigger)
+        {
+            T remember;
+
+            int i, j;
+
+            for (i = 1; i < array.Length; i++)
+            {
+                remember = array[j = i];
+
+                while (--j >= 0 && IsRValueBigger(remember, array[j]))
+                {
+                    array[j + 1] = array[j];
+                    array[j] = remember;
+                }
+            }
+        }
+        public static void InsertionSort<T>(this List<T> list, Func<T, T, bool> IsRValueBigger)
+        {
+            T remember;
+
+            int i, j;
+
+            for (i = 1; i < list.Count; i++)
+            {
+                remember = list[j = i];
+
+                while (--j >= 0 && IsRValueBigger(remember, list[j]))
+                {
+                    list[j + 1] = list[j];
+                    list[j] = remember;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Kimbokchi/Utility.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kimbokchi
+{
+    public static class Utility
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a; a = b; b = temp;
+        }
+
+        public static void Swap<T>(this T[] array, int indexA, int indexB)
+        {
+            T temp = array[indexA]; array[indexA] = array[indexB]; array[indexB] = temp;
+        }
+
+        public static void Swap<T>(this List<T> list, int indexA, int indexB)
+        {
+            T temp = list[indexA]; list[indexA] = list[indexB]; list[indexB] = temp;
+        }
+
+        public static int LuckyNumber(params float[] probablities)
+        {
+            float sum = 0f;
+
+            float lucky = (float)new Random().NextDouble();
+
+            for (int i = 0; i < probablities.Length; ++i)
+            {
+                sum += probablities[i];
+
+                if (lucky <= sum) return i;
+            }
+            return -1;
+        }
+    }
+
+    public class LuckyBox<T>
+    {
+        private Action<LuckyBox<T>> mEmptyItemList;
+
+        private Dictionary<float, List<T>> mItemTableOrigin;
+        private Dictionary<float, List<T>> mItemTable;
+
+        private float mComplement;
+
+        private Random mRandom;
+
+        public LuckyBox(Action<LuckyBox<T>> emptyItemListAction = null)
+        {
+            mComplement = 0f;
+
+            mEmptyItemList = emptyItemListAction ?? new Action<LuckyBox<T>>(o => o.Refill());
+
+            mItemTableOrigin = new Dictionary<float, List<T>>();
+            mItemTable       = new Dictionary<float, List<T>>();
+
+            mRandom = new Random();
+        }
+        public void AddItem(float probablity, params T[] itemList)
+        {
+            if (!mItemTableOrigin.ContainsKey(probablity))
+            {
+                mItemTableOrigin.Add(probablity, new List<T>());
+                mItemTable      .Add(probablity, new List<T>());
+            }
+            for (int i = 0; i < itemList.Length; ++i)
+            {
+                mItemTableOrigin[probablity].Add(itemList[i]);
+                mItemTable      [probablity].Add(itemList[i]);
+            }
+        }
+        public void Refill()
+        {
+            mComplement = 0f;
+
+            foreach (var item in mItemTableOrigin)
+            {                
+                if (item.Value.Count > mItemTable[item.Key].Count)
+                {
+                    item.Value.ForEach(o => mItemTable[item.Key].Add(o));
+                }
+            }
+        }
+        public T RandomItem()
+        {
+            float sum = 0f;
+            float probablity = (float)mRandom.NextDouble();
+
+            if (mItemTable.All(o => o.Value.Count == 0))
+            {
+                mEmptyItemList?.Invoke(this);
+            }
+            foreach (var item in mItemTable)
+            {
+                if (item.Value.Count == 0) continue;
+
+                sum += item.Key + mComplement;
+
+                if (probablity <= sum)
+                {
+                    int index = mRandom.Next(0, item.Value.Count);
+
+                    T value = item.Value         [index];
+                              item.Value.RemoveAt(index);
+
+                    if (item.Value.Count == 0)
+                    {
+                        mComplement += (item.Key + mComplement) / mItemTable.Count(o => o.Value.Count > 0);
+                    }
+                    return value;
+                }
+            }
+            return default;
+        }
+    }
+}
