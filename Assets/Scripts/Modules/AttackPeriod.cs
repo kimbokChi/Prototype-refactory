8e3b5445d9a8666f04c64b77163e6df256ac178a
한대건 <han15050424@gmail.com>
2020-10-05 08:44:33 +0900

    AttackPeriod를 하나의 파일로 분리한다
    파일의 위치는 Modules폴더이다.

---
+++ Assets/Scripts/Modules/AttackPeriod.cs
@@ -0,0 +1,52 @@
+﻿using System;
+public class AttackPeriod
+{
+    private enum Period { Begin, After };
+
+
+    private Period mWaitPeriod;
+
+    private Timer mTimer;
+
+    private Action mBeginOverAction;
+    private Action mAfterOverAction;
+
+    private AbilityTable mAbilityTable;
+
+
+    public AttackPeriod(AbilityTable abilityTable,
+        Action beginOverAction = null,
+        Action afterOverAction = null)
+    {
+        mWaitPeriod = Period.Begin;
+
+        (mTimer = new Timer()).Start(abilityTable.AfterAttackDelay);
+
+        mAbilityTable = abilityTable;
+
+        mBeginOverAction = beginOverAction;
+        mAfterOverAction = afterOverAction;
+    }
+    public void Update()
+    {
+        mTimer.Update();
+
+        if (mTimer.IsOver())
+        {
+            if (mWaitPeriod.Equals(Period.Begin))
+            {
+                mBeginOverAction?.Invoke();
+
+                mWaitPeriod = Period.After;
+                mTimer.Start(mAbilityTable.AfterAttackDelay);
+            }
+            else
+            {
+                mAfterOverAction?.Invoke();
+
+                mWaitPeriod = Period.Begin;
+                mTimer.Start(mAbilityTable.BeginAttackDelay);
+            }
+        }
+    }
+}
