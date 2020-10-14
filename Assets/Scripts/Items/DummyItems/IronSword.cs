4fafe54921695637115ffef0f549504db04eed23
한대건 <han15050424@gmail.com>
2020-10-15 08:33:55 +0900

    철검 아이템을 추가한다
    철검 아이템의 프리팹과, 스크립트를 추가하며 ItemLibrary에 추가한다.

---
+++ Assets/Scripts/Items/DummyItems/IronSword.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class IronSword : Item
+{
+    public override ItemRating Rating => ItemRating.Epic;
+
+    public override float WeaponRange => 1.5f;
+
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        switch (offSlot)
+        {
+            case SlotType.Accessory:
+                Inventory.Instance.ChargeAction -= ChargeAction;
+                break;
+
+            case SlotType.Weapon:
+                Inventory.Instance.AttackAction -= AttackAction;
+                break;
+        }
+    }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        switch (onSlot)
+        {
+            case SlotType.Accessory:
+                Inventory.Instance.ChargeAction += ChargeAction;
+                break;
+
+            case SlotType.Weapon:
+                Inventory.Instance.AttackAction += AttackAction;
+                break;
+        }
+    }
+
+    private void ChargeAction(float charge)
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        GameObject player = FindObjectOfType(typeof(Player)) as GameObject;
+
+        for (int i = 0; i < enemies.Length; ++i)
+        {
+            if (enemies[i].activeSelf)
+            {
+                if (enemies[i].TryGetComponent(out ICombatable combatable)) {
+                    combatable.Damaged(charge * 8f, player);
+                }
+            }
+        }
+    }
+
+    private void AttackAction(GameObject attacker, ICombatable combat)
+    {
+        combat.Damaged(5f, attacker);
+    }
+}
