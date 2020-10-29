7845b4895c1676a4e95d03b25754f7bd5cbe6bed
한대건 <han15050424@gmail.com>
2020-10-29 14:11:17 +0900

    체력바 UI에 사용되는 HealthBar스크립트를 추가한다
    HealthBar스크립트를 사용하는 게임오브젝트는 인스펙터에서 지정한 대상과 지정한 간격을 두고서 따라다니며,
    대상의 남은 체력의 비율을 표시하는 기능을 한다.
    
    현재 이러한 체력바를 사용하는 개체는 ItemTest씬의 플레이어 뿐이다. 다른 개체들도 플레이어와 같이
    사용할 것이다.

---
+++ Assets/Scripts/Modules/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Vector3 Offset;
+    [SerializeField] private Image FillHealthBarImage;
+
+    [Header("Master Info")]
+    [SerializeField] private Transform MasterTransform;
+    [SerializeField] private AbilityTable AbilityTable;
+
+    private void LateUpdate()
+    {
+        transform.position = MasterTransform.position + Offset;
+
+        FillHealthBarImage.fillAmount = 
+            AbilityTable.Table[Ability.CurHealth] 
+          / AbilityTable.Table[Ability.MaxHealth];
+    }
+}
