cfd6d1aa5ac41ea010ba959bbd0537bcd4ae907b
한대건 <han15050424@gmail.com>
2020-10-29 16:23:35 +0900

    체력바 UI들을 Pool로 관리하는 스크립트, HealthBarPool을 추가한다
    추가한 스크립트는 싱글톤으로, 요청에 따라 체력바 UI를 제공하고 반납받는 간단한 기능을 한다.
    
    UsingHealthBar()함수를 통해 인자로 입력받은 정보에 맞춰 체력바를 제공하고,
    UnUsingHealthBar()함수를 통해 제공했던 체력바를 돌려받는다.

---
+++ Assets/Scripts/Modules/HealthBar/HealthBarPool.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarPool : Singleton<HealthBarPool>
+{
+    [SerializeField] private uint HoldingCount; [Space()]
+
+    [SerializeField] private HealthBar OrignHealthBar;
+    [SerializeField] private Transform WorldCanvasTransform;
+
+    private Queue<HealthBar> mPool;
+
+    private void Awake()
+    {
+        mPool = new Queue<HealthBar>();
+
+        for (int i = 0; i < HoldingCount; i++)
+        {
+            HealthBar healthBar = Instantiate(OrignHealthBar, WorldCanvasTransform);
+            UnUsingHealthBar(healthBar);
+        }
+    }
+
+    public void UsingHealthBar(float offsetY, Transform master, AbilityTable abilityTable)
+    {
+        if (mPool.Count == 0) {
+            mPool.Enqueue(Instantiate(OrignHealthBar, WorldCanvasTransform));
+        }
+        HealthBar healthBar = mPool.Dequeue();
+
+        healthBar.Init(Vector3.up * offsetY, master, abilityTable);
+        healthBar.gameObject.SetActive(true);
+    }
+
+    public void UnUsingHealthBar(HealthBar healthBar)
+    {
+        mPool.Enqueue(healthBar);
+                      healthBar.gameObject.SetActive(false);
+    }
+}
