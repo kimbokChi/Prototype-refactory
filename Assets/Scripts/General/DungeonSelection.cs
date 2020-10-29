963dcefda75b0ad7d0b7cbcdf14ecabd699ad358
한대건 <han15050424@gmail.com>
2020-10-29 11:05:45 +0900

    던전의 이름에 기반하여 던전을 불러오는 스크립트를 추가한다
    추가한 스크립트의 이름은 DungeonSelection으로, 던전을 불러오는 기능은 DungeonEnter()함수를 통해 실행된다.
    
    DungeonEnter()함수는 인스펙터에서 지정한 씬의 이름(DungeonName)과 대치되는 씬을 불러오는 기능을 한다.

---
+++ Assets/Scripts/General/DungeonSelection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DungeonSelection : MonoBehaviour
+{
+    [TextArea(1, 1)]
+    [SerializeField]
+    private string DungeonName;
+
+    public void DungeonEnter()
+    {
+        SceneManager.LoadScene(DungeonName);
+    }
+}
