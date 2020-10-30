02cfab8cb2cdcecf39b26f759e0041799caf5751
한대건 <han15050424@gmail.com>
2020-10-30 12:30:22 +0900

    PlayerAnimator과 마찬가지의 기능을 하는, EnemyAnimator를 추가한다
    이 스크립트는 PlayerAnimator과 같은 방식으로 사용할 수 있는 스크립트이다.
    현재 기본 고블린 개체에게만 적용되어있다.

---
+++ Assets/Scripts/Enemies/EnemyAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyAnim
+{
+    Idle, Move, Attack, Damaged, Death
+}
+
+public class EnemyAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Animator Animator;
+
+    private int mControlKey;
+
+    private void Reset()
+    {
+        TryGetComponent(out Animator);
+    }
+
+    public void Init()
+    {
+        mControlKey = Animator.GetParameter(0).nameHash;
+    }
+
+    public void ChangeState(EnemyAnim anim)
+    {
+        Animator.SetInteger(mControlKey, (int)anim);
+    }
+}
