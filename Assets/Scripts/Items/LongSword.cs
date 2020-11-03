aae71bc6a5c84cc2e5971aa96cff498d2f5f5c55
한대건 <han15050424@gmail.com>
2020-11-03 19:43:59 +0900

    플레이어가 사용하는 무기인 LongSword의 스크립트를 추가한다
    스크립트를 통해 LongSword의 애니메이션을 제어하여 적을 공격하도록 한다.
    
    * 그 외
    - LongSword에게 선딜/후딜 시간을 부여하기위해 ItemData에 LongSword항목을 추가한다.
    - LongSword를 사용하지 않은 상태를 표현하기 위해, 기본 상태 애니메이션을 추가한다.
    - 3가지 애니메이션에 애니메이션 이벤트를 추가하여 LongSword가 애니메이션의 종료에 따라
    행동을 어쩌구 할 수 있도록 한다.

---
+++ Assets/Scripts/Items/LongSword.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class LongSword : Item
+{
+    [SerializeField] private Animator Animator;
+    [SerializeField] private Area CollisionArea;
+
+    private int mAnimPlayKey;
+    private int mAnimControlKey;
+
+    private bool mCanAttack;
+
+    private GameObject mPlayer;
+
+    public override float AttackTime
+    { get => 0.245f; }
+    public override float WeaponRange
+    { get => 1.5f; }
+    public override ItemRating Rating
+    { 
+        get => ItemRating.Rare; 
+    }
+
+    private void Reset()
+    {
+        Debug.Assert(TryGetComponent(out Animator));
+    }
+
+    public override void OffEquipThis(SlotType offSlot)
+    { }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        if (onSlot.Equals(SlotType.Weapon))
+        {
+            CollisionArea.SetEnterAction(HitAction);
+
+            mAnimPlayKey    = Animator.GetParameter(0).nameHash;
+            mAnimControlKey = Animator.GetParameter(1).nameHash;
+        }
+    }
+
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        mCanAttack = true;
+        CollisionArea.enabled = true;
+
+        Animator.SetBool(mAnimPlayKey, true);
+        Animator.SetBool(mAnimControlKey, !Animator.GetBool(mAnimControlKey));
+
+        mPlayer = attacker;
+    }
+
+    private void HitAction(GameObject hitObject)
+    {
+        if (mCanAttack) {
+            if (hitObject.TryGetComponent(out ICombatable combatable))
+            {
+                combatable.Damaged(1f, mPlayer);
+
+                Inventory.Instance.OnAttackEvent(mPlayer, combatable);
+            }
+        }
+    }
+
+    private void AnimationPlayOver()
+    {
+        mCanAttack = false;
+        CollisionArea.enabled = false;
+    }
+}
