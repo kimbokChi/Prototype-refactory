b7fde41babf6106fbffc5c14859cddda207d31dd
한대건 <han15050424@gmail.com>
2020-11-05 05:39:26 +0900

    고블린 족장의 스크립트를 새로 추가한다
    기존의 것을 수정하는 방법보다 이게 더 나은것 같다고 생각했기 때문에 이렇게 변경하고,
    이 스크립트를 고블린 족장 프리팹에 적용한다.

---
+++ Assets/Scripts/Enemies/Boss/GoblinChief.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinChief : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
+{
+    [Header("Ability")]
+    [SerializeField] private AbilityTable AbilityTable;
+
+    [Header("Skill Info")]
+    [SerializeField] private SpecialTotem BuffTotem;
+    [SerializeField] private SpecialTotem BombTotem;
+    [SerializeField] private SpecialTotem LightningTotem;
+    [SerializeField] private Lightning    Lightning;
+
+    private Player mPlayer;
+
+    public AbilityTable GetAbility => throw new System.NotImplementedException();
+
+    public void AnimationPlayOver(AnimState anim)
+    {
+        
+    }
+
+    public void Damaged(float damage, GameObject attacker)
+    {
+        
+    }
+
+    public void IInit()
+    {
+        LightningTotem.SetSkill(() =>
+        {
+            Lightning.gameObject.SetActive(true);
+
+            Lightning.SetAttackPower(15f);
+            Lightning.transform.position += mPlayer.transform.position + (Vector3.up * 5f);
+        });
+    }
+
+    public void IUpdate()
+    {
+        
+    }
+
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        mPlayer = enterPlayer;
+    }
+
+    public void PlayerExit(MESSAGE message)
+    {
+        if (message.Equals(MESSAGE.BELONG_FLOOR)) {
+            mPlayer = null;
+        }
+    }
+
+    public void CastBuff(BUFF buffType, IEnumerator castedBuff) {
+        StartCoroutine(castedBuff);
+    }
+
+    public bool IsActive() {
+        return gameObject.activeSelf;
+    }
+
+    public GameObject ThisObject() {
+        return gameObject;
+    }
+}
