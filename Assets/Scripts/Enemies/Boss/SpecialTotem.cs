c198f4075f47d942c62d8171e6e3e2c6d6d59aeb
한대건 <han15050424@gmail.com>
2020-11-05 05:28:09 +0900

    스페셜 토템 전용 스크립트를 추가한다
    스페셜 토템 정용 스크립트는 애니메이션 이벤트를 통해 발동하도록 설계되어있는데,
    SetSkill함수를 통해 미리 행동을 정의한 뒤에 애니메이션 이벤트로 CastSkill함수를 실행하여
    SwtSkill으로 지정한 행동을 실행한다.
    
    현재 이 스크립트를 사용하는 객체는 특수 번개토템뿐이다.

---
+++ Assets/Scripts/Enemies/Boss/SpecialTotem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialTotem : MonoBehaviour
+{
+    private Action mSkill;
+
+    public void SetSkill(Action skill)
+    {
+        mSkill = skill;
+    }
+
+    public void CastSkill()
+    {
+        mSkill?.Invoke();
+    }
+
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
