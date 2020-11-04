f413642f6ce5b90c999b8b73676af4b85ad5fd70
한대건 <han15050424@gmail.com>
2020-11-04 20:57:19 +0900

    피격 이펙트와 같은 이펙트에 대한 시스템을 구축하고, 허수아비 개체가 피격 이펙트를 사용하도록 한다
    이펙트들은 Effect스크립트를 사용해야한다. Effect스크립트에는 LifeTimeOver()라는 함수가 있는데,
    이 함수를 애니메이션 이벤트로 실행하여 EffectLibrary로 돌아가도록 한다.
    
    EffectLibrary는 Effect에 대한 풀링을 지원하는 스크립트이다. 인스펙터 창에서 Effect를 추가한 뒤
    UsingEffect함수로 이펙트를 불러와, 사용한다.
    
    현재까지 이러한 이펙트들은 피격 하나뿐이며(프리팹화 함) 사용하는 개체또한 허수아비(기본 고블린)뿐이다.

---
+++ Assets/Scripts/Effect/Effect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour
+{
+    [SerializeField]
+    private EffectKind EffectKind;
+
+    public void LifeTimeOver() {
+        EffectLibrary.Instance.UnUsingEffect(EffectKind, this);
+    }
+}
---
+++ Assets/Scripts/Effect/EffectLibrary.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectKind
+{
+    EnemyDmgEffect
+}
+
+[System.Serializable]
+public struct EffectPage
+{
+    public EffectKind EffectKind;
+    public Effect     EffectOrigin;
+
+    public uint HoldingCount;
+}
+
+public class EffectLibrary : Singleton<EffectLibrary>
+{
+    [SerializeField]
+    private EffectPage[] Effects;
+
+    private Dictionary<EffectKind, Stack<Effect>> EffectLib;
+
+    private void Awake()
+    {
+        EffectLib = new Dictionary<EffectKind, Stack<Effect>>();
+
+        for (int i = 0; i < Effects.Length; i++) 
+        {
+
+            var origin = Effects[i].EffectOrigin;
+            var   kind = Effects[i].EffectKind;
+
+            EffectLib.Add(kind, new Stack<Effect>());
+
+            for (int j = 0; j < Effects[i].HoldingCount; ++j) 
+            {
+                var instance = Instantiate(origin);
+
+                EffectLib[kind].Push(instance);
+                                     instance.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void UnUsingEffect(EffectKind kind, Effect effect)
+    {        
+        EffectLib[kind].Push(effect);
+                             effect.gameObject.SetActive(false);
+    }
+
+
+    public void UsingEffect(EffectKind kind, Vector2 worldPoint)
+    {
+        if (EffectLib[kind].Count == 1) {
+            EffectLib[kind].Push(Instantiate(EffectLib[kind].Peek()));
+        }
+        var effect
+            = EffectLib[kind].Pop();
+
+        effect.transform.position = worldPoint;
+        effect.gameObject.SetActive(true);
+    }
+}
