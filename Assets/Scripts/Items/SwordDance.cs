74cbd5e9928a53d17125136ad057bf45e2d8aa96
한대건 <han15050424@gmail.com>
2020-11-05 04:56:02 +0900

    검기 스킬로 사용되는 오브젝트의 스크립트를 추가한다

---
+++ Assets/Scripts/Items/SwordDance.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordDance : MonoBehaviour
+{
+    [SerializeField] private float Damage;
+    [SerializeField] private float LifeTime;
+    [SerializeField] private float Speed;
+
+    [SerializeField] private string TargetTag;
+
+    private float DeltaTime {
+        get => Time.deltaTime * Time.timeScale;
+    }
+
+    public void Launch(Vector2 direction)
+    {
+        gameObject.SetActive(true);
+
+        StartCoroutine(EUpdate(direction));
+    }
+
+    private IEnumerator EUpdate(Vector3 direction)
+    {
+        for (float i = 0f; i < LifeTime; i += DeltaTime)
+        {
+            transform.position += direction * Speed;
+
+            yield return null;
+        }
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(TargetTag)) {
+            if (collision.TryGetComponent(out ICombatable combatable))
+            {
+                combatable.Damaged(Damage, gameObject);
+            }
+        }
+    }
+}
