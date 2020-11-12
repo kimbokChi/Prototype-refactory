5fc538ae30540103e3a09ebf4f9ef52115de15b5
한대건 <han15050424@gmail.com>
2020-11-12 15:33:09 +0900

    GameTester에 게임 시간을 가속/감속하는 기능을 추가한다
    빠른 시뮬레이션을 위해 이러한 가속/감속 기능을 추가한다.

--- Assets/Scripts/General/GameTester.cs
+++ Assets/Scripts/General/GameTester.cs
@@ -13,6 +13,8 @@ public class AddItem : Editor
 
     private string mKillAllEnemyExpectionName = "Scarecrow";
 
+    private float mGameSpeed = 1f;
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
@@ -69,6 +71,19 @@ public class AddItem : Editor
                    }
                 }
             }
+            //==================================================================
+            GUILayout.Space(6f);
+
+            GUILayout.Label("Game Control", EditorStyles.boldLabel);
+            GUILayout.Label("Game Speed");
+            mGameSpeed = GUILayout.HorizontalSlider(mGameSpeed, 0.1f, 2f);
+            GUILayout.Space(14f);
+
+            if (GUILayout.Button("Speed Reset", GUILayout.Height(20f))) {
+                mGameSpeed = 1f;
+            }
+            Time.timeScale = mGameSpeed;
+            GUILayout.Space(6f);
         }
     }
 }
