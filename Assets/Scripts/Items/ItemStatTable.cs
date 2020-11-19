17ffdac5e67f44bfabe796ab2f0bf26b35c55aaa
한대건 <han15050424@gmail.com>
2020-11-19 13:04:06 +0900

    아이템의 능력치를 저장하는 클래스, ItemStatTable을 추가한다
    ItemStatTable은 사정거리, 선딜/후딜과 같은 능력치에 대한 정보를 제공해주는 클래스이다.
    Item은 이 클래스를 사용하여 아이템의 능력치를 미리 저장해두고 사용하도록 한다.
    
    동시에, ItemData.json파일의 선딜/후딜 레이블의 이름에 사용되는 특수 기호를 변경한다.

---
+++ Assets/Scripts/Items/ItemStatTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemStat
+{
+    AttackPower, Range,
+    Begin_AttackDelay, 
+    After_AttackDelay,
+    End
+}
+public class ItemStatTable : MonoBehaviour
+{
+    private const string JsonTableName = "ItemData";
+    public ItemRating Rating
+    { get; private set; }
+
+    public  Dictionary<ItemStat, float>  Table
+    {
+        get
+        {
+            if (_Table == null)
+            {
+                Init();
+            }
+            return _Table;
+        }
+    }
+    private Dictionary<ItemStat, float> _Table;
+
+    [TextArea(1, 1)]
+    [SerializeField] private string JsonLabelName;
+
+    private void Init()
+    {
+        _Table = new Dictionary<ItemStat, float>();
+
+        string JsonString(string s)
+        {
+            return DataUtil.GetDataValue(JsonTableName, "ID", JsonLabelName, s);
+        }
+
+        for (ItemStat i = 0; i < ItemStat.End; i++)
+        {
+            _Table[i] = float.Parse(JsonString(i.ToString()));
+        }
+        Rating = (ItemRating)Enum.Parse(typeof(ItemRating), JsonString("Rating"));
+    }
+}
