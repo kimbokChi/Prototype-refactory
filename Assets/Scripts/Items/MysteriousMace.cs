673a68cfc1dae04e7186e116d168595683017ec4
한대건 <han15050424@gmail.com>
2020-11-19 20:27:50 +0900

    '신비한 메이스'아이템을 추가한다
    기존의 LongSword보다 더 큰 카메라 흔들림을 발생시키는, '신비한 메이스'아이템을 추가한다.
    아직 아이템의 특수 능력은 구현되지 않은 상태이다.

---
+++ Assets/Scripts/Items/MysteriousMace.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class MysteriousMace : Item
+{
+    [SerializeField] private Animator Animator;
+    [SerializeField] private Area CollisionArea;
+
+    private int mAnimPlayKey;
+    private int mAnimControlKey;
+
+    private GameObject mPlayer;
+
+    private void Reset()
+    {
+        Debug.Assert(TryGetComponent(out Animator));
+    }
+
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        Animator.SetBool(mAnimPlayKey, true);
+        Animator.SetBool(mAnimControlKey, !Animator.GetBool(mAnimControlKey));
+
+        mPlayer = attacker;
+    }
+
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        
+    }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        if (onSlot.Equals(SlotType.Weapon))
+        {
+            CollisionArea.SetEnterAction(HitAction);
+
+            mAnimPlayKey    = Animator.GetParameter(0).nameHash;
+            mAnimControlKey = Animator.GetParameter(1).nameHash;
+
+            mPlayer = transform.parent.parent.gameObject;
+        }
+    }
+
+    private void HitAction(GameObject target)
+    {
+        if (target.TryGetComponent(out ICombatable combatable))
+        {
+            combatable.Damaged(StatTable.Table[ItemStat.AttackPower], mPlayer);
+
+            Inventory.Instance.OnAttackEvent(mPlayer, combatable);
+        }
+    }
+
+    protected override void CameraShake()
+    {
+        MainCamera.Instance.Shake(0.25f, 1.5f, true);
+    }
+}
