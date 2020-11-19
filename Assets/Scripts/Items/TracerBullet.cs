ca01df1d753d09a7e68000e567230889fe864f68
한대건 <han15050424@gmail.com>
2020-11-19 21:23:40 +0900

    지정한 대상을 추적하는 탄환, TraceBullet을 추가한다
    '신비한 메이스'의 특수능력이 사용하는 탄환으로, Shoot()함수의 인자로 지정한 대상을 LifeTime동안 추적한다.
    만약 이미 대상을 추적하고 있었다면, Shoot()함수는 false를 반환하게 된다.

---
+++ Assets/Scripts/Items/TracerBullet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class TracerBullet : MonoBehaviour
+{
+    [SerializeField] private float LifeTime;
+    [SerializeField] private float Speed;
+
+    private Transform TargetTransform;
+    private IEnumerator ETarceTarget;
+    private Action<GameObject> TargetHitAction;
+
+    public bool Shoot(Transform target, Action<GameObject> hitAction)
+    {
+        bool canShoot = ETarceTarget == null;
+
+        if (canShoot)
+        {
+            TargetTransform = target;
+            TargetHitAction = hitAction;
+
+            StartCoroutine(ETarceTarget = TarceTarget());
+        }
+        return canShoot;
+    }
+
+    private IEnumerator TarceTarget()
+    {
+        float DeltaTime()
+        {
+            return Time.deltaTime * Time.timeScale;
+        }
+        for (float i = 0f; i < LifeTime; i += DeltaTime())
+        {
+            Vector3 direction
+                = TargetTransform.position - transform.position;
+
+            transform.position += direction.normalized * Speed;
+
+            yield return null;
+        }
+        ETarceTarget = null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.Equals(TargetTransform))
+        {
+            TargetHitAction.Invoke(collision.gameObject);
+
+            if (ETarceTarget != null)
+            {
+                StopCoroutine(ETarceTarget);
+
+                ETarceTarget = null;
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
