2744383437aeec7f23a8646f06038b48063440d7
한대건 <han15050424@gmail.com>
2020-11-19 21:25:18 +0900

    '신비한 메이스'의 특수능력을 구현한다
    적을 공격할 때마다 30%의 확률로 사정거리 밖 근처 적을 추적하는 탄환을 발사하는 기능을 구현한다.
    
    다만, 현재 코드가 굉장히 굉장히 비효율적으로 되어있다. 정말 '구현'만을 위해 작성한..그런..

--- Assets/Scripts/Items/MysteriousMace.cs
+++ Assets/Scripts/Items/MysteriousMace.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
+// 코드가 매우 비효율적으로 작성되어있음!
 public class MysteriousMace : Item
 {
+    [SerializeField] private TracerBullet TracerBullet;
+
     [SerializeField] private Animator Animator;
     [SerializeField] private Area CollisionArea;
 
@@ -48,7 +52,39 @@ public class MysteriousMace : Item
             combatable.Damaged(StatTable.Table[ItemStat.AttackPower], mPlayer);
 
             Inventory.Instance.OnAttackEvent(mPlayer, combatable);
+
+            // 30%
+            if (Random.value <= 0.3f)
+            {
+                Vector2 playerPos = mPlayer.transform.position;
+
+                Instantiate(TracerBullet, playerPos, Quaternion.identity).Shoot(FindCloestEnemy(), TargetHitAction);
+            }
         }
+    }
+
+    private void TargetHitAction(GameObject target)
+    {
+        if (target.TryGetComponent(out ICombatable combat))
+        {
+            // 2/3의 피해
+            combat.Damaged(StatTable.Table[ItemStat.AttackPower] * 0.666f, mPlayer);
+        }
+    }
+
+    private Transform FindCloestEnemy()
+    {
+        float Distance(GameObject o)
+        {
+            return Vector2.Distance(o.transform.position, mPlayer.transform.position);
+        }
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy")
+            .Where(o => o.activeSelf)
+            .Where(o => Distance(o) > WeaponRange);
+
+        var enemy = enemies.OrderBy(o => Distance(o)).FirstOrDefault();
+
+        return enemy?.transform;
     }
 
     protected override void CameraShake()
