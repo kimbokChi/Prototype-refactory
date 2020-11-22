154f9fe558f0442000609d4147a272ab5ada4ab9
한대건 <han15050424@gmail.com>
2020-11-22 11:28:04 +0900

    ItemLibrary의 무작위 아이템 반환 기능을 보완한다
    - 등급별 아이템의 확률은 인스펙터 창에서 지정할 수 있다.
    - 인스펙터에서 지정한 확률의 총합은 1이되어야 하기 때문에, 인스펙터에서 실행할 수 있는
    함수를 추가한다. 이 함수를 통해 확률의 총합을 알 수 있다.
    - 무작위 아이템을 반환하는 GetRandomItem()함수를 보완한다(신식코드로 재작성한다 머 그런..).

--- Assets/Scripts/Singletons/ItemLibrary.cs
+++ Assets/Scripts/Singletons/ItemLibrary.cs
@@ -9,17 +9,29 @@ public enum ItemRating
 }
 public class ItemLibrary : Singleton<ItemLibrary>
 {
-    public const float    COMMON_PROBABILITY =  0.4f; // 40%
-    public const float      RARE_PROBABILITY = 0.35f; // 35%
-    public const float      EPIC_PROBABILITY =  0.2f; // 20%
-    public const float LEGENDARY_PROBABILITY = 0.05f; //  5%
+    [Header("Item Probablities")]
+    [SerializeField][Range(0f, 1f)] private float Common;
+    [SerializeField][Range(0f, 1f)] private float Rare;
+    [SerializeField][Range(0f, 1f)] private float Epic;
+    [SerializeField][Range(0f, 1f)] private float Legendary;
 
-    [SerializeField]
-    private Item[] Items;
+    [Header("Registered Items")]
+    [SerializeField] private Item[] Items;
 
     private Dictionary<ItemRating, List<Item>> mLibrary;
 
     private float[] mProbabilityArray;
+
+    [ContextMenu("Probablity Check")]
+    private void ProbablityCheck()
+    {
+        float sum = Common + Rare + Epic + Legendary;
+
+        Debug.Log($"Probablity Sum : {sum}");
+
+        if (sum != 1f)
+            Debug.LogError($"아이템 등장 확률의 총합은 1이 될 것을 권장합니다.");
+    }
 
     private void Awake()
     {
@@ -36,36 +48,56 @@ public class ItemLibrary : Singleton<ItemLibrary>
         }
         mProbabilityArray = new float[4] 
         {
-            COMMON_PROBABILITY, RARE_PROBABILITY, EPIC_PROBABILITY, LEGENDARY_PROBABILITY
+            Common, Rare, Epic, Legendary
         };
     }
 
     public Item GetRandomItem()
     {
-        Item getItem = null;
-
+        float sum = 0f;
         float probability = Random.value;
 
-        float sum = 0f;
+        int invokeCount = 0;
 
-        for (int i = 0; i < 4; i++)
+        foreach (var lib in mLibrary)
         {
-            sum += (mLibrary[(ItemRating)i].Count == 0) ? 0 : mProbabilityArray[i];
-
-            if (sum <= probability)
+            // 현재 등급의 아이템에서 반환할 수 있는 아이템이 없다면
+            if (lib.Value.Count == 0)
             {
-                ItemRating rating = (ItemRating)i;
+                int division = 
+                    mLibrary.ToList().Count(o => o.Value.Count > 0);
 
-                if (mLibrary[rating].Count != 0)
+                float additive =
+                    mProbabilityArray[invokeCount] / division;
+
+                for (int i = 0; i < 4; i++)
                 {
-                    int itemIndex = Random.Range(0, mLibrary[rating].Count);
-
-                    getItem = mLibrary[rating][itemIndex];
-                              mLibrary[rating].RemoveAt(itemIndex);
+                    // 반환할 수 있는 나머지 등급들의 확률을 보정한다
+                    if (mLibrary[(ItemRating)i].Count > 0)
+                    {
+                        mProbabilityArray[i] += additive;
+                    }
                 }
+                // 그리고 mLibrary에서 제거
+                mLibrary.Remove(lib.Key);
+                continue;
             }
+            sum += mProbabilityArray[invokeCount];
+
+            // 반환할 아이템의 등급이 정해졌다면
+            if (probability <= sum)
+            {
+                int index = Random.Range(0, lib.Value.Count);
+
+                var item = lib.Value[index];
+                           lib.Value.RemoveAt(index);
+
+                // 그 등급의 아이템 중 무작위 아이템 반환
+                return item;
+            }
+            invokeCount++;
         }
-        return getItem;
+        return null;
     }
 
     public Item GetRandomItem(ItemRating rating)
