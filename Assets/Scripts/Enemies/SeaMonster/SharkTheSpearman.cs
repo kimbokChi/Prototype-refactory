a4b63a794a81cb72505a5ed3214cf10db17215d9
한대건 <han15050424@gmail.com>
2020-11-23 08:24:41 +0900

    상어 창병의 스크립트와 프리팹을 추가한다
    상어 창병의 스크립트는 아직 미완성상태이다.

--- Assets/Scripts/Enemies/SeaMonster/SharkTheSpearman.cs
+++ Assets/Scripts/Enemies/SeaMonster/SharkTheSpearman.cs
@@ -4,29 +4,74 @@ using UnityEngine;
 
 public class SharkTheSpearman : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
 {
+    [Header("Spear Info")]
+    [SerializeField] private Arrow Spear;
+    [SerializeField] private float ShootSpeed;
+    [SerializeField] private Vector2 ShootPos;
+
+    [Header("Ability")]
     [SerializeField] private AbilityTable AbilityTable;
     [SerializeField] private EnemyAnimator EnemyAnimator;
+
+    private AttackPeriod _AttackPeriod;
+
+    private IEnumerator _EMove;
 
     private Player mPlayer;
 
     public void AnimationPlayOver(AnimState anim)
     {
-        // ToDo
-    }
+        switch (anim)
+        {
+            case AnimState.Attack:
+                
+                break;
 
-    public void CastBuff(BUFF buffType, IEnumerator castedBuff)
-    {
-        StartCoroutine(castedBuff);
+            case AnimState.Damaged:
+                EnemyAnimator.ChangeState(AnimState.Idle);
+                break;
+
+            case AnimState.Death:
+                gameObject.SetActive(false);
+                break;
+
+            case AnimState.AttackBegin:
+
+                break;
+        }
     }
 
     public void Damaged(float damage, GameObject attacker)
     {
-        
+        EffectLibrary.Instance.UsingEffect(EffectKind.EnemyDmgEffect, transform.position);
+
+        if ((AbilityTable.Table[Ability.CurHealth] -= damage) <= 0)
+        {
+            _AttackPeriod.StopPeriod();
+
+            EnemyAnimator.ChangeState(AnimState.Death);
+            HealthBarPool.Instance.UnUsingHealthBar(transform);
+        }
     }
 
     public void IInit()
     {
-        
+        EnemyAnimator.Init();
+        HealthBarPool.Instance.UsingHealthBar(-1f, transform, AbilityTable);
+
+        _AttackPeriod = new AttackPeriod(AbilityTable);
+
+        _AttackPeriod.SetAction(Period.Attack, () =>
+        {
+            if (_EMove != null)
+            {
+                StopCoroutine(_EMove);
+                _EMove = null;
+            }
+
+            EnemyAnimator.ChangeState(AnimState.AttackBegin);
+        });
+        Spear = Instantiate(Spear);
     }
 
     public void IUpdate()
@@ -51,10 +96,14 @@ public class SharkTheSpearman : MonoBehaviour, IObject, ICombatable, IAnimEventR
         }
     }
 
-    public AbilityTable GetAbility => AbilityTable;
     public GameObject ThisObject() => gameObject;
+    public AbilityTable GetAbility => AbilityTable;
     public bool IsActive()
     {
         return gameObject.activeSelf;
+    }
+    public void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
     }
 }
