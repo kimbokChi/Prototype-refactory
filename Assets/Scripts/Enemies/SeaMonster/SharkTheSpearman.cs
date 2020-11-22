e3f98034ba8b992f0e04ffc7dd8e9899e02d92aa
한대건 <han15050424@gmail.com>
2020-11-22 19:04:11 +0900

    상어 창병의 스크립트를 추가한다
    아직 내용이 제대로 갖춰지지 않은 상태이고, EnemyBase를 상속받지 않는다.
    
    EnemyBase를 상속받지 않는 이유는, 적 개체의 대한 행동을 모듈화할 계획인데,
    적들이 하는 행동에 대한 정보가 부족하기 때문이다.

---
+++ Assets/Scripts/Enemies/SeaMonster/SharkTheSpearman.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkTheSpearman : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
+{
+    [SerializeField] private AbilityTable AbilityTable;
+    [SerializeField] private EnemyAnimator EnemyAnimator;
+
+    private Player mPlayer;
+
+    public void AnimationPlayOver(AnimState anim)
+    {
+        // ToDo
+    }
+
+    public void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+
+    public void Damaged(float damage, GameObject attacker)
+    {
+        
+    }
+
+    public void IInit()
+    {
+        
+    }
+
+    public void IUpdate()
+    {
+
+    }
+
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        if (AbilityTable.CanRecognize(message))
+        {
+
+            mPlayer = enterPlayer;
+        }
+    }
+
+    public void PlayerExit(MESSAGE message)
+    {
+        if (AbilityTable.CantRecognize(message)) {
+
+            mPlayer = null;
+        }
+    }
+
+    public AbilityTable GetAbility => AbilityTable;
+    public GameObject ThisObject() => gameObject;
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+}
