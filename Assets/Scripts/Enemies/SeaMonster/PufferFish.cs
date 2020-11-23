1b952c426effc46b4bba3e31b5d9381a391f5aaa
한대건 <han15050424@gmail.com>
2020-11-23 19:53:01 +0900

    복어를 완성하고, 복어가 있는 방을 추가하며 GoogleSheet의 데이터를 갱신한다
    복어 스크립트는 상어 창병의 스크립트를 복붙한 뒤, 일부를 제거하여 완성했다..만
    아직 '기절'효과는 완성되지 않았다. 이제 기절 효과를 만들 차례..
    
    그리고 복어 애니메이터의 트랜지션 로직을 완성하고, 애니메이션 이벤트를 배치한다.
    
    추가한 복어가 있는 방은 Room_002이다.

---
+++ Assets/Scripts/Enemies/SeaMonster/PufferFish.cs
@@ -0,0 +1,226 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PufferFish : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
+{
+    [Header("Ability")]
+    [SerializeField] private bool IsLookAtLeft;
+    [SerializeField] private AbilityTable AbilityTable;
+    [SerializeField] private EnemyAnimator EnemyAnimator;
+    [SerializeField] private Area Range;
+
+    [Header("Movement Info")]
+    [SerializeField] private float WaitMoveTimeMin;
+    [SerializeField] private float WaitMoveTimeMax;
+    [SerializeField] private Vector2 InitPosition;
+
+    private AttackPeriod _AttackPeriod;
+
+    private IEnumerator _Move;
+
+    private Player _Player;
+
+    public void AnimationPlayOver(AnimState anim)
+    {
+        switch (anim)
+        {
+            case AnimState.Attack:
+                {
+                    _AttackPeriod.StopPeriod();
+
+                    HealthBarPool.Instance.UnUsingHealthBar(transform);
+                    gameObject.SetActive(false);
+                }
+                break;
+
+            case AnimState.Damaged:
+                EnemyAnimator.ChangeState(AnimState.Idle);
+                break;
+
+            case AnimState.Death:
+                gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    public void Damaged(float damage, GameObject attacker)
+    {
+        EffectLibrary.Instance.UsingEffect(EffectKind.EnemyDmgEffect, transform.position);
+        EnemyAnimator.ChangeState(AnimState.Damaged);
+
+        if ((AbilityTable.Table[Ability.CurHealth] -= damage) <= 0)
+        {
+            _AttackPeriod.StopPeriod();
+
+            EnemyAnimator.ChangeState(AnimState.Death);
+            HealthBarPool.Instance.UnUsingHealthBar(transform);
+        }
+    }
+
+    public void IInit()
+    {
+        EnemyAnimator.Init();
+        HealthBarPool.Instance.UsingHealthBar(-1f, transform, AbilityTable);
+
+        _AttackPeriod = new AttackPeriod(AbilityTable);
+
+        _AttackPeriod.SetAction(Period.Attack, () => 
+        {
+            MoveStop();
+
+            EnemyAnimator.ChangeState(AnimState.Attack);
+        });
+
+        Range.SetScale(AbilityTable[Ability.Range]);
+        StartCoroutine(MoveRoutine());
+    }
+
+    public void IUpdate()
+    {
+        if (!_AttackPeriod.IsProgressing())
+        {
+            if (Range.HasAny() && IsLookAtPlayer())
+            {
+                MoveStop();
+
+                _AttackPeriod.StartPeriod();
+            }
+        }
+    }
+
+    // 애니메이션 이벤트로 실행됨!
+    private void AttackAction()
+    {
+        MainCamera.Instance.Shake(0.8f, 1.1f, true);
+
+        if (Range.TryEnterTypeT(out Player player) && _Player != null)
+        {
+            _Player.CastBuff(BUFF.SPEEDUP, BuffLibrary.Instance.GetSlowBUFF(BUFF.SPEEDUP, 3, 15f, _Player.GetAbility));
+        }
+    }
+
+    private bool IsLookAtPlayer()
+    {
+        if (_Player != null)
+        {
+            return (_Player.transform.position.x < transform.position.x &&  IsLookAtLeft ||
+                    _Player.transform.position.x > transform.position.x && !IsLookAtLeft);
+        }
+        return false;
+    }
+    private void SetLookingLeft(bool lookingLeft)
+    {
+        IsLookAtLeft = lookingLeft;
+
+        if (IsLookAtLeft)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+        else
+            transform.rotation = Quaternion.Euler(Vector3.up * 180);
+    }
+
+    private void MoveStop()
+    {
+        if (_Move != null)
+        {
+            StopCoroutine(_Move);
+            _Move = null;
+        }
+    }
+
+    private IEnumerator MoveRoutine()
+    {
+        bool CanMovement()
+        {
+            return EnemyAnimator.CurrentState() == AnimState.Idle
+                && !_AttackPeriod.IsProgressing();
+        }
+        while (AbilityTable[Ability.CurHealth] > 0)
+        {
+            yield return new WaitUntil(CanMovement);
+            float waitTime = Random.Range(WaitMoveTimeMin, WaitMoveTimeMax);
+
+            yield return new WaitForSeconds(waitTime);
+            yield return new WaitUntil(CanMovement);
+
+            Vector2 movePoint = InitPosition;
+
+            if (IsLookAtPlayer())
+            {
+                if (IsLookAtLeft)
+                {
+                    movePoint.x = -3.5f;
+                }
+                else
+                    movePoint.x = +3.5f;
+            }
+            else
+            {
+                movePoint.x += Random.Range(-3.5f, 3.5f);
+            }
+            SetLookingLeft(movePoint.x < transform.localPosition.x);
+            StartCoroutine(_Move = Move(movePoint));
+
+            EnemyAnimator.ChangeState(AnimState.Move);
+        }
+    }
+
+    private IEnumerator Move(Vector2 movePoint)
+    {
+        Vector3 direction = (movePoint.x > transform.localPosition.x)
+            ? Vector3.right : Vector3.left;
+
+        float DeltaTime()
+        {
+            return Time.deltaTime * Time.timeScale;
+        }
+        bool CanMoving()
+        {
+            return direction.x > 0 && transform.localPosition.x < movePoint.x ||
+                   direction.x < 0 && transform.localPosition.x > movePoint.x;
+        }
+        do
+        {
+            transform.localPosition += direction * AbilityTable.MoveSpeed * DeltaTime();
+
+            yield return null;
+
+        } while (CanMoving());
+
+        transform.localPosition = movePoint;
+
+        EnemyAnimator.ChangeState(AnimState.Idle);
+        _Move = null;
+    }
+
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        if (AbilityTable.CanRecognize(message))
+        {
+
+            _Player = enterPlayer;
+        }
+    }
+
+    public void PlayerExit(MESSAGE message)
+    {
+        if (AbilityTable.CantRecognize(message))
+        {
+
+            _Player = null;
+        }
+    }
+
+    public GameObject ThisObject() => gameObject;
+    public AbilityTable GetAbility => AbilityTable;
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+    public void CastBuff(BUFF buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+}
