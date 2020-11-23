9988fae92be125ff054cf04835df7dc7ed58e7de
한대건 <han15050424@gmail.com>
2020-11-23 14:42:24 +0900

    게임의 타격감을 보강한다
    피격 이펙트의 크기를 2배 증가시키고, 플레이어 또한 피격시 피격 이펙트를 사용하도록 하며
    피격시 카메라의 흔들리는 정도를 증가시킨다.
    
    이렇게 하여, 게임의 타격감 머 그런거를 보강한다 근데 이걸 왜 지금하는거지 암튼 앓~~

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -387,7 +387,7 @@ public class Player : MonoBehaviour, ICombatable
         {
             mBlinkTimer.Start(mBlinkTime);
 
-            MainCamera.Instance.Shake(0.16f, 0.5f, true);
+            MainCamera.Instance.Shake(0.3f, 1.0f, true);
 
             mInventory.OnDamaged(ref damage, attacker, gameObject);
 
@@ -396,6 +396,7 @@ public class Player : MonoBehaviour, ICombatable
             {
                 DeathEvent.Invoke();
             }
+            EffectLibrary.Instance.UsingEffect(EffectKind.EnemyDmgEffect, transform.position);
         }
     }
 
