8360f08d08bcb35bf8a053a48a0b3e2b57dc227b
한대건 <han15050424@gmail.com>
2020-11-25 21:29:06 +0900

    크라켄의 포격 패턴을 구현한다
    - 아직 구현되지 않은 부분들은 주석으로 처리했다.
    - 크라켄은 아직 어느 방에 종속되지 않기 때문에, 유니티 이벤트 함수를 통해 초기화와 업데이트가 이루어진다.
    
    현재 크라켄의 포격 패턴은 마우스 왼클릭을 통해 테스트ㄹ할 수 있다.

--- Assets/Scripts/Enemies/Boss/Kraken/Kraken.cs
+++ Assets/Scripts/Enemies/Boss/Kraken/Kraken.cs
@@ -2,17 +2,131 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Kraken : MonoBehaviour
+public class Kraken : MonoBehaviour, IObject
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum Pattern
+    {
+        FallingTentacle, StrikeTentacle, ArtilleryFire, SummonTentacle, SummonSeaMonster
+    }
+    [SerializeField] private AbilityTable AbilityTable;
+    [SerializeField] private EnemyAnimator EnemyAnimator;
+
+    [SerializeField] private GameObject[] SeaMonsters;
+
+    [Header("ArtilleryFire Info")]
+    [SerializeField] private Projection ArtilleryShell;
+    [SerializeField] private float ArtilleryShellSpeed;
+    [SerializeField] private float ArtilleryFireTime;
+    [SerializeField] private float ArtilleryFireDelay;
+    [SerializeField] private float RotationSpeed;
+
+    private Pattern _NextPattern;
+    private int _PatternInvokeCount;
+
+    private AttackPeriod _AttackPeriod;
+    private Pool<Projection> _ArtilleryShellPool;
+
+    public void IInit()
+    {
+        _PatternInvokeCount = 0;
+        //EnemyAnimator.Init();
+
+        _ArtilleryShellPool = new Pool<Projection>();
+        _ArtilleryShellPool.Init(16, ArtilleryShell, o => 
+        {
+            o.SetAction(
+                hitTarget =>
+                {
+                    if (hitTarget.TryGetComponent(out ICombatable combatable))
+                    {
+                        combatable.Damaged(AbilityTable.AttackPower, gameObject);
+                    }
+                },
+                projection => 
+                {
+                    _ArtilleryShellPool.Add(projection);
+                });
+        });
+
+        _AttackPeriod = new AttackPeriod(AbilityTable);
+        _AttackPeriod.SetAction(Period.Attack, () => 
+        {
+            _PatternInvokeCount++;
+
+            if (_PatternInvokeCount == 3)
+            {
+                _NextPattern = Pattern.SummonSeaMonster;
+
+                _PatternInvokeCount = 0;
+            }
+            else if (_PatternInvokeCount <= 3)
+            {
+                _NextPattern = Pattern.SummonTentacle;
+            }
+            else
+            {
+                _NextPattern = (Pattern)Random.Range(0, 3);
+            }
+        });
+        // _AttackPeriod.StartPeriod();
+    }
+
+    public void IUpdate()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        IInit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(ArtilleryFire());
+        }
+    }
+
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
     {
         
     }
+
+    public void PlayerExit(MESSAGE message)
+    {
+        
+    }
+
+    private IEnumerator ArtilleryFire()
+    {
+        float speed = ArtilleryShellSpeed;
+
+        float theta = 0f;
+
+        for (float i = 0f; i < ArtilleryFireTime; i += ArtilleryFireDelay)
+        {
+            float angle = 0f;
+
+            for (int j = 0; j < 4; j++)
+            {
+                angle += (theta + (90 * j));
+                angle *= Mathf.Deg2Rad;
+
+                var direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
+
+                _ArtilleryShellPool.Get().Shoot(transform.position, direction, speed);
+            }
+            yield return new WaitForSeconds(ArtilleryFireDelay); 
+            
+            theta += RotationSpeed;
+
+            if (theta >= 360f) theta = 0f;
+        }
+        _AttackPeriod.AttackActionOver();
+    }
+
+    public GameObject ThisObject() => gameObject;
+    public bool IsActive() => gameObject.activeSelf;
 }
