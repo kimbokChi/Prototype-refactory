58ea301839ab0f2baa266c2000ed819330a1e4ea
kimbokChi <han15050424@gmail.com>
2020-12-05 11:10:40 +0900

    대검의 스프라이트와 스크립트를 추가한다
    대검의 스크립트는 아직 구성이 덜 된 상태이다.

---
+++ Assets/Scripts/Items/GreatSword.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreatSword : Item
+{
+    [SerializeField] private Animator Animator;
+    [SerializeField] private Area AttackArea;
+
+    [Header("Charge Ablity")]
+    [SerializeField] private Projection SwordDance;
+    [Range(0f, 1f)]
+    [SerializeField] private float DemandCharge;
+
+    private int mAnimControlKey;
+    private int mAnimPlayKey;
+
+    private GameObject mPlayer;
+
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        base.AttackAction(attacker, combatable);
+    }
+
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        switch (offSlot)
+        {
+            case SlotType.Weapon:
+                Inventory.Instance.ChargeAction -= ChargeAction;
+                break;
+        }
+    }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        switch (onSlot)
+        {
+            case SlotType.Weapon:
+                Inventory.Instance.ChargeAction += ChargeAction;
+                break;
+        }
+    }
+
+    private void ChargeAction(float charge)
+    {
+
+    }
+
+    protected override void CameraShake()
+    {
+        
+    }
+}
