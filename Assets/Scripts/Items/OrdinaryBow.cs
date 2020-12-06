5ab2db5449bc45947a1b8de1ba25e9fd71619b59
kimbokChi <han15050424@gmail.com>
2020-12-06 11:30:58 +0900

    평범한 활 아이템을 완성한다
    - 평범한 활과 그 화살을 각각 프리팹화한다.
    - 평범한 활이 화살을 발사하는 동작은 애니메이션 이벤트를 통해 이루어진다.
    - 평범한 활의 능력치를 변경한다. 평범한 활은 선딜과 후딜이 없다.
      선딜과 후딜이 없는 것이 더욱 느낌있기 때문에 이렇게 변경한다.
      이 변경사항은 로컬에만 적용되어 있는 상태이다.

---
+++ Assets/Scripts/Items/OrdinaryBow.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrdinaryBow : Item
+{
+    [SerializeField] private Projection Arrow;
+
+    [SerializeField] private Animator Animator;
+
+    private int mAnimPlayKey;
+
+    private GameObject mPlayer;
+
+    private Pool<Projection> mArrowPool;
+    private bool mIsAlreadyInit = false;
+
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        mPlayer = attacker;
+
+        Animator.SetBool(mAnimPlayKey, true);
+    }
+
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        
+    }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        switch (onSlot)
+        {
+            case SlotType.Weapon:
+                {
+                    if (!mIsAlreadyInit)
+                    {
+                        mArrowPool = new Pool<Projection>();
+                        mArrowPool.Init(3, Arrow, o =>
+                        {
+                            o.SetAction(hit =>
+                            {
+                                if (hit.TryGetComponent(out ICombatable combatable))
+                                {
+                                    combatable.Damaged(StatTable[ItemStat.AttackPower], mPlayer);
+
+                                    MainCamera.Instance.Shake();
+                                }
+                            },
+                            p => mArrowPool.Add(p));
+                        });
+
+                        mAnimPlayKey = Animator.GetParameter(0).nameHash;
+                        mIsAlreadyInit = true;
+                    }
+                }
+                break;
+        }
+    }
+
+    private void ShootArrow()
+    {
+        var direction = Vector2.right;
+
+        if (mPlayer.transform.localRotation.eulerAngles.y > 0f)
+        {
+            direction = Vector2.left;
+        }
+        var arrow = mArrowPool.Get();
+
+        arrow.transform.rotation = mPlayer.transform.localRotation;
+        arrow.Shoot(transform.position, direction, 11.5f);
+    }
+
+    protected override void AttackAnimationPlayOver()
+    {
+        base.AttackAnimationPlayOver();
+
+        Animator.SetBool(mAnimPlayKey, false);
+    }
+
+    protected override void CameraShake()
+    {
+        MainCamera.Instance.Shake();
+    }
+}
