15bb272f1d5e4d439ecb953991c34a5aaab4a3d9
kimbokChi <han15050424@gmail.com>
2020-12-07 20:57:48 +0900

    현재 아이템의 상황을 저장하는 클래스를 추가한다
    추가한 스크립트의 이름은 ItemStateSaver으로, 런타임 상황에서 씬을 이동할 때 플레이어의
    아이템 정보(어떤 아이템을 장비하고 있고, 저장하고 있는지..)와 아이템 라이브러리의 정보
    (어떤 아이템이 라이브러리에서 없어졌는지..)를 저장하는 객체이다.
    
    현재로서는 아이템 라이브러리의 딕셔너리를 저장하고, 불러오는 기능이 구현되었다.

---
+++ Assets/Scripts/Items/ItemStateSaver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStateSaver : Singleton<ItemStateSaver>
+{
+    private Dictionary<ItemRating, List<Item>> _ItemLibDictionary;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Discard()
+    {
+        Destroy(gameObject);
+    }
+
+    public void SaveLibDictionary(Dictionary<ItemRating, List<Item>> itemLibDictionary)
+    {
+        _ItemLibDictionary = itemLibDictionary;
+    }
+
+    public void LoadLibDictionary(out Dictionary<ItemRating, List<Item>> itemLibDictionary)
+    {
+        if (_ItemLibDictionary == null)
+        {
+            itemLibDictionary = new Dictionary<ItemRating, List<Item>>();
+        }
+        else
+            itemLibDictionary = _ItemLibDictionary;
+    }
+}
