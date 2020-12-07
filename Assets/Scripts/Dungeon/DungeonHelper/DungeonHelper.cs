e0b9dfb4d21a9b9558a58e4901ba13fd06332143
kimbokChi <han15050424@gmail.com>
2020-12-07 20:09:46 +0900

    던전 아이템 지급 NPC를 완성한다
    상호작용을 통해 아이템을 떨구는 NPC를 완성한다. 아이템을 떨구는 것은 베지어 곡선을 통해 구현되었다.
    
    해당 NPC의 프리팹또한 완성하였는데, 인지 범위를 늘렸으며 씨리얼라이즈된 변수에 각각 알맞은 값을
    설정한다.

--- Assets/Scripts/Dungeon/DungeonHelper/DungeonHelper.cs
+++ Assets/Scripts/Dungeon/DungeonHelper/DungeonHelper.cs
@@ -1,49 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Kimbokchi;
 
 public class DungeonHelper : MonoBehaviour
 {
-    [SerializeField] private ChargeGauge Charge;
+    [SerializeField] private DropItem DropItem;
+    [SerializeField] private Transform DropItemHolder;
 
-    [SerializeField] private GameObject HandOut;
-    
-    [SerializeField] private Sprite NullImage;
-    
+    [Header("ItemDrop Curve")]
+    [SerializeField] private float CurveSpeed; [Space()]
+    [SerializeField] private Vector2 PointA;
+    [SerializeField] private Vector2 PointB;
+    [SerializeField] private Vector2 PointC;
+    [SerializeField] private Vector2 PointD;
+
     private bool mHasPlayer;
 
-    private Item mContainItem;
-
-    private void Reset()
+    private void OnEnable()
     {
-        Charge = FindObjectOfType(typeof(ChargeGauge)) as ChargeGauge;
+        DropItem.Init(ItemLibrary.Instance.GetRandomItem());
+
+        Finger.Instance.Gauge.DisChargeEvent += Interaction;
     }
 
-    private void Awake()
+    private void Interaction()
     {
-        mHasPlayer = false;
-
-        Charge.DisChargeEvent += Interact;
-
-        HandOut.transform.position = transform.position + Vector3.forward;
-        
-
-        if (HandOut.TryGetComponent(out SpriteRenderer render))
+        if (mHasPlayer)
         {
-            Debug.Log("렌더러가 있네요?");
-            Item rand = null; // = ItemLibrary.Instance.GetRandomItem();
-            if (!rand)
-            {
-                Debug.Log("아이템이 읎어요");
-                render.sprite = NullImage;
-            }
-            else
-            {
-                Debug.Log("아이템이 있어요");
-                render.sprite = mContainItem?.Sprite;
-            }
+            DropItem.gameObject.SetActive(true);
+
+            StartCoroutine(ItemDrop());
         }
-        else Debug.Log("렌더러가 읎어요");
+    }
+
+    private IEnumerator ItemDrop()
+    {
+        for (float ratio = 0f; ratio < 1; ratio += Time.deltaTime * CurveSpeed)
+        {
+            ratio = ratio > 1 ? 1 : ratio;
+
+            DropItemHolder.localPosition = Utility.BezierCurve3(PointA, PointB, PointC, PointD, ratio);
+            yield return null;
+        }
+        Finger.Instance.Gauge.DisChargeEvent -= Interaction;
+
+        yield return null;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -53,15 +55,5 @@ public class DungeonHelper : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) mHasPlayer = false;
-    }
-
-    private void Interact()
-    {
-        if (mHasPlayer)
-        {
-            HandOut.SetActive(true);
-            HandOut.GetComponent<HandOut>().Throw();
-            Inventory.Instance.AddItem(mContainItem);
-        }
     }
 }
