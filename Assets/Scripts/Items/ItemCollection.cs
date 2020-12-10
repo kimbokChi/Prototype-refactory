868ca99e018888c4292241f28dce7ba9a8491a79
kimbokChi <han15050424@gmail.com>
2020-12-10 09:34:49 +0900

    ItemCollection스크립트를 추가한다
    해당 스크립트의 현재 기능은 해금 된 아이템과 해금되지 않은 아이템을 구분하여,
    버튼의 이미지를 설정하는 것이다.
    
    이후에는 아이템의 갯수에 따라 버튼의 갯수를 동적으로 설정하도록 할 계획이며
    ItemLibrary을 통해 반환할 수 있는 아이템은 해금된 아이템들로만 설정할 계획이다.

---
+++ Assets/Scripts/Items/ItemCollection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollection : MonoBehaviour
+{
+    [Header("Unlocked Section")]
+    [SerializeField] private   Item[] UnlockedItems;
+    [SerializeField] private Button[] UnlockedItemButtons;
+
+    [Header("Locked Section")]
+    [SerializeField] private   Item[] LockedItems;
+    [SerializeField] private Button[] LockedItemButtons;
+
+    private void Awake()
+    {
+        for (int i = 0; i < LockedItems.Length; i++)
+        {
+            LockedItemButtons[i].image.sprite = LockedItems[i].Sprite;
+        }
+        for (int i = 0; i < UnlockedItems.Length; i++)
+        {
+            UnlockedItemButtons[i].image.sprite = UnlockedItems[i].Sprite;
+        }
+    }
+}
