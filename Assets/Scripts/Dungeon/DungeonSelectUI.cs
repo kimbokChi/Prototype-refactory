e64034a0f3fb00bf3bc31867381b639f0d910259
kimbokChi <han15050424@gmail.com>
2020-12-11 07:56:02 +0900

    던전 선택 UI를 완성한다
    그리고 던전 선택지를 선택했을 때 색상의 변화를 자연스럽게 한다.

--- Assets/Scripts/Dungeon/DungeonSelectUI.cs
+++ Assets/Scripts/Dungeon/DungeonSelectUI.cs
@@ -22,11 +22,6 @@ public class DungeonSelectUI : MonoBehaviour
 
     public void SelectDungeon(Button button)
     {
-        foreach (Button Child in mSelections)
-            Child.image.color = new Color(1f, 1f, 1f, 0.5f);
-
-        button.image.color = new Color(1f, 0, 0, 0.5f);
-
         Debug.Assert(button.TryGetComponent(out mDungeon));
     }
 
