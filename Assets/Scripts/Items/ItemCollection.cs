94aff28bfe9a2698ed0b27aab43c92740f6babf4
kimbokChi <han15050424@gmail.com>
2020-12-10 11:14:51 +0900

    아이템 컬렉션은 각 세션에 해당하는 아이템의 갯수에따라 달라지도록 한다
    ItemCollection클래스는 아이템의 갯수에따라 CollectionBox를 생성하고, 알맞은 위치에 설정한다.
    
    예를 들어, 잠긴 아이템이 3개라면 잠긴 아이템 항목에 CollectionBox가 1줄 생성되고,
    해금된 아이템이 4개라면 해금된 아이템 항목에 CollectionBox가 2줄 생성되는 식이다.

--- Assets/Scripts/Items/ItemCollection.cs
+++ Assets/Scripts/Items/ItemCollection.cs
@@ -1,25 +1,62 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemCollection : MonoBehaviour
 {
+    [Header("CollectionBox Section")]
+    [SerializeField] private GameObject CollectionBox;
+    [SerializeField] private float IntervalY;
+    [SerializeField] private float IntervalX;
+
     [Header("Unlocked Section")]
-    [SerializeField] private  Item[] UnlockedItems;
-    [SerializeField] private Image[] UnlockedItemImages;
+    [SerializeField] private Transform Unlocked;
+    [SerializeField] private Item[] UnlockedItems;
 
     [Header("Locked Section")]
-    [SerializeField] private  Item[] LockedItems;
-    [SerializeField] private Image[] LockedItemImages;
+    [SerializeField] private Transform Locked;
+    [SerializeField] private Item[] LockedItems;
 
     private void Awake()
     {
+        var collection = CreateCollectionBox(LockedItems.Length, Locked);
+
         for (int i = 0; i < LockedItems.Length; i++)
         {
-            LockedItemImages[i].sprite = LockedItems[i].Sprite;
+            if (collection[i].GetChild(0).TryGetComponent(out Image image))
+            {
+
+                image.sprite = LockedItems[i].Sprite;
+            }
         }
+        collection = CreateCollectionBox(UnlockedItems.Length, Unlocked);
+
         for (int i = 0; i < UnlockedItems.Length; i++)
         {
-            UnlockedItemImages[i].sprite = UnlockedItems[i].Sprite;
+            if (collection[i].GetChild(0).TryGetComponent(out Image image))
+            {
+
+                image.sprite = UnlockedItems[i].Sprite;
+            }
         }
+    }
+
+    private List<Transform> CreateCollectionBox(int itemListLength, Transform parent)
+    {
+        var list = new List<Transform>();
+        int line = (itemListLength / 3) + 1;
+
+
+        for (int i = 0; i < line; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                var instance = Instantiate(CollectionBox, parent);
+                    instance.transform.localPosition = new Vector2(IntervalX * j, IntervalY * i - 100f);
+
+                list.Add(instance.transform);
+            }
+        }
+        return list;
     }
 }
