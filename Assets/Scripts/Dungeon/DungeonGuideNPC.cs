1f64b858b60d18f10ac8485eb82e7c08ffa3a81a
kimbokChi <han15050424@gmail.com>
2020-12-10 21:15:00 +0900

    모든 NPC는 충전이 아닌, 버튼 클릭을 통해 상호작용하도록 한다

--- Assets/Scripts/Dungeon/DungeonGuideNPC.cs
+++ Assets/Scripts/Dungeon/DungeonGuideNPC.cs
@@ -2,40 +2,27 @@
 
 public class DungeonGuideNPC : MonoBehaviour
 {
-    [SerializeField]
-    private ChargeGauge Charge;
-
-    [SerializeField]
-    private GameObject DungeonSelectWindow;
+    [SerializeField] private GameObject InteractionButton;
+    [SerializeField] private GameObject DungeonSelectWindow;
 
     private bool mHasPlayer;
-
-    private void Reset()
-    {
-        Charge = FindObjectOfType(typeof(ChargeGauge)) as ChargeGauge;
-    }
 
     private void Awake()
     {
         mHasPlayer = false;
-
-        Charge.DisChargeEvent += Interact; 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) mHasPlayer = true;
+        if (collision.CompareTag("Player")) InteractionButton.SetActive(mHasPlayer = true);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) mHasPlayer = false;
+        if (collision.CompareTag("Player")) InteractionButton.SetActive(mHasPlayer = false);
     }
 
-    private void Interact()
+    public void Interact()
     {
-        if (mHasPlayer)
-        {
-            DungeonSelectWindow.SetActive(true);
-        }
+        DungeonSelectWindow.SetActive(!DungeonSelectWindow.activeSelf);
     }
 }
