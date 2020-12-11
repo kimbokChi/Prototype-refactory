4ec3bb9e231e991b407c666c7e1cc18c87d5f9a2
kimbokChi <han15050424@gmail.com>
2020-12-11 09:28:46 +0900

    던전 클리어 UI의 스크롤이 제대로 보이게끔 수정하고, 아이템을 해금하는 기능을 추가한다

---
+++ Assets/Scripts/Dungeon/DungeonClearUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DungeonClearUI : MonoBehaviour
+{
+    [SerializeField] private Item[] UnlockItems;
+
+    [SerializeField] private Image[] ItemBoxes;
+    
+    private void Awake()
+    {
+        for (int i = 0; i < UnlockItems.Length; ++i)
+        {
+            ItemBoxes[i].sprite = UnlockItems[i].Sprite;
+        }
+        ItemLibrary.Instance.ItemUnlock(UnlockItems);
+    }
+}
