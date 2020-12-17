3311a4d84ff8b56911b5700c78cdf80ca181b5b0
kimbokChi <han15050424@gmail.com>
2020-12-18 08:38:04 +0900

    수리검 아이템의 스크립트를 추가한다
    추가한 스크립트의 이름은 Shuriken으로, 아직가지는 Item의 추상 함수만 구현한 상태이다.
    덧붙여, json파일상의 수리검의 명칭을 Dagger에서 Shuriken으로 변경한다.

---
+++ Assets/Scripts/Items/Intance/Shuriken.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuriken : Item
+{
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        switch (offSlot)
+        {
+            case SlotType.Accessory:
+                {
+
+                }
+                break;
+
+            case SlotType.Weapon:
+                {
+
+                }
+                break;
+        }
+    }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        switch (onSlot)
+        {
+            case SlotType.Accessory:
+                {
+
+                }
+                break;
+
+            case SlotType.Weapon:
+                {
+
+                }
+                break;
+        }
+    }
+}
