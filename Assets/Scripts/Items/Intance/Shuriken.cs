0a054458e760949b7c8eb2a8f3aaa9f19536bc9e
kimbokChi <han15050424@gmail.com>
2020-12-18 11:18:41 +0900

    수리검 아이템을 반절정도 완성한다
    - 수리검 아이템의 애니메이션을 기존의 다른 아이템들과 마찬가지로, 자식 오브젝트 요소의
      값을 조절하는 방식으로 변경한다.
    - 수리검 아이템의 프리팹과, 그에 사용되는 투사체 프리팹을 추가한다(애니메이션 포함)
    - RegisteredItem스크립터블 오브젝트의 해금된 아이템에 수리검을 추가한다.
    - 수리검 아이템의 스크립트를 통해, 1번 타입의 공격을 구현한다.

--- Assets/Scripts/Items/Intance/Shuriken.cs
+++ Assets/Scripts/Items/Intance/Shuriken.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttackType
+{
+    Continuous, ThreeDirection
+}
+
 public class Shuriken : Item
 {
+    [SerializeField] private Animator Animator;
+    [SerializeField] private Projection Projection;
+
+    [SerializeField] private float ShootSpeed;
+    [SerializeField] private AttackType AttackType;
+
+    private Pool<Projection> _ShurikenPool;
+    private bool _IsAlreadyInit = false;
+
+    private GameObject _Player;
+
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        _Player = attacker;
+
+        Animator.SetBool   (Animator.GetParameter(0).nameHash, true);
+        Animator.SetInteger(Animator.GetParameter(1).nameHash, (int)AttackType);
+    }
+
     public override void OffEquipThis(SlotType offSlot)
     {
+        Init();
+
         switch (offSlot)
         {
             case SlotType.Accessory:
                 {
-
+                    Inventory.Instance.MoveBeginAction += Accessory_MoveBeginAction;
                 }
                 break;
 
@@ -24,11 +50,13 @@ public class Shuriken : Item
 
     public override void OnEquipThis(SlotType onSlot)
     {
+        Init();
+
         switch (onSlot)
         {
             case SlotType.Accessory:
                 {
-
+                    Inventory.Instance.MoveBeginAction -= Accessory_MoveBeginAction;
                 }
                 break;
 
@@ -38,5 +66,61 @@ public class Shuriken : Item
                 }
                 break;
         }
+    }
+
+    private void ShootShuriken()
+    {
+        var direction = Vector2.right;
+
+        if (_Player.transform.localRotation.eulerAngles.y > 0f)
+        {
+            direction = Vector2.left;
+        }
+        _ShurikenPool.Get().Shoot(transform.GetChild(0).position, direction, ShootSpeed);
+        MainCamera.Instance.Shake(0.1f, 0.8f);
+    }
+
+    private void Accessory_MoveBeginAction(Vector2 dir)
+    {
+
+    }
+
+    private IEnumerator EAccessory_MoveBeginAction(Vector2 dir)
+    {
+        yield return null;
+    }
+
+    private void Init()
+    {
+        if (!_IsAlreadyInit)
+        {
+            _IsAlreadyInit = true;
+
+            _ShurikenPool = new Pool<Projection>();
+            _ShurikenPool.Init(3, Projection, o => 
+            {
+                o.SetAction(
+                    hit => 
+                    {
+                        if (hit.TryGetComponent(out ICombatable combatable))
+                        {
+                            combatable.Damaged(StatTable[ItemStat.AttackPower], _Player);
+
+                            MainCamera.Instance.Shake(0.1f, 0.8f);
+                        }
+                    }, 
+                    pro => 
+                    {
+                        _ShurikenPool.Add(pro);
+                    });
+            });
+        }
+    }
+
+    protected override void AttackAnimationPlayOver()
+    {
+        base.AttackAnimationPlayOver();
+
+        Animator.SetBool(Animator.GetParameter(0).nameHash, false);
     }
 }
