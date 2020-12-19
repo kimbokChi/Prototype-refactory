07766ff9a71e9c461003a78ab296636281987d78
kimbokChi <han15050424@gmail.com>
2020-12-19 16:04:17 +0900

    얼음 구두 아이템의 스크립트를 추가한다
    얼믕 구두는 아이템 자체의 공격능력은 없는 대신, 얼음 기둥을 소환하는 아이템이다.
    얼음 기둥은 Area스크립트를 포함하는 오브젝트이다.

---
+++ Assets/Scripts/Items/Intance/FrozenShose.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrozenShose : Item
+{
+    [SerializeField] private Animator Animator;
+
+    [Header("Pillar Section")]
+    [SerializeField] private Area FrozenPillar;
+    [SerializeField] private Vector2 PillarOffset;
+
+    private Pool<Area> _PillarPool;
+    private bool _IsAlreadyInit = false;
+
+    private GameObject _Player;
+
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        _Player = attacker;
+
+        Animator.SetBool(Animator.GetParameter(0).nameHash, true);
+    }
+
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        // To do...
+    }
+
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        Init();
+
+        // To do...
+    }
+
+    private void Init()
+    {
+        if (!_IsAlreadyInit)
+        {
+            _PillarPool = new Pool<Area>();
+            _PillarPool.Init(4, FrozenPillar, pillar =>
+            {
+                pillar.SetEnterAction(o => 
+                {
+                    if (o.TryGetComponent(out ICombatable combatable))
+                    {
+
+                        combatable.Damaged(StatTable[ItemStat.AttackPower], _Player);
+                    }
+                }); 
+            });
+
+            _IsAlreadyInit = true;
+        }
+    }
+
+    // 애니메이션 이벤트로 실행된다
+    private void SummonFrozenPillar(Vector2 summonPoint)
+    {
+        var pillar = _PillarPool.Get();
+
+        pillar.transform.position = summonPoint + PillarOffset;
+    }
+}
