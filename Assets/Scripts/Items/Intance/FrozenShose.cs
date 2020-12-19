89c43ac5552fcd0cfd6b4a3d4d3c0683f1dd65b0
kimbokChi <han15050424@gmail.com>
2020-12-19 19:24:39 +0900

    얼음 구두를 완전히 구현한다
    얼음 구두는 장비한 상태에서 마우스 클릭으로 공격을 할 수 있으며, 공격은 현재 플레이어가
    있는 방에 무작위로 얼음 기둥을 4개 소환하는 것이다.

--- Assets/Scripts/Items/Intance/FrozenShose.cs
+++ Assets/Scripts/Items/Intance/FrozenShose.cs
@@ -8,37 +8,34 @@ public class FrozenShose : Item
 
     [Header("Pillar Section")]
     [SerializeField] private Projection FrozenPillar;
-    [SerializeField] private Vector2 PillarOffset;
+    [SerializeField] private float PillarOffsetY;
 
     private Pool<Projection> _PillarPool;
     private bool _IsAlreadyInit = false;
+    private int _AnimControlKey;
 
     private GameObject _Player;
 
     protected override void AttackAnimationPlayOver()
     {
-        Animator.SetBool(Animator.GetParameter(0).nameHash, false);
-
         base.AttackAnimationPlayOver();
+
+        Animator.SetBool(_AnimControlKey, false);
     }
 
     public override void AttackAction(GameObject attacker, ICombatable combatable)
     {
         _Player = attacker;
 
-        Animator.SetBool(Animator.GetParameter(0).nameHash, true);
+        Animator.SetBool(_AnimControlKey, true);
     }
 
     public override void OffEquipThis(SlotType offSlot)
-    {
-        // To do...
-    }
+    { }
 
     public override void OnEquipThis(SlotType onSlot)
     {
         Init();
-
-        // To do...
     }
 
     private void Init()
@@ -59,21 +56,27 @@ public class FrozenShose : Item
                     _PillarPool.Add(pillar);
                 }); 
             });
+            _AnimControlKey = Animator.GetParameter(0).nameHash;
 
             _IsAlreadyInit = true;
         }
     }
 
-    private void Shake()
-    {
-        MainCamera.Instance.Shake(0.3f, 1.1f);
-    }
-
     // 애니메이션 이벤트로 실행된다
-    private void SummonFrozenPillar(Vector2 summonPoint)
+    private void SummonFrozenPillar()
     {
-        var pillar = _PillarPool.Get();
+        MainCamera.Instance.Shake(0.35f, 1.3f);
 
-        pillar.transform.position = summonPoint + PillarOffset;
+        var playerMovePoints = Castle.Instance.GetPlayerRoom().GetMovePoints();
+
+        for (int i = 0; i < 2; i++) {
+
+            for (int j = 0; j < 2; j++)
+            {
+                float pointX = Random.Range(playerMovePoints[i].x, playerMovePoints[i + 1].x);
+
+                _PillarPool.Get().Shoot(new Vector3(pointX, _Player.transform.position.y + PillarOffsetY, 0f), Vector2.zero, 0f);
+            }
+        }
     }
 }
