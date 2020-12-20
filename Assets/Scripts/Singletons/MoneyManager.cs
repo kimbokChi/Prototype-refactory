351909cf445276f2cf108cb799a87f58476aac70
kimbokChi <han15050424@gmail.com>
2020-12-21 08:52:47 +0900

    재화라는 개념을 관리해줄 MoneyManager를 추가한다
    유저가 가진 재화에대한 관리를 해주는 싱글톤 스크립트이다.

---
+++ Assets/Scripts/Singletons/MoneyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : Singleton<MoneyManager>
+{
+    public int Money
+    {
+        get;
+        private set;
+    }
+
+    public void Addition(int increasement)
+    {
+        Money += increasement;
+    }
+    public bool Subtract(int decreasement)
+    {
+        if (Money >= decreasement)
+        {
+            Money -= decreasement;
+
+            return true;
+        }
+        return false;
+    }
+}
