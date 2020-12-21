57d87de73ce29f4c161f27aa142dabdf67149e8e
kimbokChi <han15050424@gmail.com>
2020-12-21 22:39:03 +0900

    행상인 NPC의 스크립트를 추가한다
    행상인 던전 클리어로 등장하며, 중복이 가능한 무작위 아이템 하나를 보여주고
    그 아이템을 기본 가격보다 더 비싸게 판매하는 NPC이다.

---
+++ Assets/Scripts/Dungeon/PeddlerNPC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeddlerNPC : MonoBehaviour
+{
+    [SerializeField] private Canvas WorldSpaceCanvas;
+
+    [Header("Item Section")]
+    [SerializeField] private SpriteRenderer ShowItemRenderer;
+    [Range(1f, 2f)]
+    [SerializeField] private float ItemCostScalling;
+    [SerializeField] private TMPro.TextMeshProUGUI ItemCostText;
+    [SerializeField] private TMPro.TextMeshProUGUI ItemNameText;
+
+    private int _ItemCost;
+    private Item _ContainItem;
+
+    private void OnEnable()
+    {
+        var list = ItemLibrary.Instance.GetUnlockedItemListForTest();
+
+        _ContainItem = list[Random.Range(0, list.Count)];
+
+        ShowItemRenderer.sprite = _ContainItem.Sprite;
+        _ItemCost = (int)System.Math.Ceiling(_ContainItem.Cost * ItemCostScalling);
+
+        ItemCostText.text = _ItemCost.ToString();
+        ItemNameText.text = _ContainItem.NameKR;
+    }
+}
