67122c05367093914ad62bd04ae3363235e2ca1f
kimbokChi <han15050424@gmail.com>
2020-12-21 13:19:19 +0900

    Effect의 라이프타임을 설정할 수 있도록 한다
    LifeTime의 값은 기본이 0이고, 이 값이 0보다 클때 설정한 값을 라이프타임으로 사용하도록 한다.
    라이프타임이 끝나게 되면 다른 이펙트들과 마찬가지로 LifeTimeOver함수를 실행한다.

--- Assets/Scripts/Effect/Effect.cs
+++ Assets/Scripts/Effect/Effect.cs
@@ -7,16 +7,29 @@ public class Effect : MonoBehaviour
 {
     public event Action OnDisableEvent;
 
-    [SerializeField]
-    private EffectKind EffectKind;
+    [SerializeField] private EffectKind EffectKind;
+    [SerializeField] private float LifeTime;
 
     public void LifeTimeOver() 
     {
         EffectLibrary.Instance.UnUsingEffect(EffectKind, this);
     }
+    private void OnEnable()
+    {
+        if (LifeTime > 0f)
+        {
+            StartCoroutine(ELifeTime());
+        }
+    }
     private void OnDisable()
     {
         OnDisableEvent?.Invoke();
         OnDisableEvent = null;
+    }
+    private IEnumerator ELifeTime()
+    {
+        yield return new WaitForSeconds(LifeTime);
+
+        LifeTimeOver();
     }
 }
