34b317a0fe32a13bd606be8b87ac7f6728cfc3d4
kimbokChi <han15050424@gmail.com>
2020-12-21 19:28:11 +0900

    아이템 상점에서 아이템을 구매하는 기능을 구현한다
    아이템 블록이 클릭되면 ItemStore에게 알리고, ItemStore는 선택된 아이템 블록을 기억한다.
    그리고 '구매하기' 버튼을 누른다면 선택된 아이템 블록이 포함하는 아이템을 생성하고,
    인벤토리에 추가한다.

--- Assets/Scripts/General/ItemShowBlock.cs
+++ Assets/Scripts/General/ItemShowBlock.cs
@@ -5,21 +5,38 @@ using UnityEngine.UI;
 
 public class ItemShowBlock : MonoBehaviour
 {
+    [SerializeField] private ItemStore Store;
+
     [SerializeField] private Text ItemNameText;
     [SerializeField] private Text ItemCostText;
 
     [SerializeField] private Image ItemImage;
 
-    private Item _ContainItem;
+    public int ItemCost
+    {
+        get;
+        private set;
+    }
+    public Item ContainItem
+    {
+        get;
+        private set;
+    }
+
+    public void Selected()
+    {
+        Store.BlockSelect(this);
+    }
 
     public void SetBlock(Item blockItem)
     {
         int rating = (int)blockItem.Rating + 8;
 
-        _ContainItem = blockItem;
+        ContainItem = blockItem;
+        ItemCost = Random.Range(rating, rating * 3);
 
         ItemNameText.text = blockItem.GetType().ToString();
-        ItemCostText.text = Random.Range(rating, rating * 3).ToString();
+        ItemCostText.text = ItemCost.ToString();
 
         ItemImage.sprite = blockItem.Sprite;
     }
--- Assets/Scripts/General/ItemStore.cs
+++ Assets/Scripts/General/ItemStore.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,12 +10,13 @@ public class ItemStore : MonoBehaviour
     [SerializeField] private ItemShowBlock[] ItemBlocks;
 
     private bool _IsAlreadyInit = false;
+    private ItemShowBlock _SelectedBlock;
 
     private void Start()
     {
         if (!_IsAlreadyInit)
         {
-            var list = ItemLibrary.Instance.GetUnlockedItemListForTest();
+            var list = ItemLibrary.Instance.GetUnlockedItemListForTest().ToList();
 
             for (int i = 0; i < 3; i++)
             {
@@ -26,8 +28,22 @@ public class ItemStore : MonoBehaviour
             _IsAlreadyInit = true;
         }
     }
+    public void BlockSelect(ItemShowBlock block)
+    {
+        _SelectedBlock = block;
+    }
     public void Purchace()
     {
+        if (_SelectedBlock != null)
+        {
+            if (MoneyManager.Instance.SubtractMoney(_SelectedBlock.ItemCost))
+            {
+                var item = Instantiate(_SelectedBlock.ContainItem);
+                    item.transform.position = new Vector2(-10f, 0f);
 
+                Inventory.Instance.AddItem(item);
+                item.transform.parent = ItemStateSaver.Instance.transform;
+            }
+        }
     }
 }
