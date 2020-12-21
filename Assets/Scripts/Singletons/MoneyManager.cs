3d2acae76d178a09755309eb9201264108c6c27e
kimbokChi <han15050424@gmail.com>
2020-12-21 09:01:14 +0900

    보유한 돈을 표시하는 기능을 추가한다
    유저가 보유한 돈을 UI로 표시할 수 있는 기능을 추가한다.
    이 UI는 인벤토리에 표시되기 때문에 MoneyManager스크립트는 InventoryWindow에 귀속된다.

--- Assets/Scripts/Singletons/MoneyManager.cs
+++ Assets/Scripts/Singletons/MoneyManager.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class MoneyManager : Singleton<MoneyManager>
 {
+    [SerializeField]
+    private TMPro.TextMeshProUGUI MoneyText;
+
     public int Money
     {
         get;
         private set;
     }
+    public void TextUpdate()
+    {
+        MoneyText.text = Money.ToString();
+    }
 
     public void Addition(int increasement)
     {
         Money += increasement;
+
+        TextUpdate();
     }
     public bool Subtract(int decreasement)
     {
         if (Money >= decreasement)
         {
             Money -= decreasement;
+            TextUpdate();
 
             return true;
         }
