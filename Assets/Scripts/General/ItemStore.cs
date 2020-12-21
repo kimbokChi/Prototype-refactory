bcd4a9068b1017e3333575f823b03ddc60e64fc1
kimbokChi <han15050424@gmail.com>
2020-12-21 18:49:00 +0900

    아이템 상점의 아이템 목록을 띄우는 기능을 구현한다
    ItemStore스크립트를 통해서 구현된다. 목록에 등장하는 아이템은 이미 획득한 아이템일 수 있다.

---
+++ Assets/Scripts/General/ItemStore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public struct ItemShowBlock
+{
+    public Text ItemNameText;
+    public Text ItemCostText;
+
+    public Image ItemImage;
+}
+
+public class ItemStore : MonoBehaviour
+{
+    [SerializeField] private RegisteredItem RegisteredItem;
+    [SerializeField] private ItemShowBlock[] ItemBlocks;
+
+    private bool _IsAlreadyInit = false;
+
+    private void Start()
+    {
+        if (!_IsAlreadyInit)
+        {
+            var list = ItemLibrary.Instance.GetUnlockedItemListForTest();
+
+            for (int i = 0; i < 3; i++)
+            {
+                int  index = Random.Range(0, list.Count);
+                int rating = (int)list[index].Rating;
+
+                ItemBlocks[i].ItemImage.sprite = list[index].Sprite;
+
+                ItemBlocks[i].ItemNameText.text = list[index].GetType().ToString();
+                ItemBlocks[i].ItemCostText.text = Random.Range(8 + rating, 8 + 3 * rating).ToString();
+
+                list.RemoveAt(index);
+            }
+            _IsAlreadyInit = true;
+        }
+    }
+}
