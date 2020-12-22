0fb1867f5611b2bc5a0e9f597559024b90ff7627
kimbokChi <han15050424@gmail.com>
2020-12-22 10:23:49 +0900

    행상인으로부터 아이템을 구매한 여부에 따라 메시지를 변경하도록 한다
    아이템을 구매했다면 가격과 아이템 이름을 표시하는 텍스트를 비활성화하고,
    버튼 위에 띄우는 메시지에는 "구매완료!"를 표기하도록 한다.

--- Assets/Scripts/Dungeon/PeddlerNPC.cs
+++ Assets/Scripts/Dungeon/PeddlerNPC.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PeddlerNPC : MonoBehaviour
 {
     [Header("Item Section")]
+    [Range(1f, 2f)]
+    [SerializeField] private float ItemCostScalling;
     [SerializeField] private SpriteRenderer ShowItemRenderer;
     [SerializeField] private Animator ShowItemAnimator;
     [SerializeField] private GameObject InteractionUI;
-    [Range(1f, 2f)]
-    [SerializeField] private float ItemCostScalling;
+
+    [Header("Message Section")]
     [SerializeField] private TMPro.TextMeshProUGUI ItemCostText;
     [SerializeField] private TMPro.TextMeshProUGUI ItemNameText;
+    [SerializeField] private TMPro.TextMeshProUGUI  MessageText;
 
     private int _DropItemControlKey;
     private int _ItemCost;
@@ -26,6 +29,9 @@ public class PeddlerNPC : MonoBehaviour
         ShowItemRenderer.sprite = _ContainItem.Sprite;
         _ItemCost = (int)System.Math.Ceiling(_ContainItem.Cost * ItemCostScalling);
 
+        ItemCostText.gameObject.SetActive(true);
+        ItemNameText.gameObject.SetActive(true);
+
         ItemCostText.text = _ItemCost.ToString();
         ItemNameText.text = _ContainItem.NameKR;
 
@@ -33,6 +39,7 @@ public class PeddlerNPC : MonoBehaviour
         {
             _DropItemControlKey = ShowItemAnimator.GetParameter(0).nameHash;
         }
+        MessageText.text = "구매확인";
     }
     private void OnDisable()
     {
@@ -45,7 +52,10 @@ public class PeddlerNPC : MonoBehaviour
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) InteractionUI.SetActive(false);
+        if (collision.CompareTag("Player"))
+        {
+            InteractionUI.SetActive(false);
+        }
     }
 
     public void Purchace()
@@ -59,6 +69,11 @@ public class PeddlerNPC : MonoBehaviour
             item.transform.parent = ItemStateSaver.Instance.transform;
 
             ShowItemAnimator.SetBool(_DropItemControlKey, true);
+
+            ItemCostText.gameObject.SetActive(false);
+            ItemNameText.gameObject.SetActive(false);
+
+            MessageText.text = "구매완료!";
         }
     }
 }
