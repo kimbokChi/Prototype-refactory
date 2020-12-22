5d855465b230732f791391cf9d76619a6cfb9367
kimbokChi <han15050424@gmail.com>
2020-12-22 10:00:47 +0900

    아이템 행상인 NPC의 기능을 완전히 구현한다
    아이템 행상인에 플레이어가 접근하면, NPC는 구매를 확정할 것인지를 물어보는 버튼을
    띄우도록한다. 띄운 버튼을 클릭하면 제시된 비용만큼 돈이 차감되고 아이템을 지급한다.

--- Assets/Scripts/Dungeon/PeddlerNPC.cs
+++ Assets/Scripts/Dungeon/PeddlerNPC.cs
@@ -6,11 +6,14 @@ public class PeddlerNPC : MonoBehaviour
 {
     [Header("Item Section")]
     [SerializeField] private SpriteRenderer ShowItemRenderer;
+    [SerializeField] private Animator ShowItemAnimator;
+    [SerializeField] private GameObject InteractionUI;
     [Range(1f, 2f)]
     [SerializeField] private float ItemCostScalling;
     [SerializeField] private TMPro.TextMeshProUGUI ItemCostText;
     [SerializeField] private TMPro.TextMeshProUGUI ItemNameText;
 
+    private int _DropItemControlKey;
     private int _ItemCost;
     private Item _ContainItem;
 
@@ -25,5 +28,37 @@ public class PeddlerNPC : MonoBehaviour
 
         ItemCostText.text = _ItemCost.ToString();
         ItemNameText.text = _ContainItem.NameKR;
+
+        if (_DropItemControlKey == default)
+        {
+            _DropItemControlKey = ShowItemAnimator.GetParameter(0).nameHash;
+        }
+    }
+    private void OnDisable()
+    {
+        ShowItemAnimator.SetBool(_DropItemControlKey, false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) InteractionUI.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) InteractionUI.SetActive(false);
+    }
+
+    public void Purchace()
+    {
+        if (MoneyManager.Instance.SubtractMoney(_ItemCost))
+        {
+            var item = Instantiate(_ContainItem);
+                item.transform.position = new Vector2(-10f, 0f);
+
+            Inventory.Instance.AddItem(item);
+            item.transform.parent = ItemStateSaver.Instance.transform;
+
+            ShowItemAnimator.SetBool(_DropItemControlKey, true);
+        }
     }
 }
