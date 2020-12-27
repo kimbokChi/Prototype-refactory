961560a41c1d78e8d6f216bcb475610119f7d3ad
kimbokChi <han15050424@gmail.com>
2020-12-27 21:36:46 +0900

    아이템 슬롯에 마우스를 가져다대면, 아이템 정보 UI를 표시하도록 한다
    이 기능은 ItemSlot에게 IPointerEnterHandler와 IPointerExitHandler인터페이스를 추가하고,
    ItemInfoPopup이라는 싱글톤 객체를 통해 구현된다.
    
    ItemSlot은 두 인터페이스를 통해 커서가 자신을 가리킨다는 것을 확인하고, 아이템 정보 UI를
    활성화 시킨다. 아이템 정보 UI를 활성화 시키는 것은 ItemInfoPopup클래스가 담당하는데,
    ShowPopup이라는 함수를 통해서 활성화 시키고 ClosePopup이라는 함수를 통해 비활성화시킨다.

---
+++ Assets/Scripts/Items/ItemInfoPopup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfoPopup : Singleton<ItemInfoPopup>
+{
+    [SerializeField] private GameObject    _PopupObject;
+    [SerializeField] private RectTransform _RectTransform;
+
+    public void ShowPopup(DIRECTION9 pivot, Vector2 position)
+    {
+        switch (pivot)
+        {
+            case DIRECTION9.TOP_LEFT:
+                _RectTransform.pivot = new Vector2(0.0f, 1.0f);
+                break;
+            case DIRECTION9.TOP:
+                _RectTransform.pivot = new Vector2(0.5f, 1.0f);
+                break;
+            case DIRECTION9.TOP_RIGHT:
+                _RectTransform.pivot = new Vector2(1.0f, 1.0f);
+                break;
+            case DIRECTION9.MID_LEFT:
+                _RectTransform.pivot = new Vector2(0.0f, 0.5f);
+                break;
+            case DIRECTION9.MID:
+                _RectTransform.pivot = new Vector2(0.5f, 0.5f);
+                break;
+            case DIRECTION9.MID_RIGHT:
+                _RectTransform.pivot = new Vector2(1.0f, 0.5f);
+                break;
+            case DIRECTION9.BOT_LEFT:
+                _RectTransform.pivot = new Vector2(0.0f, 0.0f);
+                break;
+            case DIRECTION9.BOT:
+                _RectTransform.pivot = new Vector2(0.5f, 0.0f);
+                break;
+            case DIRECTION9.BOT_RIGHT:
+                _RectTransform.pivot = new Vector2(1.0f, 0.0f);
+                break;
+            default:
+                break;
+        }
+        _PopupObject.SetActive(true);
+
+        _RectTransform.position = position;
+    }
+
+    public void ClosePopup()
+    {
+        _PopupObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Items/Singleton/Inventory/ItemSlot.cs
+++ Assets/Scripts/Items/Singleton/Inventory/ItemSlot.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using System;
 
 public enum SlotType
 {
     Container, Accessory, Weapon
 }
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField]
-    private Sprite EmptySprite;
+    [SerializeField] private Sprite EmptySprite;
+    [SerializeField] private DIRECTION9 PopupPivot;
 
     public event Action<Item> ItemChangeEvent;
     public event Action<Item> ItemEquipEvent;
@@ -64,5 +65,15 @@ public class ItemSlot : MonoBehaviour
 
         SetItem(Finger.Instance.CarryItem);
                 Finger.Instance.CarryItem = containItem;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ItemInfoPopup.Instance.ShowPopup(PopupPivot, transform.position);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ItemInfoPopup.Instance.ClosePopup();
     }
 }
