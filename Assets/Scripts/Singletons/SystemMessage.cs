1308e2984e31df7fa4de9dee4aba8b838755eef5
kimbokChi <han15050424@gmail.com>
2020-12-27 14:43:34 +0900

    시스템 메시지 스크립트를 추가하고, 오브젝트에 적용시킨다
    시스템 메시지 스크립트는 싱글톤 객체로, 시스템 메시지 UI를 통해 메시지를 띄우는 기능을
    제공한다.
    
    이 기능을 성공적으로 제공하기 위해 InGameUI프리팹에 해당 스크립트를 포함하는 오브젝트를
    추가하고 그 자식으로 시스템 메시지 UI오브젝트를 종속시킨다.

---
+++ Assets/Scripts/Singletons/SystemMessage.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SystemMessage : Singleton<SystemMessage>
+{
+    [SerializeField] private GameObject MessageBox;
+    [SerializeField] private TMPro.TextMeshProUGUI MessageText;
+
+    public void ShowMessage(string message)
+    {
+        MessageBox.SetActive(true);
+
+        MessageText.text = message;
+    }
+}
