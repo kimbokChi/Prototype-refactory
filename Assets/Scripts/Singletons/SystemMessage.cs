dba884c42415080368c8e6f5886bc2c1adc5487a
kimbokChi <han15050424@gmail.com>
2020-12-27 15:01:04 +0900

    메시지 박스가 활성화된 동안에는 다른 입력을 할 수 없도록 한다
    메시지 박스가 활성화되면, 게임 시간을 0으로 설정하고 메시지를 제외한 요소들은 반투명하게
    표시되도록 한다.
    
    이 기능을 구현하기 위해, SystemMessage오브젝트에 반투명한 Background오브젝트를 추가한다.
    
    ShowMessage함수가 호출되면 Background오브젝트를 활성화시키고 게임 시간을 0으로 설정하며,
    CloseMessage함수를 추가한다. CloseMessage함수는 ShowMessage함수와 반대로 작동하는 함수로,
    UI의 '닫기'버튼을 클릭하면 실행된다.

--- Assets/Scripts/Singletons/SystemMessage.cs
+++ Assets/Scripts/Singletons/SystemMessage.cs
@@ -2,13 +2,24 @@
 
 public class SystemMessage : Singleton<SystemMessage>
 {
+    [SerializeField] private GameObject Background;
     [SerializeField] private GameObject MessageBox;
     [SerializeField] private TMPro.TextMeshProUGUI MessageText;
 
     public void ShowMessage(string message)
     {
+        Time.timeScale = 0f;
+
+        Background.SetActive(true);
         MessageBox.SetActive(true);
 
         MessageText.text = message;
+    }
+    public void CloseMessage()
+    {
+        Time.timeScale = 1f;
+
+        Background.SetActive(false);
+        MessageBox.SetActive(false);
     }
 }
