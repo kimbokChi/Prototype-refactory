7d2b728f76326f33159f92dcb2ba130dc626b548
kimbokChi <han15050424@gmail.com>
2021-01-04 18:18:41 +0900

    플레이어를 바라보고, 어느 방향을 바라보고 있는지에 대한 기능을 모듈화한다
    이렇게 만든 모듈의 이름은 RecognitionModule이다.
    - PlayerEnter/Exit함수를 통해 플레이어를 감지할 수 있는 상황인지를 확인한다.
    - SetLookingLeft함수를 통해 바라보는 방향을 설정한다.
    - IsLookAtPlayer함수를 통해 플레이어를 바라보고있는지의 여부를 반환한다.
    - IsLookAtLeft메서드를 통해 왼쪽을 바라보고있는지의 여부를 불러온다.

---
+++ Assets/Scripts/Enemies/Module/RecognitionModule.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class RecognitionModule : MonoBehaviour
+{
+    public bool IsLookAtLeft => _IsLookAtLeft;
+    private Player _Player;
+
+    [SerializeField] private bool _IsLookAtLeft;
+
+    public void PlayerEnter(Player player)
+    {
+        _Player = player;
+    }
+    public void PlayerExit()
+    {
+        _Player = null;
+    }
+    public void SetLookingLeft(bool lookAtLeft)
+    {
+        _IsLookAtLeft = lookAtLeft;
+
+        if (_IsLookAtLeft)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+        }
+        else
+            transform.rotation = Quaternion.Euler(Vector3.up * 180);
+    }
+    public bool IsLookAtPlayer()
+    {
+        if (_Player != null)
+        {
+            return (_Player.transform.position.x < transform.position.x &&  _IsLookAtLeft ||
+                    _Player.transform.position.x > transform.position.x && !_IsLookAtLeft);
+        }
+        return false;
+    }
+}
