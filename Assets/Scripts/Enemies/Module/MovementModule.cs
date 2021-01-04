19820354d2ef66e153dd252ba204a2d4d1a33bf4
kimbokChi <han15050424@gmail.com>
2021-01-04 17:35:14 +0900

    Enemy의 행동을 모듈화한다
    Enemy를 구성하는 이동, 공격, 인지등의 행동을 각각의 모듈로 구분하여 사용하기로 한다.
    이 계획에 첫 걸음으로, '이동'을 모듈화한 클래스 MovementModule을 추가한다.

---
+++ Assets/Scripts/Enemies/Module/MovementModule.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class MovementModule : MonoBehaviour
+{
+    #region
+    /// <summary>
+    /// parameter : 이동지점이 왼쪽에 있는지의 여부
+    /// </summary>
+    #endregion
+    public event Action<bool> BeginOfMovementEvent;
+    public event Action         EndOfMovementEvent;
+
+    public Func<bool> CanMovement;
+    public Func<bool> IsLookAtPlayer;
+    public Func<bool> IsLookAtLeft;
+
+    [Header("Movement Module")]
+    [SerializeField] private Vector2 _BasePosition;
+    [SerializeField] private float _WaitForMovementMin;
+    [SerializeField] private float _WaitForMovementMax;
+
+    private IEnumerator _EMove;
+    private AbilityTable _AbilityTable;
+
+    public void Init(AbilityTable abilityTable)
+    {
+        _AbilityTable = abilityTable;
+
+        CanMovement    = () => false;
+        IsLookAtPlayer = () => false;
+        IsLookAtLeft   = () => false;
+    }
+    public bool IsMoving()
+    {
+        return _EMove != null;
+    }
+    public void MoveStop()
+    {
+        if (_EMove != null)
+        {
+            StopCoroutine(_EMove);
+            _EMove = null;
+        }
+    }
+    private IEnumerator MoveRoutine()
+    {
+        while (_AbilityTable[Ability.CurHealth] > 0)
+        {
+            yield return new WaitUntil(CanMovement);
+            float waitTime = UnityEngine.Random.Range(_WaitForMovementMin, _WaitForMovementMax);
+
+            yield return new WaitForSeconds(waitTime);
+            yield return new WaitUntil(CanMovement);
+
+            Vector2 movePoint = _BasePosition;
+
+            if (IsLookAtPlayer())
+            {
+                if (IsLookAtLeft())
+                {
+                    movePoint.x = -3.5f;
+                }
+                else
+                    movePoint.x = +3.5f;
+            }
+            else
+            {
+                movePoint.x += UnityEngine.Random.Range(-3.5f, 3.5f);
+            }
+            StartCoroutine(_EMove = Move(movePoint));
+
+            BeginOfMovementEvent?.Invoke(movePoint.x < transform.localPosition.x);
+        }
+    }
+    private IEnumerator Move(Vector2 movePoint)
+    {
+        Vector3 direction = (movePoint.x > transform.localPosition.x)
+            ? Vector3.right : Vector3.left;
+
+        float DeltaTime()
+        {
+            return Time.deltaTime * Time.timeScale;
+        }
+        bool CanMoving()
+        {
+            return direction.x > 0 && transform.localPosition.x < movePoint.x ||
+                   direction.x < 0 && transform.localPosition.x > movePoint.x;
+        }
+        do
+        {
+            transform.localPosition += direction * _AbilityTable.MoveSpeed * DeltaTime();
+
+            yield return null;
+
+        } while (CanMoving());
+        transform.localPosition = movePoint;
+
+        EndOfMovementEvent?.Invoke();
+        _EMove = null;
+    }
+}
