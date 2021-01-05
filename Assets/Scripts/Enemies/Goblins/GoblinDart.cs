b3bfb6ee457eb7db0e5ca963cee72e8f75b9e859
kimbokChi <han15050424@gmail.com>
2021-01-05 11:33:18 +0900

    다트 고블린을 리팩토링한다
    원거리 공격, 이동, 인지 모듈을 사용하여 GoblinDart라는 고유의 스크립트를 추가한다.
    
    기존의 다트 고블린 프리팹을 제거하고 새로운 프리팹으로 추가하며, 다트 고블린이 존재했던
    방에 존재하는 다트 고블린또한 변경한다.
    
    애니메이터는 FixedDuration을 0으로 설정하였고 공격, 사망, 피격 애니메이션을 조정했다.
    결과는 성공적이다 앓~

---
+++ Assets/Scripts/Enemies/Goblins/GoblinDart.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GoblinDart : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
+{
+    [Header("Ability")]
+    [SerializeField] private AbilityTable _AbilityTable;
+    [SerializeField] private EnemyAnimator _EnemyAnimator;
+
+    [Header("Modules")]
+    [SerializeField] private MovementModule _Movement;
+    [SerializeField] private RecognitionModule _Recognition;
+    [SerializeField] private LongRangeModule _AttackModule;
+
+    public AbilityTable GetAbility => _AbilityTable;
+
+    public void AnimationPlayOver(AnimState anim)
+    {
+        switch (anim)
+        {
+            case AnimState.Attack:
+                {
+                    _EnemyAnimator.ChangeState(AnimState.Idle);
+
+                    _AttackModule.PeriodAttackPartOver();
+                }
+                break;
+
+            case AnimState.Damaged:
+                {
+                    if (_Movement.IsMoving())
+                    {
+                        _EnemyAnimator.ChangeState(AnimState.Move);
+                    }
+                    else
+                        _EnemyAnimator.ChangeState(AnimState.Idle);
+                }
+                break;
+
+            case AnimState.Death:
+                gameObject.SetActive(false);
+                break;
+        }
+    }
+    public void Damaged(float damage, GameObject attacker)
+    {
+        EffectLibrary.Instance.UsingEffect(EffectKind.Damage, transform.position);
+
+        _EnemyAnimator.ChangeState(AnimState.Damaged);
+        if ((_AbilityTable.Table[Ability.CurHealth] -= damage) <= 0)
+        {
+            _AttackModule.SetActivePeriod(false);
+
+            _Movement.MoveStop();
+            _EnemyAnimator.ChangeState(AnimState.Death);
+            HealthBarPool.Instance.UnUsingHealthBar(transform);
+        }
+    }
+
+    public void IInit()
+    {
+        _EnemyAnimator.Init();
+        HealthBarPool.Instance.UsingHealthBar(-1f, transform, _AbilityTable);
+
+        _AttackModule.Init(gameObject, _AbilityTable);
+        _AttackModule.RunningDrive();
+        _AttackModule.SetPeriodAction(Period.Begin, () =>
+        {
+            _Movement.MoveStop();
+        });
+        _AttackModule.SetPeriodAction(Period.Attack, () =>
+        {
+            _EnemyAnimator.ChangeState(AnimState.Attack);
+        });
+
+        MovementModuleInit();
+    }
+    public void IUpdate()
+    {
+        if (_AbilityTable[Ability.CurHealth] > 0)
+        {
+
+            if (!_AttackModule.IsPeriodProgressing())
+            {
+                if (_AttackModule.RangeHasAny() && _Recognition.IsLookAtPlayer())
+                {
+
+                    _Movement.MoveStop();
+                    _AttackModule.SetActivePeriod(true);
+                }
+            }
+        }
+    }
+    private void AttackAction()
+    {
+        Vector2 direction;
+
+        if (_Recognition.IsLookAtLeft)
+        {
+            direction = Vector2.left;
+        }
+        else
+        {
+            direction = Vector2.right;
+        }
+        _AttackModule.ShootProjection(direction);
+    }
+    private void MovementModuleInit()
+    {
+        _Movement.SetMovementEvent(_Recognition,
+        () =>
+        {
+            _EnemyAnimator.ChangeState(AnimState.Move);
+        },
+        () =>
+        {
+            _EnemyAnimator.ChangeState(AnimState.Idle);
+        });
+        _Movement.SetMovementLogic(_Recognition,
+        () =>
+        {
+            return _EnemyAnimator.CurrentState() == AnimState.Idle &&
+                  !_AttackModule.IsPeriodProgressing();
+        });
+        _Movement.RunningDrive(_AbilityTable);
+    }
+
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        if (_AbilityTable.CanRecognize(message))
+        {
+            _Recognition.PlayerEnter(enterPlayer);
+        }
+    }
+    public void PlayerExit(MESSAGE message)
+    {
+        if (_AbilityTable.CantRecognize(message))
+        {
+            _Recognition.PlayerExit();
+        }
+    }
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+    public GameObject ThisObject()
+    {
+        return gameObject;
+    }
+    public void CastBuff(Buff buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+}
