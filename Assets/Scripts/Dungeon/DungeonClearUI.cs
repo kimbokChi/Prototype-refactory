a5d702cfeb286e8254f9cf85a19c00d273c9addd
kimbokChi <han15050424@gmail.com>
2021-01-05 22:02:49 +0900

    던전을 거듭 클리어 한다면 메시지가 다르게 표기되도록 한다
    던전이 해금 되었든, 이미 해금되었든 간에 던전에 대한 정보는 표시하되,
    그 위에 표시하는 메시지를 다르게 하도록 한다.

--- Assets/Scripts/Dungeon/DungeonClearUI.cs
+++ Assets/Scripts/Dungeon/DungeonClearUI.cs
@@ -11,6 +11,7 @@ public class DungeonClearUI : MonoBehaviour
 
     [Header("UnlockDungeon Para")]
     [SerializeField] private Image _DungeonInfoImage;
+    [SerializeField] private TMPro.TextMeshProUGUI _DungeonUnlockMessage;
     [SerializeField] private TMPro.TextMeshProUGUI _DungeonTitle;
     [SerializeField] private TMPro.TextMeshProUGUI _DungeonComment;
 
@@ -36,14 +37,21 @@ public class DungeonClearUI : MonoBehaviour
         ClearTime.text = $"{clearMin:D2} : {clearSec:D2}";
         KillCount.text = $"{GameLoger.Instance.KillCount:D3} 마리";
 
+
+        _DungeonTitle.text       = _UnlockDungeonInfo.UnLockedTitle;
+        _DungeonInfoImage.sprite = _UnlockDungeonInfo.UnLockedSprite;
+        _DungeonComment.text     = _UnlockDungeonInfo.UnLockedComment;
+
         // 아직 해금되지 않은 던전일 때에만
         if (_UnlockDungeonIndex > GameLoger.Instance.UnlockDungeonIndex)
         {
             GameLoger.Instance.RecordStageUnlock();
 
-            _DungeonTitle.text       = _UnlockDungeonInfo.UnLockedTitle;
-            _DungeonInfoImage.sprite = _UnlockDungeonInfo.UnLockedSprite;
-            _DungeonComment.text     = _UnlockDungeonInfo.UnLockedComment;
+            _DungeonUnlockMessage.text = "해금된 던전";
+        }
+        else
+        {
+            _DungeonUnlockMessage.text = "이미 해금됨";
         }
     }
 
