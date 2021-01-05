7ddebf012ec8aefb0a1afec69e2a207f3cf29ba9
kimbokChi <han15050424@gmail.com>
2021-01-05 18:19:51 +0900

    던전 선택지가 해금 여부에 따라 표기하는 정보를 저장하는 스크립터블 오브젝트를 추가한다
    이렇게 추가한 스크립터블 오브젝트의 이름은 DungeonSelectionInfo로,
    잠겨있을때/열려있을때의 이미지, 타이틀, 코멘트를 저장하고 이에 대해 외부에서
    접근할 수 있는 접근점을 제공하는 스크립트이다.
    
    구체적으로 구분되지 않은 스크립터블 오브젝트의 스크립트를 분류한다.

---
+++ Assets/Scripts/ScriptableObject/DungeonSelectionInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "SelectionInfo", menuName = "ScriptableObject/Dungeon/SelectionInfo")]
+public class DungeonSelectionInfo : ScriptableObject
+{
+    [Header("DungeonLocked Section")]
+    [TextArea(1, 1)] 
+    [SerializeField] private string _LockedTitle;
+    [TextArea(3, 4)] 
+    [SerializeField] private string _LockedComment;
+    [SerializeField] private Sprite _LockedSprite;
+    public string LockedTitle
+    { get => _LockedTitle; }
+    public string LockedComment
+    { get => _LockedComment; }
+    public Sprite LockedSprite 
+    { get => _LockedSprite; }
+
+
+    [Header("DungeonUnLocked Section")]
+    [TextArea(1, 1)]
+    [SerializeField] private string _UnLockedTitle;
+    [TextArea(3, 4)]
+    [SerializeField] private string _UnLockedComment;
+    [SerializeField] private Sprite _UnLockedSprite;
+    public string UnLockedTitle
+    { get => _UnLockedTitle; }
+    public string UnLockedComment
+    { get => _UnLockedComment; }
+    public Sprite UnLockedSprite
+    { get => _UnLockedSprite; }
+}
