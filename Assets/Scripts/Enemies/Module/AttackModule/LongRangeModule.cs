12b6f3684d1d15cc810ddb60faf9f1624de027e2
kimbokChi <han15050424@gmail.com>
2021-01-05 11:22:17 +0900

    원거리 공격 모듈을 추가한다
    Pool을 사용하여 Projection을 관리하는 운거리 공격 모듈을 추가한다.
    이 모듈을 통해 사용한 투사체는 적중하면 대상에게 피해를 입히도록 되어있디.
    
    ShootProjection함수를 통해 지정한 방향으로 투사체를 발사하고,
    AddTargetHitAction함수 또는 TargetHitEvent를 통해 목표를 맞췄을 때의 행동을 지정할 수 있고,
    마찬가지로 AddLifeOverAction함수 도는 LifeOverEvent을 통해 투사체의 효력이 다했을 때의
    행동을 지정할 수 있다.

---
+++ Assets/Scripts/Enemies/Module/AttackModule/LongRangeModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class LongRangeModule : AttackModule
+{
+    public event Action<GameObject> TargetHitEvent;
+    public event Action<Projection>  LifeOverEvent;
+
+    [SerializeField] private Area _Range;
+
+    [Header("Prohection Section")]
+    [SerializeField] private Projection _Projection;
+    [SerializeField] private int _HoldingCount;
+
+    [Header("Shoot Section")]
+    [SerializeField] private Vector3   _ShootPosition;
+    [SerializeField] private Transform _ShootPivot;
+    [SerializeField] private float     _ShootSpeed;
+    
+    private Pool<Projection> _Pool;
+
+    public bool RangeHasAny()
+    {
+        return _Range.HasAny();
+    }
+    public void RunningDrive()
+    {
+        // Pool Setting
+        _Pool = new Pool<Projection>();
+        _Pool.Init(_HoldingCount, _Projection, pro => 
+        {
+            pro.SetAction(
+                hit => TargetHitEvent?.Invoke(hit), 
+                lif =>  LifeOverEvent?.Invoke(lif));
+        });
+
+        // Default Event Setting
+        TargetHitEvent += hit =>
+        {
+            if (hit.TryGetComponent(out ICombatable combatable))
+            {
+                combatable.Damaged(_AbilityTable.AttackPower, _User);
+            }
+        };
+        LifeOverEvent += lif => _Pool.Add(lif);
+
+        _Range.SetScale(_AbilityTable.Range);
+    }
+    public void ShootProjection(Vector2 direction)
+    {
+        var projection = _Pool.Get();
+
+        projection.Shoot(_ShootPivot.position + _ShootPosition, direction, _ShootSpeed);
+    }
+    public void AddTargetHitAction(Action<GameObject> action)
+    {
+        TargetHitEvent += hit => action.Invoke(hit);
+    }
+    public void AddLifeOverAction(Action<Projection> action)
+    {
+        LifeOverEvent += lif => action.Invoke(lif);
+    }
+}
