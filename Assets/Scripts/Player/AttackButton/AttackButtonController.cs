6eec06453c9b72f682d4e9d58543ed1b9088173b
kimbokChi <han15050424@gmail.com>
2021-01-07 10:52:37 +0900

    잘못된 클래스의 이름을 수정한다
    AttackButtonControlor를 AttackButtonController으로 변경한다.

--- Assets/Scripts/Player/AttackButton/AttackButtonController.cs
+++ Assets/Scripts/Player/AttackButton/AttackButtonController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackButtonControlar : Singleton<AttackButtonControlar>
+public class AttackButtonController : Singleton<AttackButtonController>
 {
     public bool IsButtonHide
     {
