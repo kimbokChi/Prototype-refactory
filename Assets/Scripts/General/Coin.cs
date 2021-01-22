772732027552bbc9383a30115038ece6f0b9818d
kimbokChi <han15050424@gmail.com>
2021-01-22 16:45:13 +0900

    코인의 행동을 제어하는 스크립트를 추가한다
    이렇게 추가한 스크립트의 이름은 Coin으로,
    현재 하는 행동은 플레이어와 접촉했을 때, 코인을 획득하는 애니메이션으로 전환하도록 하는 것이다.

---
+++ Assets/Scripts/General/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private Animator _Animator;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _Animator.SetBool(_Animator.GetParameter(0).nameHash, true);
+        }
+    }
+}
