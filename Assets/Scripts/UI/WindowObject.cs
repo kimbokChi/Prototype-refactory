bae4b90fac7879c3aed1e0dedee79c0d38638fa5
kimbokChi <han15050424@gmail.com>
2021-01-22 12:17:39 +0900

    윈도우가 활성활 될 때 다른 윈도우는 활성화하지 못하도록 한다
    윈도우 형태의 UI에게 WindowObject스크립트를 추가하여, 윈도우가 활성화 될 때
    다른 UI의 입력을 차단하는 UI를 활성화시키도록 한다.
    
    덧붙여, 이러한 UI스크립트들을 UI라는 이름의 폴더로 구분한다.

---
+++ Assets/Scripts/UI/WindowObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WindowObject : MonoBehaviour
+{
+    [SerializeField] private GameObject _BlockingLayer;
+
+    private void OnEnable()
+    {
+        _BlockingLayer.SetActive(true);
+    }
+    private void OnDisable()
+    {
+        _BlockingLayer.SetActive(false);
+    }
+}
