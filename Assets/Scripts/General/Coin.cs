40031f27ba5e1b96b3e4b756d2cf7346c3ce7b00
kimbokChi <han15050424@gmail.com>
2021-01-22 17:49:24 +0900

    코인이 통통 튀도록 한다
    코드를 통해서 이를 구현한다. 통통 뛰는 범위는 화면을 벗어나지 않는다!

--- Assets/Scripts/General/Coin.cs
+++ Assets/Scripts/General/Coin.cs
@@ -6,17 +6,32 @@ public class Coin : MonoBehaviour
 {
     public int Value;
 
+    [Header("PopAnimation")]
+    [SerializeField] private float _PopAnimationTime;
+    [Range(0f, 1f)]
+    [SerializeField] private float _PopRange;
+
+    [Header("Animator")]
     [SerializeField] private Animator _Animator;
+    
     private int _AnimationHash;
+    private Coroutine _PopRoutine;
 
     private void OnEnable()
     {
+        if (_PopRoutine == null)
+        {
+            _PopRoutine = new Coroutine(this);
+        }
+        _PopRoutine.StartRoutine(PopAnimation());
         _AnimationHash = _Animator.GetParameter(0).nameHash;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !_Animator.GetBool(_AnimationHash))
         {
+            _PopRoutine.StopRoutine();
+
             MoneyManager.Instance.AddMoney(Value);
             _Animator.SetBool(_AnimationHash, true);
         }
@@ -24,5 +39,19 @@ public class Coin : MonoBehaviour
     private void Disable()
     {
         gameObject.SetActive(false);
+    }
+    private IEnumerator PopAnimation()
+    {
+        Vector2 start = transform.position;
+
+        Vector2 target = start + Vector2.right * Random.Range(-_PopRange, _PopRange);
+                target.x.Range(-4, 4);
+
+        for (float i = 0; i < _PopAnimationTime; i += Time.deltaTime * Time.timeScale)
+        {
+            transform.position = Vector2.Lerp(start, target, i / _PopAnimationTime);
+            yield return null;
+        }
+        _PopRoutine.Finish();
     }
 }
