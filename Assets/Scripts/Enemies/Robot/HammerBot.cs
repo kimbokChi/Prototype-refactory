7d3bed8ffcee57792d6e2a446e6505a80708fe74
kimbokChi <han15050424@gmail.com>
2021-01-22 21:01:11 +0900

    스팀펑크 테마의 적들 또한, 코인을 떨구도록 한다

--- Assets/Scripts/Enemies/Robot/HammerBot.cs
+++ Assets/Scripts/Enemies/Robot/HammerBot.cs
@@ -12,6 +12,7 @@ public class HammerBot : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
     [SerializeField] private MovementModule _Movement;
     [SerializeField] private RecognitionModule _Recognition;
     [SerializeField] private ShortRangeModule _AttackModule;
+    [SerializeField] private CoinDropper _CoinDropper;
 
     public AbilityTable GetAbility => AbilityTable;
 
@@ -57,6 +58,8 @@ public class HammerBot : MonoBehaviour, IObject, ICombatable, IAnimEventReceiver
             _Movement.MoveStop();
             EnemyAnimator.ChangeState(AnimState.Death);
             HealthBarPool.Instance.UnUsingHealthBar(transform);
+
+            _CoinDropper.Drop(5);
         }
     }
 
