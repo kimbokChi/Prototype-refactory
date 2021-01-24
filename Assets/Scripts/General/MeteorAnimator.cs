c1cdf7df1e6ca915ae6ee84379c29bc3c273b672
kimbokChi <han15050424@gmail.com>
2021-01-24 22:42:36 +0900

    운석 애니메이션들을 통한 연출을 보조하는 스크립트를 추가한다
    추가한 스크립트는 MeteorAnimator으로, 자신의 자식 오브젝트를 차례차례 활성화 시키는 기능을 한다.

---
+++ Assets/Scripts/General/MeteorAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorAnimator : MonoBehaviour
+{
+    private LinkedList<GameObject> _MeteorList;
+
+    private void Awake()
+    {
+        _MeteorList = new LinkedList<GameObject>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _MeteorList.AddLast(transform.GetChild(i).gameObject);
+            _MeteorList.Last.Value.SetActive(false);
+        }
+        StartCoroutine(MeteorOnableRoutine());
+    }
+    private IEnumerator MeteorOnableRoutine()
+    {
+        while (_MeteorList.Count > 0)
+        {
+            _MeteorList.First.Value.SetActive(true);
+            _MeteorList.RemoveFirst();
+
+            float waitTime = Random.Range(6f, 12f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
