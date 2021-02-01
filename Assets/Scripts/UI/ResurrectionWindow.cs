e0b66e0de6d5ddd85908830a951ab9b7047d49d4
kimbokChi <han15050424@gmail.com>
2021-02-01 10:46:06 +0900

    플레이어를 부활시키는 윈도우의 스크립트와, 부활 모듈을 추가한다
    플레이어를 부활시키는 윈도우의 스크립트는 ResurrectionWindow로, ShowAD함수를 통해  광고를 재생하고
    광고가 종료하면 플레이어를 부활시킨다. Close함수는 광고를 보지 않는 선택지이다.
    
    플레이어를 부활시키는 코드는 아직 존재하지 않는데, 부활은 Resurrectable스크립트를 통해 구현할 계획이다.

---
+++ Assets/Scripts/UI/ResurrectionWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResurrectionWindow : MonoBehaviour
+{
+    [SerializeField] private GameObject _NextWindow;
+    [SerializeField] private Resurrectable _Resurrectable;
+
+    private bool _IsAlreadyEarn = false;
+
+    [ContextMenu("FindPlayer")]
+    private void FindPlayer()
+    {
+        if (_Resurrectable == null)
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+
+            Debug.Assert(player.TryGetComponent(out _Resurrectable));
+        }
+    }
+    public void ShowAD()
+    {
+        if (!_IsAlreadyEarn)
+        {
+            Ads.Instance.ShowRewardAd();
+            Ads.Instance.UserEarnedRewardEvent(() =>
+            {
+                _IsAlreadyEarn = true;
+                gameObject.SetActive(false);
+
+                _Resurrectable.Resurrect();
+            });
+        }
+    }
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        _NextWindow.SetActive(true);
+    }
+}
