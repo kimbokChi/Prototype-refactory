e8663e0bcfb9aa8fd4f5b97c36750939eb8ae494
kimbokChi <han15050424@gmail.com>
2021-02-04 16:14:33 +0900

    기존의 공격 버튼을 사용되지 않는 클래스로 치부한다
    Obsolete 어트리뷰트를 사용하여 클래스를 제거하는 방식 대신에 사용하지 않는 것으로 처리한다.

--- Assets/Scripts/Player/Controller/AttackButtonController.cs
+++ Assets/Scripts/Player/Controller/AttackButtonController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Obsolete]
 public class AttackButtonController : Singleton<AttackButtonController>
 {
     public bool IsButtonHide
--- Assets/Scripts/Player/Controller/AttackButtonHider.cs
+++ Assets/Scripts/Player/Controller/AttackButtonHider.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Obsolete]
 public class AttackButtonHider : MonoBehaviour
 {
     private bool _ButtonAlreadyOpen;
