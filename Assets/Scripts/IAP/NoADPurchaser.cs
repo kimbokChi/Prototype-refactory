5ba51cf2d9ff3f9f5e4cec83a30959888697149d
kimbokChi <han15050424@gmail.com>
2021-02-06 15:29:59 +0900

    광고 제거 버튼을 구현한다
    NoADPurchaser스크립트를 통해 광고 제거 기능을 구현한다.

---
+++ Assets/Scripts/IAP/NoADPurchaser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoADPurchaser : MonoBehaviour
+{
+    public void PurchaseSuccess()
+    {
+        IAP.Instance.Reward();
+    }
+    public void PurchaseFaild()
+    {
+        IAP.Instance.Faild();
+    }
+}
