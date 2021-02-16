654b3db190a4e923a19d423f60d41aa03fbda721
kimbokChi <han15050424@gmail.com>
2021-02-16 20:30:11 +0900

    가상 조이스틱의 위치를 옮기는 스크립트를 구현한다
    이는 VirtualJoystickReposer스크립트를 통해 구현된다.
    
    이 스크립트가 적용된 오브젝트가 활성화 될 때 컨트롤러의 버튼을 비활성화 시키고,
    반대로 비활성화 될 때에는 컨트롤러의 모든 버튼을 활성화시킨다.
    
    활성화된 상태에서 버튼을 클릭한다면, EUpdate코루틴을 실행한다. 이 코루틴은 버튼에서 손을 때면 중단된다.
    코루틴이 실행되는 동안에 버튼은 커서를 따라오게된다.

---
+++ Assets/Scripts/UI/VirtualJoystick/VirtualJoystickReposer.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class VirtualJoystickReposer : MonoBehaviour
+{
+    private readonly Vector3 HalfScreen 
+        = new Vector3(Screen.width / 2f, Screen.height / 2f);
+
+    [SerializeField] private GameObject _SettingWindow;
+    [SerializeField] private PlayerController _Controller;
+
+    private bool _IsButtonStateDown = false;
+    private bool _IsAlreadyInit = false;
+
+    private void OnEnable()
+    {
+        if (!_IsAlreadyInit)
+        {
+            _Controller.AddInteractionAction(Reposition);
+        }
+        _SettingWindow.SetActive(false);
+        _Controller.SetActiveInteraction(true);
+
+        for (Direction d = 0; (int)d < 4; d++)
+        {
+            if (_Controller[d].TryGetComponent(out Button btn))
+            {
+                btn.enabled = false;
+            }
+            _Controller[d].enabled = false;
+        }
+        {
+            if (_Controller.AttackButton.TryGetComponent(out Button btn))
+            {
+                btn.enabled = false;
+            }
+            _Controller.AttackButton.enabled = false;
+        }
+    }
+    private void OnDisable()
+    {
+        _SettingWindow.SetActive(true);
+        _Controller.SetActiveInteraction(false);
+        
+        for (Direction d = 0; (int)d < 4; d++)
+        {
+            _Controller[d].enabled = true;
+
+            if (_Controller[d].TryGetComponent(out Button btn))
+            {
+                btn.enabled = true;
+            }
+        }
+        {
+            if (_Controller.AttackButton.TryGetComponent(out Button btn))
+            {
+                btn.enabled = true;
+            }
+            _Controller.AttackButton.enabled = true;
+        }
+    }
+
+    private void Reposition(ButtonState state)
+    {
+        switch (state)
+        {
+            case ButtonState.Down:
+                _IsButtonStateDown = true;
+
+                StartCoroutine(EUpdate());
+                break;
+
+            case ButtonState.Up:
+                _IsButtonStateDown = false;
+
+                GameLoger.Instance.ControllerPos = _Controller.transform.localPosition;
+                break;
+        }
+    }
+    private IEnumerator EUpdate()
+    {
+        while (_IsButtonStateDown)
+        {
+            yield return null;
+
+            Vector2 position = Input.mousePosition - HalfScreen;
+            _Controller.transform.localPosition = position;
+        }
+    }
+}
