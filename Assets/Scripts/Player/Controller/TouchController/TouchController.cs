94b450f89bc8d72c7e81e12eb177cf4cf27422b4
kimbokChi <han15050424@gmail.com>
2021-02-19 10:46:51 +0900

    플레이어의 스와이프 이동을 구현한다
    이는 TouchController스크립트를 통해서 구현된다. 현재까지는 이동만이 구현되었으며, 마을 씬에서만 가능하다.
    (테스트를 위해 임시로 마을 씬의 EventSystem오브젝트에 스크립트를 적용해 두었다.)

---
+++ Assets/Scripts/Player/Controller/TouchController/TouchController.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchController : MonoBehaviour
+{
+    private const float NeedMovingLength = 30;
+
+    private Coroutine _MoveRoutine;
+    private TouchPhase _CurrentPhase;
+    private Vector2 _BeganInputPoint;
+
+    private Player _Player;
+
+    public bool IsMobilePlatform()
+    {
+        switch (Application.platform)
+        {
+            case RuntimePlatform.Android:
+                return true;
+            default:
+                return false;
+        }
+    }
+    public bool HasInput(TouchPhase phase)
+    {
+        if (IsMobilePlatform())
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == phase)
+                {
+                    return EventSystem.current.IsPointerOverGameObject(touch.fingerId);
+                }
+            }
+            return false;
+        }
+        else
+        {
+            switch (phase)
+            {
+                case TouchPhase.Began:
+                    {
+                        return Input.GetMouseButtonDown(0);
+                    }
+                case TouchPhase.Moved:
+                case TouchPhase.Stationary:
+                    {
+                        return Input.GetMouseButton(0);
+                    }
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    {
+                        return Input.GetMouseButtonUp(0);
+                    }
+            }
+        }
+        return false;
+    }
+    public Vector2 InputPosition()
+    {
+        if (IsMobilePlatform())
+        {
+            return Input.GetTouch(0).position;
+        }
+        else
+        {
+            return Input.mousePosition;
+        }
+    }
+
+    private void Start()
+    {
+        _MoveRoutine = new Coroutine(this);
+        _CurrentPhase = TouchPhase.Ended;
+
+        _Player = FindObjectOfType<Player>();
+    }
+    private void Update()
+    {
+        CurrentPhaseCheck();
+
+        switch (_CurrentPhase)
+        {
+            case TouchPhase.Began:
+                {
+                    _BeganInputPoint = InputPosition();
+                }
+                break;
+            case TouchPhase.Moved:
+                {
+                    Vector2 inputPosition = InputPosition();
+
+                    if (Vector2.Distance(inputPosition, _BeganInputPoint) >= NeedMovingLength)
+                    {
+                        Vector2 direction = (inputPosition - _BeganInputPoint).normalized;
+                        _BeganInputPoint = inputPosition;
+
+                        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                        {
+                            if (direction.x > 0)
+                            {
+                                _MoveRoutine.StartRoutine(Move(Direction.Right));
+                            }
+                            else
+                            {
+                                _MoveRoutine.StartRoutine(Move(Direction.Left));
+                            }
+                        }
+                        else
+                        {
+                            if (direction.y > 0)
+                            {
+                                _Player.MoveOrder(Direction.Up);
+                            }
+                            else
+                            {
+                                _Player.MoveOrder(Direction.Down);
+                            }
+                        }
+                    }
+                }
+                break;
+            case TouchPhase.Stationary:
+                break;
+            case TouchPhase.Ended:
+                break;
+            case TouchPhase.Canceled:
+                break;
+            default:
+                break;
+        }
+    }
+    private void CurrentPhaseCheck()
+    {
+        for (TouchPhase phase = TouchPhase.Began; (int)phase < 5; phase++)
+        {
+            if (HasInput(phase))
+            {
+                _CurrentPhase = phase;
+                break;
+            }
+        }
+    }
+    private IEnumerator Move(Direction direction)
+    {
+        _Player.MoveOrder(direction);
+
+        while (_CurrentPhase == TouchPhase.Moved) { yield return null; }
+
+        _Player.MoveStop();
+
+        _MoveRoutine.Finish();
+    }
+}
