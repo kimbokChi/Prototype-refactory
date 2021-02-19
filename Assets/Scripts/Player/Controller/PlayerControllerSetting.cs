adf852adcccf613d0a4bfb93e88cce81ff96ab97
kimbokChi <han15050424@gmail.com>
2021-02-19 20:04:27 +0900

    VirtualJoystickSetting스크립트의 이름을 PlayerControllerSetting으로 변경한다
    해당 스크립트를 가상 조이스틱의 설정의 목적으로만 사용할 것이 아니기 때문에, 보다 범용적인 이름으로 변경한다.

--- Assets/Scripts/Player/Controller/PlayerControllerSetting.cs
+++ Assets/Scripts/Player/Controller/PlayerControllerSetting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VirtualJoystickSetting : MonoBehaviour
+public class PlayerControllerSetting : MonoBehaviour
 {
     [Header("____Controller Props_____")]
     [SerializeField] private VirtualJoystick _Controller;
