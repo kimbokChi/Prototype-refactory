cb215b5c8c99b62068f3b69061a660897d584ba0
kimbokChi <han15050424@gmail.com>
2021-02-21 19:53:17 +0900

    SoundManager는 SoundPack스크립트를 사용하도록 한다
    새로 추가한 스크립트인 SoundPack은, 기존의 SoundSet구조체를 대체하는 클래스이다.
    
    SoundManager의 인스펙터에 전부 때려박는 방식은 편집이 어렵기 때문에
    구조체와는 달리 오브젝트에 추가할 수 있다는 특징을 가진 클래스를 사용하도록 한다.
    
    그리고 42개에 달하는 열거형 SoundName의 요소를 추가했다.

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 
 public enum SoundName
 {
-    None
-}
-
-[Serializable]
-public struct SoundSet
-{
-    public SoundName Name;
-    public AudioSource Source;
+    None, ButtonClick1, ButtonClick2, ErrorWindow, OpenInventory, EquipItem, UnlockItem, PlayerDash,
+    ItemBoxOpen, DungeonResult, MysteriousBulletHit, ArrowHit, BowPull, BowShoot, SummonFrozenPillar, NormalSwing,
+    ShieldDefence, ThrowShuriken, GreatSword,
+    BossAppear_Forest, BossAppear_DeepSea, BossAppear_Factory, RepairBot_Attack, CannonBot_Attack, HammerBot_Attack,
+    GoblinAssassin_Dash, GoblinChief_SummonTotem, GoblinChief_SwingAttack, DartHit, DartShoot, GoblinNormal_Attack,
+    BuffTotem, ExplosionTotem, LightningTotem, Golem_Attack,
+    SharkTheSpearman_Attack, LatentMonkfish_Attack, LatentMonkfish_Swoop, PufferFish_Explosion,
+    PickUpCoin, Resurrection,
+    ShowDungeonSelection, BuyItem
 }
 
 public class SoundManager : Singleton<SoundManager>
 {
-    [SerializeField] private SoundSet[] _SoundSets;
+    [SerializeField] private SoundPack[] _SoundPacks;
 
-    private Dictionary<SoundName, AudioSource> _SoundLib;
+    private Dictionary<SoundName, SoundPack> _SoundLib;
+
+    private void Reset()
+    {
+        _SoundPacks = FindObjectsOfType<SoundPack>();
+    }
 
     private void Awake()
     {
@@ -31,15 +37,15 @@ public class SoundManager : Singleton<SoundManager>
         {
             DontDestroyOnLoad(gameObject);
 
-            _SoundLib = new Dictionary<SoundName, AudioSource>();
-            for (int i = 0; i < _SoundSets.Length; ++i)
+            _SoundLib = new Dictionary<SoundName, SoundPack>();
+            for (int i = 0; i < _SoundPacks.Length; ++i)
             {
-                _SoundLib.Add(_SoundSets[i].Name, _SoundSets[i].Source);
+                _SoundLib.Add(_SoundPacks[i].Name, _SoundPacks[i]);
             }
         }
     }
     public void PlaySound(SoundName name)
     {
-        _SoundLib[name].Play();
+        _SoundLib[name].PlaySound();
     }
 }
