d07720022b7f80146a6de1b6dcd3f9a61cb0f2c9
kimbokChi <han15050424@gmail.com>
2021-02-21 18:46:34 +0900

    소리를 재생하는 기능을 제공하는 스크립트를 추가한다
    추가한 스크립트의 이름은 SoundManager로, 인스펙터를 통해 SoundName이라는 열거형과 AudioSource을 짝지은 값을 인스펙터에서 지정받는다.
    
    이 값들은 Dictionary형태로 가공되어 사용된다.

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundName
+{
+    None
+}
+
+[Serializable]
+public struct SoundSet
+{
+    public SoundName Name;
+    public AudioSource Source;
+}
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [SerializeField] private SoundSet[] _SoundSets;
+
+    private Dictionary<SoundName, AudioSource> _SoundLib;
+
+    private void Awake()
+    {
+        if (FindObjectsOfType<SoundManager>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+
+            _SoundLib = new Dictionary<SoundName, AudioSource>();
+            for (int i = 0; i < _SoundSets.Length; ++i)
+            {
+                _SoundLib.Add(_SoundSets[i].Name, _SoundSets[i].Source);
+            }
+        }
+    }
+    public void PlaySound(SoundName name)
+    {
+        _SoundLib[name].Play();
+    }
+}
