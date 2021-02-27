e2b8e3f21adb52a910a1f3472eabfc5042f79bf3
kimbokChi <han15050424@gmail.com>
2021-02-27 20:46:30 +0900

    포션을 생성하는 테스트 코드를 제거한다
    Player스크립트에 작성되었던 테스트 코드를 제거한다

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -183,8 +183,6 @@ public class Player : MonoBehaviour, ICombatable
         } mInventory.SetWeaponSlot(instance);
 
         _Resurrectable.ResurrectAction += ResurrectAction;
-
-        PotionPool.Instance.Get(PotionName.LHealingPotion).transform.position = transform.position;
     }
     private void OnDestroy()
     {
