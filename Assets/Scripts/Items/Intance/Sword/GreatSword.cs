0e4568d15400d09584698d69bddec3a0c4f7b861
kimbokChi <han15050424@gmail.com>
2021-03-07 20:26:56 +0900

    짱쎈 대검의 아이템 효과를 수정한다
    검기의 피해량이 배율의 영향을 받도록 한다

--- Assets/Scripts/Items/Intance/Sword/GreatSword.cs
+++ Assets/Scripts/Items/Intance/Sword/GreatSword.cs
@@ -7,10 +7,9 @@ public class GreatSword : Item
 
     [Header("Charge Ablity")]
     [SerializeField] private Projection SwordDance;
-    [Range(0f, 1f)]
-    [SerializeField] private float DemandCharge;
-    [Range(1f, 20f)]
-    [SerializeField] private float SwordDanceSpeed;
+    [SerializeField, Range(0f, 1f)] private float DemandCharge;
+    [SerializeField, Range(0f, 5f)] private float _DamageScale;
+    [SerializeField, Range(1f, 20f)] private float SwordDanceSpeed;
 
     private Pool<Projection> SwordDancePool;
 
@@ -58,7 +57,7 @@ public class GreatSword : Item
                             {
                                 if (o.TryGetComponent(out ICombatable combatable))
                                 {
-                                    combatable.Damaged(StatTable[ItemStat.AttackPower], mPlayer);
+                                    combatable.Damaged(StatTable[ItemStat.AttackPower] * _DamageScale, mPlayer);
                                 }
                             }, 
                             o => SwordDancePool.Add(o));
