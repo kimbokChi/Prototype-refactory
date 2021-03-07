987934b17d0f500bdfec1a260e48b72e99a56edf
kimbokChi <han15050424@gmail.com>
2021-03-07 19:14:22 +0900

    강철검이 충전의 효과로 검기를 발사하도록 한다
    강철검이 사용하는 검기 프리팹을 추가하고, 애니메이션 이벤트를 통해 검기를 발사하는 이벤트를 실행하도록 하여
    (다만, 충전한 이후에만 실행되도록 함)이를 구현한다.

--- Assets/Scripts/Items/Intance/Sword/LongSword.cs
+++ Assets/Scripts/Items/Intance/Sword/LongSword.cs
@@ -5,11 +5,19 @@ public class LongSword : Item
     [SerializeField] private Animator Animator;
     [SerializeField] private Area CollisionArea;
 
+    [Header("SwordDance Property")]
+    [SerializeField] private Projection _SwordDance;
+    [SerializeField, Range(1f, 3f)] private float _DamageScale;
+    [SerializeField, Range(1f, 9f)] private float _Speed;
+
+    private Pool<Projection> _SwordDancePool;
+
     private int _AnimPlayKey;
     private int _AnimControlKey;
 
     private GameObject mPlayer;
     private bool _IsAttackOver = true;
+    private bool _CanUsingSwordDance = false;
 
     private void Reset()
     {
@@ -20,6 +28,7 @@ public class LongSword : Item
         if (offSlot.Equals(SlotType.Weapon))
         {
             AttackOverAction = null;
+            Inventory.Instance.ChargeAction -= ChargeAction;
         }
     }
     protected override void AttackAnimationPlayOver()
@@ -38,17 +47,49 @@ public class LongSword : Item
             _AnimControlKey = Animator.GetParameter(1).nameHash;
 
             mPlayer = mPlayer ?? transform.parent.parent.gameObject;
+
+            if (_SwordDancePool == null)
+            {
+                _SwordDancePool = new Pool<Projection>();
+                _SwordDancePool.Init(2, _SwordDance, o => 
+                {
+                    o.transform.localScale = Vector2.one * 0.7f;
+                    o.SetAction(hit =>
+                    {
+                        if (hit.TryGetComponent(out ICombatable combatable))
+                        {
+                            combatable.Damaged(StatTable[ItemStat.AttackPower] * _DamageScale, mPlayer.gameObject);
+                        }
+                    }, p => _SwordDancePool.Add(p));
+                });
+            }
+            Inventory.Instance.ChargeAction += ChargeAction;
         }
     }
+    public void ShootSwordDance()
+    {
+        if (_CanUsingSwordDance)
+        {
+            var projction = _SwordDancePool.Get();
 
+            var direction = Vector2.right;
+
+            if (mPlayer.transform.localRotation.eulerAngles.y > 0f)
+            {
+                direction = Vector2.left;
+            }
+            projction.Shoot(transform.position + new Vector3(0.1f, 0.2f), direction, _Speed);
+            projction.transform.localRotation = mPlayer.transform.localRotation;
+
+            _CanUsingSwordDance = false;
+        }
+    }
     public override void AttackAction(GameObject attacker, ICombatable combatable)
     {
         Animator.SetBool(_AnimPlayKey, true);
         Animator.SetBool(_AnimControlKey, !Animator.GetBool(_AnimControlKey));
 
         _IsAttackOver = false;
-
-        mPlayer = attacker;
     }
     protected override void CameraShake()
     {
@@ -63,7 +104,11 @@ public class LongSword : Item
             Inventory.Instance.OnAttackEvent(mPlayer, combatable);
         }
     }
-
+    private void ChargeAction(float charge)
+    {
+        AttackAction(null, null);
+        _CanUsingSwordDance = true;
+    }
     public override void AttackCancel()
     {
         if (!_IsAttackOver)
