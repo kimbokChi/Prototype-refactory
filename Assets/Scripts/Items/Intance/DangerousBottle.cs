98fdcaba832f07c73e860fdb34bbaccbc26333a0
kimbokChi <han15050424@gmail.com>
2021-03-12 09:57:52 +0900

    위험해 보이는 병의 스크립트를 구현하고, 애니메이션을 수정한다
    공격 동작이 더욱 자연스러워 보이도록 공격 애니메이션을 조정한다.
    그리고, 공격하는 타이밍에만 콜라이더를 활성화도록 애니메이션에 콜라이더에 대한 프로퍼티를 추가한다.
    
    위험해 보이는 병의 스크립트를 통해서 현재 무기로서의 기능만 구현하였다.
    또한 애니메이션 이벤트로 실행되는 공격 이펙트도 구현했는데..딱히 어울리지는 않는 것 같다.

---
+++ Assets/Scripts/Items/Intance/DangerousBottle.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerousBottle : Item
+{
+    [SerializeField] private Animator _Animator;
+    [SerializeField] private BoxCollider2D _Collider;
+
+    [Header("Poision Property")]
+    [SerializeField, Range( 1,  10)] private uint _PoisionLevel;
+    [SerializeField, Range(0f, 10f)] private float _PoisionDurate;
+
+    private int _AnimatorHash;
+    private bool _IsAlreadyInit = false;
+
+    private GameObject _Player;
+    private void Init()
+    {
+        if (!_IsAlreadyInit)
+        {
+            _IsAlreadyInit = true;
+            _AnimatorHash = _Animator.GetParameter(0).nameHash;
+
+            _Collider.enabled = false;
+        }
+    }
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        _Player = attacker;
+        _Animator.SetBool(_AnimatorHash, true);
+    }
+    private void EnableCollider()
+    {
+        float minX = _Collider.size.x - _Collider.offset.x;
+        float maxX = _Collider.size.x + _Collider.offset.x;
+
+        int invoke = (int)(_Collider.size.x * 16);
+        for (int i = 0; i < invoke; i++)
+        {
+            Vector3 offset = new Vector2(Random.Range(minX, maxX), Random.value);
+            EffectLibrary.Instance.UsingEffect(EffectKind.Poision, transform.position + offset);
+        }
+    }
+    protected override void AttackAnimationPlayOver()
+    {
+        base.AttackAnimationPlayOver();
+
+        _Animator.SetBool(_AnimatorHash, false);
+    }
+    public override void AttackCancel()
+    {
+        _Animator.SetBool(_AnimatorHash, false);
+    }
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        
+    }
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        Init();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            if (collision.TryGetComponent(out ICombatable combatable))
+            {
+                combatable.CastBuff(Buff.Poision, GetPoisionBuff(combatable));
+                combatable.Damaged(StatTable[ItemStat.AttackPower], _Player);
+            }
+        }
+    }
+    private IEnumerator GetPoisionBuff(ICombatable combatable)
+    {
+        return BuffLibrary.Instance.GetBuff(Buff.Poision, _PoisionLevel, _PoisionDurate, combatable.GetAbility);
+    }
+}
