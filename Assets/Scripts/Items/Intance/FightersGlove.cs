9235fb2591afa12ce72a45a17d5012164f00cbc5
kimbokChi <han15050424@gmail.com>
2021-03-13 22:38:46 +0900

    "파이터의 장갑"아이템을 구현한다
    스크립트를 통해 행동을 구현하고, 애니메이션에 이벤트를 부여하여 알맞은 타이밍에 기능이 작동하도록 한다.
    
    이렇게 만들어진 아이템은 프리팹으로 만들어 Assets에 저장했다.

---
+++ Assets/Scripts/Items/Intance/FightersGlove.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightersGlove : Item
+{
+    private enum Anim {
+        Default, Attack_Begin, Attack_End, Attack
+    }
+
+    [Header("FightersGlove Property")]
+    [SerializeField] private Animator _Animator;
+    private int _AnimatorHash;
+
+    [SerializeField] private Area _LeftFist;
+    [SerializeField] private Area _RightFist;
+
+    private bool _IsAlreadyInit = false;
+    private GameObject _Player;
+
+    public override void AttackCancel()
+    {
+        _Animator.SetInteger(_AnimatorHash, (int)Anim.Default);
+    }
+    public override void OffEquipThis(SlotType offSlot)
+    {
+        switch (offSlot)
+        {
+            case SlotType.Accessory:
+                Inventory.Instance.AttackEvent -= AttackEvent;
+                break;
+
+            case SlotType.Weapon:
+
+                break;
+        }
+    }
+    public override void OnEquipThis(SlotType onSlot)
+    {
+        Init();
+
+        switch (onSlot)
+        {
+            case SlotType.Accessory:
+                Inventory.Instance.AttackEvent += AttackEvent;
+                break;
+
+            case SlotType.Weapon:
+                if (_Player == null) {
+                    _Player = transform.parent.parent.gameObject;
+                }
+                break;
+        }
+    }
+    public override void AttackAction(GameObject attacker, ICombatable combatable)
+    {
+        _Animator.SetInteger(_AnimatorHash, (int)Anim.Attack_Begin);
+    }
+    protected override void AttackAnimationPlayOver()
+    {
+        base.AttackAnimationPlayOver();
+        _Animator.SetInteger(_AnimatorHash, (int)Anim.Attack_End);
+    }
+    private void AttackEvent(GameObject attacker, ICombatable target)
+    {
+        target.Damaged(5f, attacker);
+    }
+    private void Init()
+    {
+        if (!_IsAlreadyInit)
+        {
+            _IsAlreadyInit = true;
+
+             _LeftFist.SetEnterAction(Hit);
+            _RightFist.SetEnterAction(Hit);
+
+            _AnimatorHash = _Animator.GetParameter(0).nameHash;
+        }
+    }
+    private void Hit(GameObject target)
+    {
+        if (target.TryGetComponent(out ICombatable combatable))
+        {
+            combatable.Damaged(StatTable[ItemStat.AttackPower], _Player);
+            Inventory.Instance.OnAttackEvent(_Player, combatable);
+        }
+    }
+}
