0f129bb7fa9a0c18d8acf6ad271a0f7afa1ce682
kimbokChi <han15050424@gmail.com>
2021-03-25 09:08:32 +0900

    예/아니요를 표시하는 시시템 메시지 창을 추가한다
    SystemMessage클래스의 "ShowCheckMessage"함수를 통해 사용할 수 있다.
    이 함수의 두 번째 인자는 "예"를 클릭했는지의 여부를 전달하는 대리자이다.
    
    이렇게 추가도니 시스템 메시지 창을 InGameUI프리팹에 추가한다.

--- Assets/Scripts/Singletons/SystemMessage.cs
+++ Assets/Scripts/Singletons/SystemMessage.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class SystemMessage : Singleton<SystemMessage>
 {
@@ -6,6 +7,11 @@ public class SystemMessage : Singleton<SystemMessage>
     [SerializeField] private GameObject MessageBox;
     [SerializeField] private TMPro.TextMeshProUGUI MessageText;
 
+    [Header("Check Btn Property")]
+    [SerializeField] private GameObject _CheckMessageBox;
+    [SerializeField] private SubscribableButton _YesButton;
+    [SerializeField] private SubscribableButton  _NoButton;
+    
     public void ShowMessage(string message)
     {
         Time.timeScale = 0f;
@@ -17,11 +23,37 @@ public class SystemMessage : Singleton<SystemMessage>
 
         SoundManager.Instance.PlaySound(SoundName.ErrorWindow);
     }
+    public void ShowCheckMessage(string message, Action<bool> result)
+    {
+        _CheckMessageBox.SetActive(true);
+        ShowMessage(message);
+        MessageBox.SetActive(false);
+
+        _YesButton.ButtonAction += state =>
+        {
+            if (state == ButtonState.Down) 
+                ButtonAction(true);
+        };
+        _NoButton.ButtonAction += state =>
+        {
+            if (state == ButtonState.Down) 
+                ButtonAction(false);
+        };
+        void ButtonAction(bool parameter)
+        {
+            result.Invoke(parameter);
+
+            CloseMessage();
+             _NoButton.ButtonActionReset();
+            _YesButton.ButtonActionReset();
+        }
+    }
     public void CloseMessage()
     {
         Time.timeScale = 1f;
 
         Background.SetActive(false);
         MessageBox.SetActive(false);
+        _CheckMessageBox.SetActive(false);
     }
 }
