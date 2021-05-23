216c0ec609cf8171cb8cea01b8e80c454fc49161
kimbokChi <han15050424@gmail.com>
2021-05-23 19:54:22 +0900

    씬에 임시로 Sword보스를 배치하고, Sword보스가 사용할 스크립트를 추가한다
    추가한 스크립트는 아직 제대로 구현되지 않은 상태이다. 일단은 애니메이션 작업부터 하고,
    스크립트를 구현하겠습니다 앓~

---
+++ Assets/Scripts/Enemies/Boss/Robots/RobotsBossSword.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotsBossSword : MonoBehaviour, IObject, ICombatable
+{
+    [SerializeField] private ItemDropper _ItemDropper;
+
+    [Header("Ability")]
+    [SerializeField] private AbilityTable _AbilityTable;
+    [SerializeField] private Animator _Animator;
+    [SerializeField] private GameObject _HealthBar;
+    [SerializeField] private Image _HealthBarImage;
+
+    private Player _Player;
+    private int _AnimControlKey;
+
+    public void IInit()
+    {
+        _AnimControlKey = _Animator.GetParameter(0).nameHash;
+    }
+    public void IUpdate()
+    {
+        
+    }
+    public void PlayerEnter(MESSAGE message, Player enterPlayer)
+    {
+        if (_AbilityTable.CanRecognize(message))
+        {
+            _Player = enterPlayer;
+        }
+    }
+    public void PlayerExit(MESSAGE message)
+    {
+        if (_AbilityTable.CantRecognize(message))
+        {
+            _Player = null;
+        }
+    }
+    public void Damaged(float damage, GameObject attacker)
+    {
+        if ((_AbilityTable.Table[Ability.CurHealth] -= damage) <= 0f)
+        {
+            _ItemDropper.CoinDrop(40);
+            _ItemDropper.TryPotionDrop(PotionName.SHealingPotion, PotionName.LHealingPotion);
+        }
+    }
+    #region
+    public void CastBuff(Buff buffType, IEnumerator castedBuff)
+    {
+        StartCoroutine(castedBuff);
+    }
+    public bool IsActive()
+    {
+        return gameObject.activeSelf;
+    }
+    public GameObject ThisObject()
+    {
+        return gameObject;
+    }
+    public AbilityTable GetAbility => _AbilityTable;
+    #endregion;
+}
