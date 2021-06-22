f3d0e7ed2f877243e32bfbfd4b8bd6d54b020fee
kimbokChi <han15050424@gmail.com>
2021-06-22 22:51:21 +0900

    조이스틱 조작의 공격 버튼의 용도를 전환할 수 있도록 한다
    - AttackButton의 이름을 CoreButton으로 바꾼다
    - VirtualJoystick 스크립트의 CoreButton은 현재 모드에 따라 하는 행동이 달라지며,
    CoreButton의 모드를 바꾸는 메서드를 제공한다.
    
    현재 존재하는 모드는 '공격'과 '상호작용'이다. 모드를 바꿀 때마다 버튼의 이미지가 바뀌게 된다!

--- Assets/Scripts/Player/Controller/VirtualJoystick/VirtualJoystick.cs
+++ Assets/Scripts/Player/Controller/VirtualJoystick/VirtualJoystick.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 
 // 플레이어에게 이동을 지시할 수 있어야 한다.
 // 플레이어의 이동은 플레이어 내부적으로 처리한다.
@@ -10,13 +10,19 @@ using UnityEngine.EventSystems;
 // 또한 위/아래로 이동할 때에는 y값만 MovePoint로 부터 불러오도록..
 // 일단 공격 버튼부터 만들자. 이동은 좀 걸릴것 같으니까.
 
+public enum CoreBtnMode
+{
+    AttackOrder, InteractionOrder
+}
+
 public class VirtualJoystick : MonoBehaviour
 {
     private readonly Vector3 HalfScreen
         = new Vector3(Screen.width / 2f, Screen.height / 2f);
 
     [SerializeField] private RectTransform _RectTransform;
-    [SerializeField] private SubscribableButton _AttackButton;
+    [SerializeField, FormerlySerializedAs("_AttackButton")] 
+    private SubscribableButton _CoreButton;
 
     [Header("___Scaling Prop___")]
     [SerializeField] private float _ScalingTime;
@@ -33,6 +39,11 @@ public class VirtualJoystick : MonoBehaviour
     [SerializeField] private SubscribableButton _Button;
     [SerializeField] private Image  _ButtonImage;
 
+    [Header("___CoreBtn Sprite Prop___")]
+    [SerializeField] private Image _CoreBtnImage;
+    [SerializeField] private Sprite _CoreBtnSprite_Attack;
+    [SerializeField] private Sprite _CoreBtnSprite_Interaction;
+
     private const float IntervalClickTime = 0.2f;
     private const float DefaultSizeDelta = 393.84615f;
     private float _LastClickTime = 0f;
@@ -42,6 +53,8 @@ public class VirtualJoystick : MonoBehaviour
     {
         get => _RectTransform.rect;
     }
+    public CoreBtnMode CrntCoreBtnMode
+    { get; private set; }
 
     public SubscribableButton this[Direction dir]
     {
@@ -59,11 +72,11 @@ public class VirtualJoystick : MonoBehaviour
                     return _LMoveButton;
 
                 default:
-                    return _AttackButton;
+                    return _CoreButton;
             }
         }
     }
-    public SubscribableButton AttackButton => _AttackButton;
+    public SubscribableButton AttackButton => _CoreButton;
 
     private Player _Player;
 
@@ -85,29 +98,37 @@ public class VirtualJoystick : MonoBehaviour
             {
                 if (find.TryGetComponent(out _Player))
                 {
-                    _AttackButton.ButtonAction += state =>
+                    _CoreButton.ButtonAction += state =>
                     {
-                        switch (state)
+                        switch (CrntCoreBtnMode)
                         {
-                            case ButtonState.Down:
+                            case CoreBtnMode.AttackOrder:
+                                switch (state)
+                                {
+                                    case ButtonState.Down:
 
-                                _WaitCharge.StartRoutine(WaitForStartCharging());
+                                        _WaitCharge.StartRoutine(WaitForStartCharging());
+                                        break;
+
+                                    case ButtonState.Up:
+
+                                        if (!_WaitCharge.IsFinished())
+                                        {
+                                            _Player.AttackOrder();
+                                        }
+                                        else
+                                        {
+                                            Finger.Instance.EndCharging();
+
+                                            _WaitScaling.StopRoutine();
+                                            _WaitScaling.StartRoutine(AttackBtnScaling(true));
+                                        }
+                                        _WaitCharge.StopRoutine();
+                                        break;
+                                }
                                 break;
+                            case CoreBtnMode.InteractionOrder:
 
-                            case ButtonState.Up:
-
-                                if (!_WaitCharge.IsFinished())
-                                {
-                                    _Player.AttackOrder();
-                                }
-                                else
-                                {
-                                    Finger.Instance.EndCharging();
-
-                                    _WaitScaling.StopRoutine();
-                                    _WaitScaling.StartRoutine(AttackBtnScaling(true));
-                                }
-                                _WaitCharge.StopRoutine();
                                 break;
                         }
                     };
@@ -194,8 +215,22 @@ public class VirtualJoystick : MonoBehaviour
         return new SubscribableButton[5]
         {
             _UMoveButton, _DMoveButton,
-            _LMoveButton, _RMoveButton, _AttackButton
+            _LMoveButton, _RMoveButton, _CoreButton
         };
+    }
+
+    public void SetCoreBtnMode(CoreBtnMode btnMode)
+    {
+        switch (CrntCoreBtnMode = btnMode)
+        {
+            case CoreBtnMode.AttackOrder:
+                _CoreBtnImage.sprite = _CoreBtnSprite_Attack;
+                break;
+
+            case CoreBtnMode.InteractionOrder:
+                _CoreBtnImage.sprite = _CoreBtnSprite_Interaction;
+                break;
+        }
     }
     public void SetPositionWithScreenRange(Vector2 position)
     {
@@ -229,7 +264,7 @@ public class VirtualJoystick : MonoBehaviour
         _LMoveButton.transform.localScale = scale;
         _RMoveButton.transform.localScale = scale;
 
-        _AttackButton.transform.localScale = scale;
+        _CoreButton.transform.localScale = scale;
 
         _RectTransform.sizeDelta = Vector2.one * defaultScale * DefaultSizeDelta;
         _WaitScaling.StopRoutine();
@@ -273,8 +308,8 @@ public class VirtualJoystick : MonoBehaviour
         {
             float ratio = Mathf.Min(i / _ScalingTime, 1f);
 
-            _AttackButton.transform.localScale = 
-                Vector3.Lerp(_AttackButton.transform.localScale, targetScale, ratio);
+            _CoreButton.transform.localScale = 
+                Vector3.Lerp(_CoreButton.transform.localScale, targetScale, ratio);
 
             yield return null;
         }
