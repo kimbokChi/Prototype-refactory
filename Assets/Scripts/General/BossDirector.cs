535cf667f55d355f09aef65a9c2691fd392205f2
kimbokChi <han15050424@gmail.com>
2021-07-12 21:06:50 +0900

    BossDirector는 플레이어가 보스의 방에 들어섰을 때 작동되도록 한다
    우선 테스트로 작성했던 코드를 제거했다.
    
    BossDirector는 인벤토리의 MoveUpDownEvent이벤트를 구독하여 현재 플레이어가 몇층에 존재
    하는지를 확인하여, 현재 보스층 이전의 층에 있다면 보스 등장 연출을 보여줄 준비를 하고,
    보스층에 있다면 보스 등장 연출을 보여주도록 한다.
    
    그리고 SE_로 시작하는 메서드를 Timeline의 SignalEmitter통해 실행하여 연출이 시작될 때, 끝날때
    등의 동작을 구현하도록 한다.

--- Assets/Scripts/General/BossDirector.cs
+++ Assets/Scripts/General/BossDirector.cs
@@ -6,23 +6,46 @@ using UnityEngine.Playables;
 public class BossDirector : MonoBehaviour
 {
     [SerializeField] private PlayableDirector _Director;
+    [SerializeField] private Floor _BossFloor;
 
     [Header("Audio Property")]
     [SerializeField] private AudioClip _BossThema;
     [SerializeField] private AudioSource _AudioSource;
 
-    [ContextMenu("Play Timeline")]
-    private void PlayTimeLine()
+    private void Awake()
     {
-        _Director.Play();
+        Inventory.Instance.MoveUpDownEvent += (pos, dir) =>
+        {
+            // 다음 층으로 이동한다면
+            if (dir == Direction.Up && pos == UnitizedPosV.BOT)
+            {
+                // 보스 이전 층에 입장
+                if (Castle.Instance.PlayerFloor.FloorIndex == _BossFloor.FloorIndex - 1)
+                {
+                    Castle.Instance.CanPlayerFloorNotify = false;
+                }
+                // 보스 층에 입장
+                else if (Castle.Instance.PlayerFloor.FloorIndex == _BossFloor.FloorIndex)
+                {
+                    _Director.Play();
+                    Castle.Instance.ForceStopUpdate();
+                }
+            }
+        };
     }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-            PlayTimeLine();
-    }
-    public void SE_Action()
+    public void SE_Start()
     {
         _AudioSource.clip = _BossThema;
+    }
+    public void SE_CameraSetPosition()
+    {
+        MainCamera.Instance.Move(_BossFloor.transform.localPosition, 100f);
+    }
+    public void SE_Finish()
+    {
+        Castle.Instance.CanPlayerFloorNotify = true;
+        Castle.Instance.PlayerFloorNotify();
+
+        Castle.Instance.ReStartUpdate();
     }
 }
